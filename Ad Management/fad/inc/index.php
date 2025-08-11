9dc99f9d35a504d867dafa3e0899487d130b1897
me-suzy <me.suzana@gmail.com>
2025-08-11 09:55:22 +0300

    Add Ad Management/fad/inc/index.php

---
+++ Ad Management/fad/inc/index.php
@@ -0,0 +1,15 @@
+<?
+//***********************************************************************************//
+//** Please leave this message alone, no one will be able to see it, except coders.**//
+// This script is copyrighted by PHP scripts, a Marsal Design Company.***************//
+// To get your own verison of this system, please do download it from our site*******//
+// located at http://www.free-php-scripts.net****************************************//
+// Script is free, no advertisement, no nothing....**********************************//
+//***********************************************************************************//
+
+//This is used as a security measure, no one will be able to access the includes folder directly
+	
+	
+header("Location: ../index.php");
+exit();
+?>
