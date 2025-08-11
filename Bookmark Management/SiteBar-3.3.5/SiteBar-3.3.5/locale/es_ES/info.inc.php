94b674fd4982d7f97c7e03be0787d8bae0ca043f
me-suzy <me.suzana@gmail.com>
2025-08-11 13:17:43 +0300

    Add Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/es_ES/info.inc.php

---
+++ Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/es_ES/info.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$info = array
+(
+    'language' => 'Espanol',
+    'authors' => array
+    (
+        'Francisco Gómez González' => 'http://www.cursoshomologados.com/',
+        'Juan Diego Gutiérrez Gallardo' => 'mailto:enreas@gmail.com',
+    ),
+);
+
+?>
