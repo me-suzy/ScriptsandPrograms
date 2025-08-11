e3e18a6a0bd02512fd1d8463a9845da89b828493
me-suzy <me.suzana@gmail.com>
2025-08-11 13:13:41 +0300

    Add Bookmark Management/phphomepage1_6a/phphomepage/include/close.inc.php

---
+++ Bookmark Management/phphomepage1_6a/phphomepage/include/close.inc.php
@@ -0,0 +1,16 @@
+<?
+/**
+ * [fr]Fichier de clôture de connection à la base
+ * [en]File closing database conexion
+ *
+ * @copyright    15/11/2003
+ * @since	     03/08/2001
+ * @version      1.5
+ * @module       connec.inc
+ * @modulegroup  include
+ * @package      php_homepage
+ * @access	     public
+ * @author       Eric BLAS <webmaster@phphomepage.net>
+ */
+mysql_close();
+?>
