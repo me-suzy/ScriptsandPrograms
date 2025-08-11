a8f678a976489380e93095e00035154d8b954127
me-suzy <me.suzana@gmail.com>
2025-08-11 13:49:26 +0300

    Add Chat Scripts/jodchat/jodchat_conf.php

---
+++ Chat Scripts/jodchat/jodchat_conf.php
@@ -0,0 +1,18 @@
+<?
+
+//password must not contain any spaces
+$moderator_password = "secret";
+
+//to enable moderator kicking set $moderator_kick = true;
+$moderator_kick = false;
+
+
+//to enable moderator bombing set $moderator_bomb = true;
+$moderator_bomb = false;
+
+//tell browser at what rate to check for new messages
+$check_rate = 2;
+
+
+
+?>
