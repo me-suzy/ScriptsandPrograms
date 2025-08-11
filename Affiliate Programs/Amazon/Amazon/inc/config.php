7231e02fca0a99f229b7c3709dec160b8cb07b56
me-suzy <me.suzana@gmail.com>
2025-08-11 10:19:54 +0300

    Add Affiliate Programs/Amazon/Amazon/inc/config.php

---
+++ Affiliate Programs/Amazon/Amazon/inc/config.php
@@ -0,0 +1,13 @@
+<?php
+/* ########################################################################
+
+Copyright (C) 2003 FORTUNE DESIGN
+Amazon feed Version 1.0
+sales@fortunedesign.co.uk
+http://www.fortunedesign.co.uk
+
+######################################################################## */
+// Amazon Config
+define('ASSOCIATES_ID', 				'barelyreadboo-21');
+
+?>
