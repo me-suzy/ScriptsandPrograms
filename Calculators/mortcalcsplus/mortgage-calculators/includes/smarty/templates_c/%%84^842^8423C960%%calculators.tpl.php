978023cc77fe964e70a1d31715ce5481382e239e
me-suzy <me.suzana@gmail.com>
2025-08-11 13:27:36 +0300

    Add Calculators/mortcalcsplus/mortgage-calculators/includes/smarty/templates_c/%%84^842^8423C960%%calculators.tpl.php

---
+++ Calculators/mortcalcsplus/mortgage-calculators/includes/smarty/templates_c/%%84^842^8423C960%%calculators.tpl.php
@@ -0,0 +1,24 @@
+<?php /* Smarty version 2.6.5-dev, created on 2005-05-12 11:39:37
+         compiled from calculators.tpl */ ?>
+<?php $this->assign('title', 'Mortgage Calculators'); ?>
+<?php $this->assign('description', "Online mortgage calculators."); ?>
+
+<h1 id="calcH1">Mortgage Calculators</h1>
+<table align="center" cellpadding="5"><tr><td valign="top">
+<ul id="calclist">
+	<li><a href="calc-monthlypayment.php">Mortgage payment calculator</a><br>Want to know how much your monthly payment is for your mortgage?</li>
+	<li><a href="calc-additionalpayment.php">Additional payment calculator</a><br>How much do you save by paying more or making additional payments than your initial mortgage terms?</li>
+	<li><a href="calc-earnhome.php">How much do I have to earn?</a><br>Not sure how much money you'll have to earn to afford your house payment and accompanying expenses?</li>
+	<li><a href="calc-affordborrow.php">How much can I borrow?</a><br>Want to know how big of a mortgage you can take on?</li>
+	<li><a href="calc-discount.php">Should I pay discount points?</a><br>Not sure if you should pay discount points on your mortgage loan?</li>
+	<li><a href="calc-recoup.php">How much will I save by refinancing my loan?</a><br>How long will it take to recoup the costs of refinancing my home mortgage?</li>
+	<li><a href="calc-deduct.php">How much will my tax deduction be?</a><br>Want to know how much your home mortgage will save you in taxes?</li>
+	<li><a href="calc-biweeklypayment.php">Bi-weekly mortgage calculator</a><br>Want to know how much time and money you'll save paying off your loan on a bi-weekly payment plan?</li>
+	<li><a href="calc-apr.php">APR calculator</a><br>To find out the annual percentage rate of your loan, enter the loan amount, interest rate, points, other costs<br>and year-length term.</li>
+	<li><a href="calc-interestonly.php">Interest only monthly payment calculator</a><br>To find out the monthly savings you could gain from an interest-only payment plan.</li>
+</ul>
+</td></tr></table>
+
+<!-- DO NOT REMOVE THE LINE BELOW. SEE THE LICENSE AGREEMENT. THANKS -->
+<div id="powered_by">Powered by <a href="http://www.mortgagecalculatorsplus.com/">Mortgage Calculators Plus</a></div>
+<!-- DO NOT REMOVE THE LINE ABOVE. SEE THE LICENSE AGREEMENT. THANKS -->
