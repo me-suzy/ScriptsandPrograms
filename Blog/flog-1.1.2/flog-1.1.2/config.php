3bd12a60c5517caf3bcfa0cfafe3196d0c640338
me-suzy <me.suzana@gmail.com>
2025-08-11 11:43:27 +0300

    Add Blog/flog-1.1.2/flog-1.1.2/config.php

---
+++ Blog/flog-1.1.2/flog-1.1.2/config.php
@@ -0,0 +1,8 @@
+<?php
+ $FLog_dir_plugins = 'plugins/';
+ $FLog_dir_data    = 'data/';
+ $FLog_dir_themes  = 'themes/';
+ $FLog_dir_include = 'include/';
+ $FLog_dir_files   = 'files/';
+ require_once($FLog_dir_include.'core.inc.php');
+?>
