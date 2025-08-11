47d08c2ac55ecd1cee644a572bec38bcc8fd9dc8
me-suzy <me.suzana@gmail.com>
2025-08-11 10:12:53 +0300

    Add Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/language/chinese_gb2312/report.lang.php

---
+++ Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/language/chinese_gb2312/report.lang.php
@@ -0,0 +1,47 @@
+<?php // $Revision: 2.0 $
+
+
+
+/************************************************************************/
+
+/* phpAdsNew 2                                                          */
+
+/* ===========                                                          */
+
+/*                                                                      */
+
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+
+/* For more information visit: http://www.phpadsnew.com                 */
+
+/*                                                                      */
+
+/*                                                                      */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+
+
+$GLOBALS['strPluginAffiliate'] 		= "²úÉúËùÑ¡¹ã¸æ·¢²¼ÈËµÄÀúÊ·×ÜÀÀ.´Ë±¨±íÊä³ö¸ñÊ½Îª¿ÉÒÔÔÚÖÆ±íÈí¼þÖÐÊ¹ÓÃµÄCSV.";
+
+$GLOBALS['strPluginCampaign'] 		= "²úÉúËùÑ¡ÏîÄ¿µÄÀúÊ·×ÜÀÀ.´Ë±¨±íÊä³ö¸ñÊ½Îª¿ÉÒÔÔÚÖÆ±íÈí¼þÖÐÊ¹ÓÃµÄCSV.";
+
+$GLOBALS['strPluginClient'] 		= "²úÉúËùÑ¡¹ã¸æ¿Í»§µÄÀúÊ·×ÜÀÀ.´Ë±¨±íÊä³ö¸ñÊ½Îª¿ÉÒÔÔÚÖÆ±íÈí¼þÖÐÊ¹ÓÃµÄCSV.";
+
+$GLOBALS['strPluginGlobal'] 		= "²úÉúÈ«¾ÖµÄÀúÊ·×ÜÀÀ.´Ë±¨±íÊä³ö¸ñÊ½Îª¿ÉÒÔÔÚÖÆ±íÈí¼þÖÐÊ¹ÓÃµÄCSV.";
+
+$GLOBALS['strPluginZone'] 		= "²úÉúËùÑ¡°æÎ»µÄÀúÊ·×ÜÀÀ.´Ë±¨±íÊä³ö¸ñÊ½Îª¿ÉÒÔÔÚÖÆ±íÈí¼þÖÐÊ¹ÓÃµÄCSV.";
+
+
+
+?>
