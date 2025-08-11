3b8961269c9028483f9401935f4505be1b88b6bb
me-suzy <me.suzana@gmail.com>
2025-08-11 14:00:07 +0300

    Add Classified Ads/jobline-1_1_1/jobline-1_1_1/jobline.search.php

---
+++ Classified Ads/jobline-1_1_1/jobline-1_1_1/jobline.search.php
@@ -0,0 +1,12 @@
+<?php
+// Joblins Search addon script
+// Copyright 2004 Olle Johansson
+// Released under the GNU General Public License
+
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
+
+// Read configuration file
+include_once("$mosConfig_absolute_path/components/com_jobline/configuration.php");
+
+
+?>
