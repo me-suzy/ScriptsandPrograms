86b7b020668546a4bea570c71ba780e622573921
me-suzy <me.suzana@gmail.com>
2025-08-11 10:12:43 +0300

    Add Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/language/chinese_big5/userlog.lang.php

---
+++ Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/language/chinese_big5/userlog.lang.php
@@ -0,0 +1,69 @@
+<?php // $Revision: 2.0 $
+
+
+
+/************************************************************************/
+
+/* phpAdsNew 2                                                          */
+
+/* ===========                                                          */
+
+/*                                                                      */
+
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+
+/* For more information visit: http://www.phpadsnew.com                 */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+
+
+// Set translation strings
+
+
+
+$GLOBALS['strDeliveryEngine']				= "µo°e¤ÞÀº";
+
+$GLOBALS['strMaintenance']				= "ºûÅ@";
+
+$GLOBALS['strAdministrator']				= "ºÞ²z­û";
+
+
+
+
+
+$GLOBALS['strUserlog'] = array (
+
+	phpAds_actionAdvertiserReportMailed 		=> "³q¹L¹q¤l¶l¥óµo°e³ø§iµ¹«È¤á{id}",
+
+	phpAds_actionPublisherReportMailed 		=> "³q¹L¹q¤l¶l¥óµo°e³ø§iµ¹µo§G¤H{id}",
+
+	phpAds_actionWarningMailed			=> "³q¹L¹q¤l¶l¥óµo°e¶µ¥Ø{id}°±¥ÎÄµ§i",
+
+	phpAds_actionDeactivationMailed			=> "³q¹L¹q¤l¶l¥óµo°e¶µ¥Ø{id}°±¥Î³qª¾",
+
+	phpAds_actionPriorityCalculation		=> "­«·s­pºâÀu¥ýÅv",
+
+	phpAds_actionPriorityAutoTargeting		=> "­«·s­pºâ¶µ¥Ø¥Ø¼Ð",
+
+	phpAds_actionDeactiveCampaign			=> "¶µ¥Ø{id}¤w¸g°±¥Î",
+
+	phpAds_actionActiveCampaign			=> "¶µ¥Ø{id}¤w¸g±Ò¥Î",
+
+	phpAds_actionAutoClean				=> "¸ê®Æ®w¦Û°Ê²M²z"
+
+);
+
+
+
+?>
