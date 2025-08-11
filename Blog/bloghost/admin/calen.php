58b626468da72c3606239c5bb01bf13c8cedf349
me-suzy <me.suzana@gmail.com>
2025-08-11 11:00:30 +0300

    Add Blog/bloghost/admin/calen.php

---
+++ Blog/bloghost/admin/calen.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<link rel="stylesheet" href="style.css" type="text/css">
+<center><table border='0' width=100%><tr><td valign='top' width=30%>
+<?PHP
+include "connect.php";
+$blogadmin=$_SESSION['blogadmin'];
+$getadmin="SELECT * from bl_admin where username='$blogadmin'";
+$getadmin2=mysql_query($getadmin) or die("Cannot get admin");
+$getadmin3=mysql_fetch_array($getadmin2);
+if($getadmin3['status']>=1)
+{
+   include "left.php";
+   print "</td>";
+   print "<td valign='top' width=70%>";
+   print "<table class='maintable'><tr class='headline'><td><center>Edit calendar</center></td></tr>";
+   print "<tr class='mainrow'><td>";
+   print "<center>Click the date to edit reminders for that date.<br><br></center><br>";
+   include "calendar.php";
+   print "</td></tr></table>";
+}
+else
+{
+  print "Not logged in.";
+  print "</td></tr></table>";
+ 
+
+}
+?>
