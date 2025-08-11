d69777664e3bec93c263275ccbf87b5705561ef3
me-suzy <me.suzana@gmail.com>
2025-08-11 10:22:47 +0300

    Add Blog/aoblogger/config.php

---
+++ Blog/aoblogger/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbhost    = "localhost";
+$dbuser    = "####";
+$dbpass    = "####";
+$dbname    = "####";
+
+$db = mysql_connect($dbhost, $dbuser, $dbpass)
+or die("<b>Error:</b> Failed to connect to database");
+
+mysql_select_db($dbname, $db)
+or die("<b>Error:</b> Failed to select database");
+
+$filename = "ppppp.php";
+
+?>
