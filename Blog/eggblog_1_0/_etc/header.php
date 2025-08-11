45a8aedad999572af33abcdcd403a158b74e2837
me-suzy <me.suzana@gmail.com>
2025-08-11 11:42:33 +0300

    Add Blog/eggblog_1_0/_etc/header.php

---
+++ Blog/eggblog_1_0/_etc/header.php
@@ -0,0 +1,51 @@
+<?
+$eggblog_release = "v1.0";
+if (file_exists("../install.php")) {
+  echo "<p><b>You MUST delete the install file, install.php.</b></p>\n";
+  exit;
+}
+session_start();
+require_once("../_etc/config.inc.php");
+require_once("../_etc/mysql.php");
+
+if (!isset($_SESSION[eggblog])) {
+  if (isset($_COOKIE[eggblog_u])) {
+    if (isset($_COOKIE[eggblog_p])) {
+      $uid = mysql_result(mysql_query("SELECT username FROM eggblog_members WHERE md5(username)='$_COOKIE[eggblog_u]' AND md5(password)='$_COOKIE[eggblog_p]'"),0);
+      $pid = mysql_result(mysql_query("SELECT password FROM eggblog_members WHERE md5(username)='$_COOKIE[eggblog_u]' AND md5(password)='$_COOKIE[eggblog_p]'"),0);
+      if (strlen($uid) > 0) {
+        $_SESSION[eggblog] = $uid;
+      }
+    }
+  }
+}
+require_once("../_etc/config.inc.php");
+require_once("../_etc/mysql.php");
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang="EN" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title><?=$eggblog_title?> | <?=$eggblog_subtitle?></title>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta http-equiv="content-style-type" content="text/css" />
+	<meta http-equiv="pragma" content="no-cache" />
+	<meta http-equiv="cache-control" content="no-cache" />
+
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="alternate" href="<?=$eggblog_url?>/rss/blogs.php" type="application/rss+xml" title="rss" />
+	<link rel="stylesheet" href="../_etc/_<?=$eggblog_css?>.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="../_etc/_print.css" type="text/css" media="print" />
+	<style type="text/css" media="screen">@import "../_etc/_layout.css";</style>
+	<script type="text/javascript" src="../_etc/global.js"></script>
+</head>
+
+<body>
+
+<div id="container">
+	<div id="header">
+		<h1><a href="../home/index.php"><?=$eggblog_title?></a></h1>
+		<h2><a href="../home/index.php"><?=$eggblog_subtitle?></a></h2>
+	</div>
+
+	<div id="main">
