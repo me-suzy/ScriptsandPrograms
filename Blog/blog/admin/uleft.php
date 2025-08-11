b0facae4a02f625eb00d2efd4c87648ce4c143b2
me-suzy <me.suzana@gmail.com>
2025-08-11 10:49:40 +0300

    Add Blog/blog/admin/uleft.php

---
+++ Blog/blog/admin/uleft.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+?>
+<link rel="stylesheet" href="style.css" type="text/css">
+<center><table border='0' width=100%><tr><td valign='top' width=30%>
+<?PHP
+include "connect.php";
+$blogadmin=$_SESSION['blogadmin'];
+$getadmin="SELECT * from bl_admin where username='$blogadmin'";
+$getadmin2=mysql_query($getadmin) or die("Cannot get admin");
+$getadmin3=mysql_fetch_array($getadmin2);
+if($getadmin3['status']==3)
+{
+   include "left.php";
+   print "</td>";
+   print "<td valign='top' width=70%>";
+   print "<table class='maintable'><tr class='headline'><td><center>Main Admin</center></td></tr>";
+   print "<tr class='mainrow'><td>";
+   if(isset($_POST['submit']))
+
+   {
+    
+      $lblock=$_POST['leftblock'];
+      $left="UPDATE `bl_left` SET `left` = '$lblock' ";
+      mysql_query($left) or die(mysql_error());
+      print "Left block edited.";
+
+   }
+   else
+   {  
+      $getleftblock="SELECT * from bl_left";
+      $getleftblock2=mysql_query($getleftblock) or die("Could not get left block");
+      $getleftblock3=mysql_fetch_array($getleftblock2);
+      print "<form action='uleft.php' method='post'>";
+      print "Put custom left block here(HTML allowed):<br>";
+      print "<textarea name='leftblock' rows='5' cols='40'>$getleftblock3[left]</textarea><br>";
+      print "<input type='submit' name='submit' value='submit'></form>";
+
+
+   }
+   print "</td></tr></table>";
+}
+else
+{
+  print "Not logged in.";
+  print "</td></tr></table>";
+ 
+
+}
+?>
