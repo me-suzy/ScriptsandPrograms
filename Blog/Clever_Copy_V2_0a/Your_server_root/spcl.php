a342d7d765f8589910e3a9636a094ebb144671f3
me-suzy <me.suzana@gmail.com>
2025-08-11 11:24:26 +0300

    Add Blog/Clever_Copy_V2_0a/Your_server_root/spcl.php

---
+++ Blog/Clever_Copy_V2_0a/Your_server_root/spcl.php
@@ -0,0 +1,18 @@
+<?php
+function pbmdd($strg){
+$alpha_array = array('Y','D','U','R','P','S','B','M','A','T','H');
+$reg = base64_decode($strg);
+list($reg,$letter) = split("\+",$reg);
+for($i=0;$i<count($alpha_array);$i++)
+   {
+   if($alpha_array[$i] == $letter)
+   break;
+}
+for($x=1;$x<=$i;$x++)
+   {
+   $reg=base64_decode($reg);
+}
+$reg = stripslashes($reg);
+return $reg;
+}
+?>
