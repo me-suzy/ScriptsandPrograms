6ffd11c3ce02b4e2b79fb4cff894c3d9f929f02b
me-suzy <me.suzana@gmail.com>
2025-08-11 10:02:01 +0300

    Add Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/hebrew/userlog.lang.php

---
+++ Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/hebrew/userlog.lang.php
@@ -0,0 +1,69 @@
+<?php // $Revision: 2.0.2.5 $
+
+
+
+/************************************************************************/
+
+/* phpAdsNew 2                                                          */
+
+/* ===========                                                          */
+
+/*                                                                      */
+
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+
+/* For more information visit: http://www.phpadsnew.com                 */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+
+
+// Set translation strings
+
+
+
+$GLOBALS['strDeliveryEngine']				= "îðåò äôöä";
+
+$GLOBALS['strMaintenance']					= "úçæå÷ä";
+
+$GLOBALS['strAdministrator']				= "àçøàé";
+
+
+
+
+
+$GLOBALS['strUserlog'] = array (
+
+	phpAds_actionAdvertiserReportMailed 	=> "ãåç ìîôøñí {id} ðùìç áàéîééì",
+
+	phpAds_actionPublisherReportMailed 		=> "ãåç ìîôéõ{id} ðùìç áàéîééì",
+
+	phpAds_actionWarningMailed				=> "àæäøú áéèåì ìîòøëú ôøñåí {id} ðùìçä áàéîééì",
+
+	phpAds_actionDeactivationMailed			=> "äåãòú áéèåì ìîòøëú äôøñåí{id} ðùìçä áàéîééì",
+
+	phpAds_actionPriorityCalculation		=> "÷ãéîåú ùå÷ììä îçãù",
+
+	phpAds_actionPriorityAutoTargeting		=> "îèøåú ÷îôééï äåòøëå îçãù",
+
+	phpAds_actionDeactiveCampaign			=> "÷îôééï {id} ùåú÷",
+
+	phpAds_actionActiveCampaign				=> "÷îôééï{id} äåôòì",
+
+		phpAds_actionAutoClean					=> "ðé÷åé àåèåîèé ùì áñéñ äðúåðéí"
+
+);
+
+
+
+?>
