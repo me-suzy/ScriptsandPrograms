d04d20af223935ffd56f96a89682a1e476158eea
me-suzy <me.suzana@gmail.com>
2025-08-11 10:07:25 +0300

    Add Ad Management/phpLedAds-2.2/ledads/dev/load_test_js.php

---
+++ Ad Management/phpLedAds-2.2/ledads/dev/load_test_js.php
@@ -0,0 +1,8 @@
+<?php
+	for($i = 0; $i < 500; $i++) {
+		?>
+			<script src="/dev/ledads/ledad_js.php?<?=$i++?>"></script>
+			<script src="/dev/ledads/ledad_js.php?<?=$i++?>"></script><br>
+		<?
+	}
+?>
