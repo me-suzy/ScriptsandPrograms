4171db9210a90bd6e954d6eafac1905cddf0afa3
me-suzy <me.suzana@gmail.com>
2025-08-11 11:02:36 +0300

    Add Blog/bloghost/admin/index.php

---
+++ Blog/bloghost/admin/index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<link rel="stylesheet" href="style.css" type="text/css">
+<center><table border='0' width=100%><tr><td valign='top' width=30%>
+<?PHP
+include "connect.php";
+include "var.php";
+$blogadmin=$_SESSION['blogadmin'];
+$getadmin="SELECT * from bl_admin where username='$blogadmin'";
+$getadmin2=mysql_query($getadmin) or die("Cannot get admin");
+$getadmin3=mysql_fetch_array($getadmin2);
+if($getadmin3['status']>=1)
+{
+   include "left.php";
+   print "</td>";
+   print "<td valign='top' width=70%>";
+   print "<table class='maintable'><tr class='headline'><td><center>Main Admin</center></td></tr>";
+   print "<tr class='mainrow'><td>";
+   print "Welcome to your blog's admin panel. From here you can customize what options to show, post entries, edit comments, administrate your guestbook and poll, and edit and change your resume and calender. Your Blog page is at http://www.ferretfusion.com/members.php?membername=$blogadmin";
+   print "</td></tr></table>";
+}
+else
+{
+  print "Not logged in.";
+  print "</td></tr></table>";
+ 
+
+}
+?>
