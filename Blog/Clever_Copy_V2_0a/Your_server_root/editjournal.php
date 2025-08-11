9ea7c7f79a4d9fb933254dc601c5a7e2015c3577
me-suzy <me.suzana@gmail.com>
2025-08-11 11:18:18 +0300

    Add Blog/Clever_Copy_V2_0a/Your_server_root/editjournal.php

---
+++ Blog/Clever_Copy_V2_0a/Your_server_root/editjournal.php
@@ -0,0 +1,206 @@
+<?php
+session_start();
+include "admin/connect.inc";
+include "admin/languages/default.php";
+include "languages/default.php";
+include "antihack.php";
+$getprefs="SELECT * from CC_prefs";
+$getprefs2=mysql_query($getprefs) or die($no_preferences_error);
+$getprefs3=mysql_fetch_array($getprefs2);
+$style = $getprefs3[personality];
+$siteaddress = $getprefs3[siteaddress];
+$siteaddress = "$siteaddress/";
+$getblocks="SELECT * from CC_block_names";
+$getblocks2=mysql_query($getblocks) or die($no_blocks_error);
+$getblocks3=mysql_fetch_array($getblocks2);
+?>
+<head><title><?php echo $getprefs3[title]; ?></title>
+<link rel="stylesheet" href="<?php echo $style; ?>" type="text/css">
+</head>
+<!-- Start Avanti Web Stats tracking code - copyright (c) 2004 Liquid Frog Software - www.liquidfrog.com -->
+<script src="<?$siteaddress?>stats/script.js" language="JavaScript"></script>
+<noscript><img src="<?$siteaddress?>stats/script.php?image=1&javascript=false"></noscript>
+</html>
+<?php
+@mysql_query("UPDATE CC_prefs SET counter=counter+1");
+//End of Avanti Web Stats tracking code-->
+if(isset($_SESSION['cuser']))
+   {
+   $cuser=$_SESSION['cuser'];
+   $getuser="SELECT * from CC_users WHERE username='$cuser'";
+   $getuser2=mysql_query($getuser) or die($no_login_error);
+   $getuser3=mysql_fetch_array($getuser2);
+   if ($getuser3[user_site_name]==""){
+        $visitor_name = $guest_name_label;
+   }else{
+        $visitor_name =  $getuser3[user_site_name];
+   }
+   include "layout.php";
+   include "header.php";
+   echo "<table border='0' width=100%>";
+   echo "<tr><td valign='top' width = '181'>";
+   echo "<img src=images/seperator.gif border='0' width='181' height='1'>";
+   $query =  ("SELECT * FROM CC_blocks ORDER By blockposition ASC") or die($no_blocks_found_error);
+   $result = mysql_query($query);
+   if ((isset($_SESSION['cuser'])) || (isset($_SESSION['cadmin']))){
+   while($row = mysql_fetch_array($result)){
+       $theblock = $row["block_file"];
+       if (isset($_SESSION['cuser'])){
+          if ((($row["side"]=="0") && ($row["view"]=="1")) || (($row["side"]=="0") && ($row["view"]=="0"))){
+                  if ($row["block_file"] == "loginblock.php")
+                  {
+                  echo "";
+             }else{
+                   include "$theblock";
+                   echo "<br>";
+             }
+          }
+       }
+       elseif (isset($_SESSION['cadmin'])){
+               if ((($row["side"]=="0")&& ($row["view"]=="2")) || (($row["side"]=="0") && ($row["view"]=="1")) || (($row["side"]=="0") && ($row["view"]=="0"))){
+                  if ($row["block_file"] == "loginblock.php")
+                       {
+                       echo "";
+                  }else{
+                  include "$theblock";
+                  echo "<br>";
+               }
+             }
+         }
+    }
+ }
+ else{
+    while($row = mysql_fetch_array($result)){
+        $theblock = $row["block_file"];
+        if (($row["side"]=="0") && ($row["view"]=="0")){
+             if ($row["block_file"] == "loginblock.php")
+                  {
+                  echo "";
+             }else{
+                   include "$theblock";
+                   echo "<br>";
+             }
+        }
+   }
+ }
+ echo "<td valign='top' ><center>";
+ echo "<img src=images/seperator.gif border='0' width='10' height='1'>";
+ if($getprefs3[showseparator]==1){echo "<hr color=$getprefs3[separatorlinecolor] size = '1'>";}
+ if ($visitor_name == $guest_name_label){
+   echo "$getprefs3[title] $welcomes_visitor_label - $visitor_name"; }
+ else {
+   echo  "$welcome_back_user_label $visitor_name"; }
+ if($getprefs3[showwelcome_message]==1){
+   include "welcome.php";}
+ if($getprefs3[shownewsticker]==1){
+   include "ticker.php";}
+ echo "<table border='0' cellspacing='0' style='border-collapse: collapse; border: 1px solid $getprefs3[blockbordercolor]' width='100%'>";
+ echo "<tr><td colspan='3'><center><b>$edit_diary_heading_label</b><br><br></center></td></tr>";
+ echo "<tr><td colspan = '3' valign='top' align = 'left'>";
+ if($_SESSION['cuser'])
+ {
+  $ID=$_GET['ID'];
+  $journal="SELECT * FROM CC_journal WHERE user = '$cuser' AND id = '$ID'";
+  $row=mysql_query($journal);
+  echo "<form action='editjournal.php?op=edit' method='post' name='form'>";
+  while($journalvalues=mysql_fetch_array($row))
+  {
+   $title = $journalvalues['title'];
+   echo "<center>$edit_this_diary_title_label<br>";
+   echo "<textarea name='title' rows='1' cols='100'> $journalvalues[title]</textarea><br><br>";
+   echo "$edit_diary_entry_label<br>";
+   echo "<textarea name='entry' rows='5' cols='100'> $journalvalues[entry]</textarea><br><br>";
+   echo "$who_can_see_diary_label<br>";
+   echo "<select name='public'>";
+   echo "<option value='1'>$public_label</option>";
+   echo "<option value='0'>$private_label</option>";
+   echo "</select><br><br><br>";
+   echo "<input type='hidden' name='id' value = '$ID'>";
+  }
+  echo "<center><input type='submit' name='submit' value='$confirm_diary_button_label'class = 'buttons'></form>";
+ }else{
+   echo "<center><b>$not_authorised_error_label</b>";
+   echo "<meta http-equiv='refresh' content='2;URL=login.php?'>";
+   echo "<center>$if_you_see_label <a href='index.php'>$click_here_label</a>";
+   exit;
+ }
+ echo "<center><form><input type='button' class = 'buttons' value='$back_label' onclick='history.back()'></form>";
+ echo "</center>";
+ switch( $_GET[ "op" ] )
+ {
+ case "edit":
+ $ID = antihaxmjr($_POST['id']);
+ $entry = antihaxmjr($_POST['entry']);
+ $title = antihaxmjr($_POST['title']);
+ $public = antihaxmjr($_POST['public']);
+ @mysql_query ("UPDATE CC_journal SET title='$title',entry='$entry', public='$public' WHERE id ='$ID' AND user = '$visitor_name'");
+ $error_msg .= mysql_error();
+ if ($error_msg == "")
+ {
+      echo "<center><b><br>$diary_edit_ok_label<br></b>";
+      echo "<meta http-equiv='refresh' content='2;URL=myjournal.php?'>";
+      echo "<center>$if_you_see_label <a href='myjournal.php'>$click_here_label</a>";
+ }else{
+      echo "<center>$there_was_problem_label - $error_msg";
+      echo "<center>$if_you_see_label <a href='myjournal.php'>$click_here_label</a>";
+ }
+ break;
+ }
+ echo "</td></tr></table>";
+ if($getprefs3[showrightblocks]==1){
+ echo "<td valign='top' width='181'>";
+ echo "<img src=images/seperator.gif border='0' width='181' height='1'>";
+ $query =  ("SELECT * FROM CC_blocks ORDER By blockposition ASC") or die($no_blocks_found_error);
+ $result = mysql_query($query);
+ if ((isset($_SESSION['cuser'])) || (isset($_SESSION['cadmin']))){
+ while($row = mysql_fetch_array($result)){
+       $theblock = $row["block_file"];
+       if (isset($_SESSION['cuser'])){
+          if ((($row["side"]=="1") && ($row["view"]=="1")) || (($row["side"]=="1") && ($row["view"]=="0"))){
+                  if ($row["block_file"] == "loginblock.php")
+                  {
+                  echo "";
+             }else{
+                   include "$theblock";
+                   echo "<br>";
+             }
+          }
+       }
+       elseif (isset($_SESSION['cadmin'])){
+               if ((($row["side"]=="1")&& ($row["view"]=="2")) || (($row["side"]=="1") && ($row["view"]=="1")) || (($row["side"]=="1") && ($row["view"]=="0"))){
+                  if ($row["block_file"] == "loginblock.php")
+                       {
+                       echo "";
+                  }else{
+                  include "$theblock";
+                  echo "<br>";
+               }
+             }
+         }
+    }
+ }
+ else{
+    while($row = mysql_fetch_array($result)){
+        $theblock = $row["block_file"];
+        if (($row["side"]=="1") && ($row["view"]=="0")){
+             if ($row["block_file"] == "loginblock.php")
+                  {
+                  echo "";
+             }else{
+                   include "$theblock";
+                   echo "<br>";
+             }
+        }
+   }
+  }
+ }
+ include "endlayout.php";
+}
+else
+   {
+   echo "<center><b>$not_authorised_error_label</b>";
+   echo "<meta http-equiv='refresh' content='2;URL=login.php?'>";
+   echo "<center>$if_you_see_label <a href='index.php'>$click_here_label</a>";
+   exit;
+}
+?>
