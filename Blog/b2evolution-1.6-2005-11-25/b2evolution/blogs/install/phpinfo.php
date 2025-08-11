b09c3b11243ef2102ce23f991e3c107490a3b7ca
me-suzy <me.suzana@gmail.com>
2025-08-11 10:28:33 +0300

    Add Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/install/phpinfo.php

---
+++ Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/install/phpinfo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is simply displayind standard phpinfo
+ *
+ * b2evolution - {@link http://b2evolution.net/}
+ * Released under GNU GPL License - {@link http://b2evolution.net/about/license.html}
+ * @copyright (c)2003-2005 by Francois PLANQUE - {@link http://fplanque.net/}
+ *
+ * @package install
+ */
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<?php phpinfo();?>
+
+</body>
+</html>
