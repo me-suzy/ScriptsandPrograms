01934d001c3a370bc33b359e9f3b16056eaf4394
me-suzy <me.suzana@gmail.com>
2025-08-11 10:19:17 +0300

    Add Ad Management/PixelAdRaptor-0.4_PHP4/PixelAdRaptor-0.4/core/request-mapper.php

---
+++ Ad Management/PixelAdRaptor-0.4_PHP4/PixelAdRaptor-0.4/core/request-mapper.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *
+ * Request mapper
+ *
+ * @author Karolis Tamutis
+ * @date 2005-10-08
+ * @file request-mapper.php
+ *
+ * Copyright (C) 2005  Karolis Tamutis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+ */ 
+ 
+if (!defined('MAIN')) {
+
+    die('No direct access.');
+    
+}
+
+if (isset($_GET['page']) && is_file('pages/'.$_GET['page'].'.php')) {
+
+    require_once('pages/'.$_GET['page'].'.php');    
+
+} else {
+
+    header('location: '.$config['MAIN']);
+
+}
+
+?>
