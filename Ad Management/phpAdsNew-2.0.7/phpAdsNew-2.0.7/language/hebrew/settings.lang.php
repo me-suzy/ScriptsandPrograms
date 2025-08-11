c11ac17504f15e0f0b851689495a8115393d53b5
me-suzy <me.suzana@gmail.com>
2025-08-11 10:01:59 +0300

    Add Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/hebrew/settings.lang.php

---
+++ Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/hebrew/settings.lang.php
@@ -0,0 +1,653 @@
+<?php // $Revision: 2.1.2.5 $
+
+
+
+/************************************************************************/
+
+/* phpAdsNew 2                                                          */
+
+/* ===========                                                          */
+
+/*                                                                      */
+
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+
+/* For more information visit: http://www.phpadsnew.com                 */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+
+
+
+
+// Installer translation strings
+
+$GLOBALS['strInstall']				= "äú÷ï";
+
+$GLOBALS['strChooseInstallLanguage']		= "áçø àú ùôú ääú÷ðä";
+
+$GLOBALS['strLanguageSelection']		= "áçéøú ùôä";
+
+$GLOBALS['strDatabaseSettings']			= "÷áéòåú áñéñ ðúåðéí";
+
+$GLOBALS['strAdminSettings']			= "÷áéòåú îðäì";
+
+$GLOBALS['strAdvancedSettings']			= "÷áéòåú îú÷ãîåú";
+
+$GLOBALS['strOtherSettings']			= "÷áéòåú àçøåú";
+
+
+
+$GLOBALS['strWarning']				= "àæäøä";
+
+$GLOBALS['strFatalError']			= "÷øúä ùâéàä âåøìéú";
+
+$GLOBALS['strUpdateError']			= "çìä ùâéàä áæîï äòãëåï";
+
+$GLOBALS['strUpdateDatabaseError']	= "îñéáåú ìà áøåøåú òãëåï áñéñ äðúåðéí ìà äöìéç. äøøê äîåîìöú ìäîùéê äéà ììçåõ <b>ðñä òãëåï îçãù</b> ëãé ìðñåú åìú÷ï áòéåú àôùøéåú. àí àúä áèåç ùáòéåú àìå ìà éëåìåú ìôâåò áúô÷åã ".$phpAds_productname." àúä éëåì ììçåõ <b>äúòìí îùâéàåú</b> ëãé ìäîùéê. äúòìîåú îùâéàåú àìå àôùø ùúéåø áòéä çîåøä åæä ìà îåîìõ!";
+
+$GLOBALS['strAlreadyInstalled']			= $phpAds_productname." ëáø îåú÷ðú áîòøëú æå. àí àúä øåöä ìòöá àåúä ìê ì<a href='settings-index.php'>÷éáòåú îîù÷</a>";
+
+$GLOBALS['strCouldNotConnectToDB']		= "ìà éëåì ìäúçáø ìáñéñ äðúåðéí, àðà áãå÷ îçãù àú ä÷áéòåú ùøùîú.";
+
+$GLOBALS['strCreateTableTestFailed']		= "ìùí äîùúîù ùøùîú àéï îñôé÷ äøùàä ìéöéøú àå òãëåï îáðä áñéñ äðúåðéí, àðà öåø ÷ùø òí äàçøàé áùøú.";
+
+$GLOBALS['strUpdateTableTestFailed']		= "ìùí äîùúîù ùøùîú àéï ãé äøùàä ìòãëåï áñéñ äðúåðéí. àðà öåø ÷ùø òí äàçøàéí.";
+
+$GLOBALS['strTablePrefixInvalid']		= "÷éãåîú äèáìàåú îëéìä àåúéåú ôñåìåú.";
+
+$GLOBALS['strTableInUse']			= "áñéñ äðúåðéí ùøùîú ðîöà ëáø áùéîåù ùì ".$phpAds_productname.", àðà áçø á÷éãåîú èáìàåú àçøú, àå ÷øà àú äîãøéê ìâáé äåøàåú ùãøåâ.";
+
+$GLOBALS['strTableWrongType']		= "ñåâ äèáìä ùáçøú àéðå ðúîê áäú÷ðú ".$phpAds_dbmsname."ùìê";
+
+$GLOBALS['strMayNotFunction']			= "ìôðé ùàúä îîùéê, àðà ú÷ï àú äáòéä äàôùøéåú äàìå:";
+
+$GLOBALS['strFixProblemsBefore']		= "äôøéè(éí) äáà(éí) ãåøùéí úé÷åï ìôðé ùðéúï éäéä ìäú÷éï àú ".$phpAds_productname.". àí éù ìê ùàìä ìâáé ùéàä æå, àðà ÷øà àú ä<i>îãøéê ìàãîéðéñèøèåø</i>, äðîöà áçáéìä ùäåøãú.";
+
+$GLOBALS['strFixProblemsAfter']			= "àí àéï áàôùøåúê ìú÷ï àú äáòéä ùäåöâä îòìä, ðà öåø ùø òí äàçøàé ùì äùøú ùòìéå àúä îðñä ìäú÷éï àú ".$phpAds_productname.". àôùø ùäåà éåëì ìòæåø ìê àå ìëååï ìôúøåï.";
+
+$GLOBALS['strIgnoreWarnings']			= "äúòìí îàæàøåú";
+
+$GLOBALS['strWarningPHPversion']		= $phpAds_productname." ðãøù PHP 4.0 àå âéøñä âáåää éåúø ìùí úô÷åã ðëåï. àúä îùúîù ëòú áâéøñä {php_version}.";
+
+$GLOBALS['strWarningRegisterGlobals']		= "÷åðôéâåøöéú  PHP ùì äîùúðä <B>register_globals</B> öøéëä ìäéåú îåôòìú.";
+
+$GLOBALS['strWarningMagicQuotesGPC']		= "÷åðôéâåøöéú  PHP ùì äîùúðä  <B>magic_quotes_gpc</B>  öøéëä ìäéåú îåôòìú.";
+
+$GLOBALS['strWarningMagicQuotesRuntime']	= "÷åðôéâåøöéú  PHP ùì äîùúðä  <B>magic_quotes_runtime</B>  öøéëä ìäéåú îåôòìú.";
+
+$GLOBALS['strWarningFileUploads']		= "÷åðôéâåøöéú  PHP ùì äîùúðä  <B>file_uploads</B>  öøéëä ìäéåú îåôòìú.";
+
+$GLOBALS['strConfigLockedDetected']		= $phpAds_productname." æéää ùä÷åáõ <b>config.inc.php</b> îåâï îëúéáä.<br> ìà úåëì ìäîùéê òã ùúùðä àú àéùåø äâéùä ì÷åáõ æä. <br>÷øà àú äúéòåã äîöåøó àí àéðê éåãò ëéöã ìòùåú æàú.";
+
+$GLOBALS['strCantUpdateDB']  			= "ìà ðéúï ìòãëï àú áñéñ äðúåðéí ëòúõ àí äçìèú ìäîùéê, ëì äáàðøéí ä÷ééîéí, ñèèéñèé÷ä åîôøñîéí éîç÷å.";
+
+$GLOBALS['strIgnoreErrors']			= "äúòìí îùâéàåú";
+
+$GLOBALS['strRetryUpdate']			= "ðñä òãëåï îçãù";
+
+$GLOBALS['strTableNames']			= "ùîåú äèáìàåú";
+
+$GLOBALS['strTablesPrefix']			= "÷éãåîú ùì èáìàåú";
+
+$GLOBALS['strTablesType']			= "ñåâé äèáìàåú";
+
+
+
+$GLOBALS['strInstallWelcome']			= "áøåëéí äáàéí ì".$phpAds_productname;
+
+$GLOBALS['strInstallMessage']			= "ìôðé äùéîåù á-".$phpAds_productname." éù ìòöá àú úô÷åãé äúåëðä åéöåø áñéñ ðúåðéí.<br>ìçõ <b>äîùê</b> ìäú÷ãîåú.";
+
+$GLOBALS['strInstallSuccess']			= "<b>ääú÷ðä ùì ".$phpAds_productname." äñúééîä áøâò æä.</b><br><br>ëãé ù- ".$phpAds_productname." úúô÷ã ðëåï òìéê âí ìååãà ù÷åáõ äúçæå÷ä éøåõ ëì ùòä. îéãò ðåñó àåãåú ðåùà æä ðéúï ìîöåà áúéòåã äîöåøó. <br><br>ìçõ <b>äîùê</b> ëãé ìäâéò ìòîåã äòéöåá/÷éðôåâ, äéëï ùúåëì ì÷áåò ðúåðéí ðåñôéí. àðà àì úùëç ìðòåì àú ä÷åáõ <B> config.inc.php</B> áøâò ùñééîú, ëãé ìîðåò ôâéòåú åäèøãåú áùøú.";
+
+$GLOBALS['strUpdateSuccess']			= "<b>äòãëåï ùì ".$phpAds_productname." äñúééí áäöìçä.</b><br><br>ëãé ù-".$phpAds_productname." úúô÷ã ðëåï òìéê âí ìäáèéç ù÷åáõ äúçæå÷ä éøåõ ëì ùòä (òã òúä äåà ðãøù ìøåõ ôòí áéåí) îéãò ðåñó áðåùà æä ðéúï ìîöåà áúéòåã äîöåøó. <br><br>ìçõ <b>äîùê</b> ëãé ìäâéò ìòîåã îîù÷ ääðäìä. àðà àì úùëç ìðòåì àú ä÷åáõ  <B>config.inc.php</B> ìîðéòú áòéåú àáèçä.";
+
+$GLOBALS['strInstallNotSuccessful']		= "<b>ääú÷ðä ùì ".$phpAds_productname."ìà äöìéçä</b><br><br>çì÷éí îñåéîéí îúäìéê ääú÷ðä ìà äåùìîå. àôùø ùáòéåú àìå äï æîðéåú áìáã, áî÷øä æä ôùåè ìçõ <b>äîùê</b> åúçæåø ìùìá äøàùåï ùì úäìéê ääú÷ðä. àí àúä øåöä ìãòú éåúø îä àåîøåú äåãòåú äùâéàä îèä, åëéúã ìôúåø àåúï, àðà äéååòõ áúéòåã äîöåøó.";
+
+$GLOBALS['strErrorOccured']			= "äùâéàä äáàä ÷øúä:";
+
+$GLOBALS['strErrorInstallDatabase']		= "îáðä áñéñ äðúåðéí ìà éëì ìäéååöø.";
+
+$GLOBALS['strErrorInstallConfig']		= "÷åáõ ä÷åðôéâåøöéä àå áñéñ äðúåðéí ìà éëìå ìäúòãëï.";
+
+$GLOBALS['strErrorInstallDbConnect']		= "ìà ðéúï äéä ìäúçáø ìáñéñ äðúåðéí.";
+
+
+
+$GLOBALS['strUrlPrefix']			= "÷éãåîú URL";
+
+
+
+$GLOBALS['strProceed']				= "äîùê &gt;";
+
+
+
+$GLOBALS['strInvalidUserPwd']			= "ùí îùúîù àå ñéñîà ôñåìéí";
+
+
+
+$GLOBALS['strUpgrade']				= "òãëåï";
+
+$GLOBALS['strSystemUpToDate']			= "äîòøëú ùìê îåëðä åîòåãðú, ìà ðãøù òãëåï áøâò æä. <br>ìçõ òì <b>äîùê</b> ëãé ìäâéò ìòîåã äáéú.";
+
+$GLOBALS['strSystemNeedsUpgrade']		= "îáðä áñéñ äðúåðéí å÷åáõ ä÷åðôéâåøöéä æ÷å÷éí ìòãëåï ëãé ùäîòøëú úúô÷ã ðëåðä.<br>àðà äúàæø áñáìðåú ëéååï ùúäìéê äòãëåï éëåì ì÷çú ëîä ã÷åú.";
+
+$GLOBALS['strSystemUpgradeBusy']		= "úäìéê òãëåï äîòøëú áòéöåîå, àðà äîúï...";
+
+$GLOBALS['strSystemRebuildingCache']		= "áåðä àú æëøåï äîèîåï îçãù, àðà äîúï...";
+
+$GLOBALS['strServiceUnavalable']		= "äùéøåú àéðå àôùøé æîðéú. úäìéê òãëåï äîòøëú áòéöåîå";
+
+
+
+$GLOBALS['strConfigNotWritable']		= "÷åáõ ä-<B>config.inc.php</B> àéðå ðéúï ìëúéáä.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*********************************************************/
+
+/* Configuration translations                            */
+
+/*********************************************************/
+
+
+
+// Global
+
+$GLOBALS['strChooseSection']			= "áçø îçì÷ä";
+
+$GLOBALS['strDayFullNames'] 			= array("øàùåï","ùðé","ùìéùé","øáéòé","çîéùé","ùéùé","ùáú");
+
+$GLOBALS['strEditConfigNotPossible']    	= "ìà ðéúï ìùðåú ÷áéòåú àìå ëéååï ù÷åáõ ä÷åðôéâåøöéä ðòåì îñéáåú áèéçåúéåú.<br> "."àí áøöåðê ìòøåê ùéðåééí, òìéê ìùçøø ÷åáõ æä îðòéìä";
+
+$GLOBALS['strEditConfigPossible']		= "ðéúï ìòøåê àú ëì ä÷áéòåú ëéååï ù÷åáõ ä÷åðôéâåøöéä àéðå ðòåì.<br>ìîðéòú îçãì áèéçåúé àðà ðòì àú ä÷åáõ <B> config.inc.php</B>.";
+
+;
+
+
+
+
+
+
+
+// Database
+
+$GLOBALS['strDatabaseSettings']			= "÷áéòåú áñéñ ðúåðéí";
+
+$GLOBALS['strDatabaseServer']			= "ùøú áñéñ äðúåðéí";
+
+$GLOBALS['strDbLocal']				= "äúçáø ìùøú äî÷åîé áàîöòåú îòáøéí (sockets)"; 
+
+$GLOBALS['strDbHost']				= "äùøú äîàøç";
+
+$GLOBALS['strDbPort']				= "îñôø äîáåà ùì áñéñ äðúåðéí (port)";
+
+$GLOBALS['strDbUser']				= "ùí äîùúîù ááñéñ äðúåðéí";
+
+$GLOBALS['strDbPassword']			= "äñéñîà ùì áñéñ äðúåðéí";
+
+$GLOBALS['strDbName']				= "äùí ùì áñéñ äðúåðéí";
+
+
+
+$GLOBALS['strDatabaseOptimalisations']		= "ééèåá áñéñ äðúåðéí";
+
+$GLOBALS['strPersistentConnections']		= " äùúîù áçéáåø øöéó (áñéñ äðúåðéí úôåñ éåúø)";
+
+$GLOBALS['strInsertDelayed']			= " äùúîù áäùçìú ðúåï îåùäú (áñéñ äðúåðéí ôçåú øâéù ìðôéìä åùâéàåú)";
+
+$GLOBALS['strCompatibilityMode']		= " äùúîù áúàéîåú áñéñ ðúåðéí";
+
+$GLOBALS['strCantConnectToDb']			= " ìà îñåâì ìäúçáø ìáñéñ äðúåðéí";
+
+
+
+
+
+
+
+// Invocation and Delivery
+
+$GLOBALS['strInvocationAndDelivery']		= "÷áéòåú ùìéôä åäôöä ùì áàðøéí";
+
+
+
+$GLOBALS['strAllowedInvocationTypes']		= "ñåâé ÷øéàä îåúøéí";
+
+$GLOBALS['strAllowRemoteInvocation']		= " àôùø ÷øéàä îøåç÷ú";
+
+$GLOBALS['strAllowRemoteJavascript']		= " àôùø ÷øéàä îøåç÷ú òí ÷åã Javascript";
+
+$GLOBALS['strAllowRemoteFrames']		= " àôùø ÷øéàä îøåç÷ú ì÷åã îñâøåú";
+
+$GLOBALS['strAllowRemoteXMLRPC']		= " àôùø ÷øéàä îøåç÷ú òí ÷åã XML-RPC";
+
+$GLOBALS['strAllowLocalmode']			= " àôùø äôòìä î÷åîéú";
+
+$GLOBALS['strAllowInterstitial']		= " àôùø áàðøéí öôéí";
+
+$GLOBALS['strAllowPopups']			= " àôùø áàðøéí ÷åôöéí";
+
+
+
+$GLOBALS['strUseAcl']				= " äùúîù áäâáìåú úöåâä";
+
+
+
+$GLOBALS['strDeliverySettings']			= "÷áéòåú úôåöä";
+
+$GLOBALS['strCacheType']				= "ñåâ îèîåï äúôåöä";
+
+$GLOBALS['strCacheFiles']				= "÷áöéí";
+
+$GLOBALS['strCacheDatabase']			= "áñéñ ðúåðéí";
+
+$GLOBALS['strCacheShmop']				= "æëøåï îùåúó/Shmop";
+
+$GLOBALS['strCacheSysvshm']				= "æëøåï îùåúó/Sysvshm";
+
+$GLOBALS['strExperimental']				= "ðñéåðé";
+
+
+
+$GLOBALS['strKeywordRetrieval']			= "ùìéôä ìôé îéìåú îôúç";
+
+$GLOBALS['strBannerRetrieval']			= " ùéèú ùìéôú äáàðøéí";
+
+$GLOBALS['strRetrieveRandom']			= " ùìéôä à÷øàéú (áøéøú îçãì)";
+
+$GLOBALS['strRetrieveNormalSeq']		= " ùìéôä ñãøúéú øâéìä";
+
+$GLOBALS['strWeightSeq']			= " ùìéôä ñãøúéú îáåññ îù÷ì";
+
+$GLOBALS['strFullSeq']				= " ùìéôä ñãøúéú îìàä";
+
+$GLOBALS['strUseConditionalKeys']		= " äùúîù áîéìåú úðàé";
+
+$GLOBALS['strUseMultipleKeys']			= " äùúîù áøéáåé îéìåú îôúç";
+
+
+
+$GLOBALS['strZonesSettings']			= "ùìéôä àæåøéú";
+
+$GLOBALS['strZoneCache']			= " æëøåï îèîåï àæåøé. (éàéõ úöåâä ùì áàðøéí îáåññé àæåø.)";
+
+$GLOBALS['strZoneCacheLimit']			= " äæîï ùáéï òãëåðé æëøåï îèîåï (áùðéåú)";
+
+$GLOBALS['strZoneCacheLimitErr']		= " äæîï ùáéï òãëåðé æëøåï îèîåï çééá ìäéåú îñôø çéåáé";
+
+
+
+$GLOBALS['strP3PSettings']			= "ôåìéñåú ôøèéåú îñåâ P3P";
+
+$GLOBALS['strUseP3P']				= " äùúîù áôåìéñåú P3P";
+
+$GLOBALS['strP3PCompactPolicy']			= "ôåìéñú P3P ÷åîô÷èéú";
+
+$GLOBALS['strP3PPolicyLocation']		= "îé÷åí ôåìéñú ä-P3P";
+
+
+
+
+
+
+
+// Banner Settings
+
+$GLOBALS['strBannerSettings']			= "÷áéòåú áàðøéí";
+
+
+
+$GLOBALS['strAllowedBannerTypes']		= "ñåâé áàðøéí îåúøéí";
+
+$GLOBALS['strTypeSqlAllow']			= " àôùø áàðøéí î÷åîééí (SQL)";
+
+$GLOBALS['strTypeWebAllow']			= " àôùø áàðøéí î÷åîééí (Webserver)";
+
+$GLOBALS['strTypeUrlAllow']			= " àôùø áàðøéí çéöåðééí";
+
+$GLOBALS['strTypeHtmlAllow']			= " àôùø áàðøéí î÷åã HTML";
+
+$GLOBALS['strTypeTxtAllow']			= " àôùø áàðøéí è÷ñèåàìééí";
+
+
+
+$GLOBALS['strTypeWebSettings']			= "÷åðôéâåøöééú áàðø î÷åîé (äùøú)";
+
+$GLOBALS['strTypeWebMode']			= "ùéèú àéçñåï";
+
+$GLOBALS['strTypeWebModeLocal']			= "úé÷ééä î÷åîéú";
+
+$GLOBALS['strTypeWebModeFtp']			= "ùøú FTP çéöåðé";
+
+$GLOBALS['strTypeWebDir']			= "úé÷ééä î÷åîéú";
+
+$GLOBALS['strTypeWebFtp']			= "ùøú áàðøéí áúöåøú FTP";
+
+$GLOBALS['strTypeWebUrl']			= "ëúåáú URL öéáåøéú";
+
+$GLOBALS['strTypeFTPHost']			= "îàøç FTP";
+
+$GLOBALS['strTypeFTPDirectory']			= "úé÷ééú FTP";
+
+$GLOBALS['strTypeFTPUsername']			= "ùí îùúîù";
+
+$GLOBALS['strTypeFTPPassword']			= "ñéñîà";
+
+$GLOBALS['strTypeFTPErrorDir']			= "úé÷ééú äùøú àéðä ÷ééîú";
+
+$GLOBALS['strTypeFTPErrorConnect']		= "ìà ðéúï äéä ìäúçáø ìùøú ä-FTP, ùí äîùúîù àå äñéñîà ùâåééí";
+
+$GLOBALS['strTypeFTPErrorHost']			= "ùí ùøú ä-FTP àéðå ðëåï";
+
+$GLOBALS['strTypeFTPErrorDir']			= "ú÷ééú äîàøç àéðä ÷ééîú";
+
+$GLOBALS['strTypeFTPErrorConnect']		= "ìà ðéúï ìäúçáø ìùøú ä-FTP, ùí äîùúîù àå äñéñîà ùâåééí";
+
+$GLOBALS['strTypeFTPErrorHost']			= "ùí äùøú äîàøç àú ä-FTP ùâåé";
+
+$GLOBALS['strTypeDirError']				= "äúé÷ééä äî÷åîéú àéðä ÷ééîú";
+
+
+
+
+
+
+
+$GLOBALS['strDefaultBanners']			= "áàðøéí ëáøéøú îçãì çìåôéú";
+
+$GLOBALS['strDefaultBannerUrl']			= "ëúåáú URL ùì áàðø çìåôé";
+
+$GLOBALS['strDefaultBannerTarget']		= "ëúåáú URL ëîèøä çìåôéú";
+
+
+
+$GLOBALS['strTypeHtmlSettings']			= "àåôöéåú áàðø HTML";
+
+$GLOBALS['strTypeHtmlAuto']			= " ùðä àú ä÷åã àåèåîèéú ëãé ìàôùø îò÷á àçø ä÷ì÷åú.";
+
+$GLOBALS['strTypeHtmlPhp']			= " àôùø ééùåí áéèåéé PHP îúåê áàðøéí îñåâ HTML ";
+
+
+
+
+
+
+
+// Host information and Geotargeting
+
+$GLOBALS['strHostAndGeo']				= "îéãò ñô÷éú åîò÷á âéàåâøôé (Geotargeting)";
+
+$GLOBALS['strRemoteHosts']			= "ùøúéí îøåç÷éí";
+
+
+
+$GLOBALS['strReverseLookup']			= "ðñä ì÷áåò àú ñô÷éú äùéøåú ùì äîá÷ø àí äðúåï ìà îâéò îäùøú";
+
+$GLOBALS['strProxyLookup']				= "ðñä ì÷áåò àú ëúåáú ä-IP äàîéúéú ùì äîá÷ø àí äåà îùúîù áîàâø áéðééí (proxy).";
+
+
+
+$GLOBALS['strGeotargeting']				= "Geotargeting - îé÷åã âéàåâøôé";
+
+$GLOBALS['strGeotrackingType']			= "ñåâ îàâø îò÷á";
+
+$GLOBALS['strGeotrackingLocation'] 		= "îé÷åí îàâø îò÷á";
+
+$GLOBALS['strGeotrackingLocationError'] = "îàâø äðúåðéí ùì îò÷á âéàåâøôé àéðå ðîöà áîé÷åí ùðîñø";
+
+$GLOBALS['strGeoStoreCookie']			= "ùîåø àú äúåöàä á÷å÷é (cookie) ìäúéçñåú òúéãéú";
+
+
+
+
+
+
+
+// Statistics Settings
+
+$GLOBALS['strStatisticsSettings']		= "÷áéòåú ñèèéñèé÷ä";
+
+
+
+$GLOBALS['strStatisticsFormat']			= "úöåøú ñèèéñèé÷ä";
+
+$GLOBALS['strCompactStats']			= " äùúîù áñèèéñèé÷ä ÷åîô÷èéú";
+
+$GLOBALS['strLogAdviews']			= " úòã çùéôåú";
+
+$GLOBALS['strLogAdclicks']				= "úòã ä÷ì÷ä áëì ôòí ùäîá÷ø ìåçõ òì áàðø";
+
+$GLOBALS['strLogSource']				= "úòã àú ðúåðé äî÷åø äîåâãøéí áæîï äçùéôä";
+
+$GLOBALS['strGeoLogStats']				= "úòã áñèèéñèé÷ä àú äàøõ îîðä îâéò äîá÷ø";
+
+$GLOBALS['strLogHostnameOrIP']			= "úòã àú ñô÷éú äùéøåú àå ëúåáú ä-IP ùì äîá÷ø";
+
+$GLOBALS['strLogIPOnly']				= "úòã àú ëúåáú ä-IP ùì äîá÷ø àôéìå àí ùí äñô÷éú éãåò";
+
+$GLOBALS['strLogIP']					= "úòã àú ëúåáú ä-IP ùì äîá÷ø";
+
+$GLOBALS['strLogBeacon']			= " äùúîù áàúúéí ìúéòåã çùéôåú";
+
+
+
+
+
+$GLOBALS['strIgnoreHosts']				= "àì úúòã ñèèéñèé÷ä îîá÷øéí äîùúîùéí áàçã îîñôøé ä-IP àå ùîåú äîàøçéí äáàéí";
+
+$GLOBALS['strBlockAdviews']				= "àì úúòã çùéôåú àí äîá÷ø ëáø ðçùó ìáàðø äæä áîäìê îñôø äùðéåú äð÷åá.";
+
+$GLOBALS['strBlockAdclicks']			= "àì úúòã ä÷ì÷åú àí äîá÷ø ëáø ìçõ òì àåúå äáàðø áîäìê îñôø äùðéåú äð÷åá";
+
+
+
+$GLOBALS['strPreventLogging']			= "îðò äúçáøåú";
+
+$GLOBALS['strEmailWarnings']			= "àúøàä áàéîééì";
+
+$GLOBALS['strAdminEmailHeaders']		= "äåñó àú äëåúøú äáàä ìëì àéîééì ùéùìç òì éãé ".$phpAds_productname;
+
+$GLOBALS['strWarnLimit']				= "ùìç àúøàä ëàùø îñôø äçùéôåú äðåúø äéðå ôçåú îäð÷åá ëàï";
+
+
+
+$GLOBALS['strWarnAdmin']			= " ùìç äúøàú îðäì áëì ôòí ù÷îôéï îñåééí ìôðé ñéåîå";
+
+$GLOBALS['strWarnClient']			= " ùìç äúøàú îôøñí áëì ôòí ùä÷îôéï ùìå ìôðé ñéåí";
+
+$GLOBALS['strQmailPatch']			= " àôùø èìàé qmail ";
+
+
+
+$GLOBALS['strAutoCleanTables']			= " ãéìåì áñéñ äðúåðéí";
+
+$GLOBALS['strAutoCleanStats']			= " ãéìåì ñèèéñèé÷ä";
+
+$GLOBALS['strAutoCleanUserlog']			= " ãéìåì úéòåã îùúîù";
+
+$GLOBALS['strAutoCleanStatsWeeks']		= " âéì îéøáé ùì ñèèéñèé÷ä <br>(3 ùáåòåú îéðéîåí)";
+
+$GLOBALS['strAutoCleanUserlogWeeks']		= " âéì îéøáé ùì úéòåã îùúîù <br>(3 ùáåòåú îéðéîåí)";
+
+$GLOBALS['strAutoCleanErr']			= " âéì îéøáé çééá ìéäåú 3 ùáåòåú ìôçåú";
+
+$GLOBALS['strAutoCleanVacuum']			= " áöò ðé÷éåï ëììé àåèåîèéú ëì ìéìä"; // only Pg
+
+
+
+
+
+// Administrator settings
+
+$GLOBALS['strAdministratorSettings']		= "÷áéòåú îéðäìä";
+
+
+
+$GLOBALS['strLoginCredentials']			= "äøùàåú äúçáøåú";
+
+$GLOBALS['strAdminUsername']			= "ùí äîùúîù - îðäì";
+
+$GLOBALS['strInvalidUsername']			= "ùí îùúîù ôñåì";
+
+
+
+$GLOBALS['strBasicInformation']			= "îéãò áñéñé";
+
+$GLOBALS['strAdminFullName']			= "äùí äîìà ùì äîðäì";
+
+$GLOBALS['strAdminEmail']			= "ëúåáú äàéîééì ùì äîðäì";
+
+$GLOBALS['strCompanyName']			= "ùí äçáøä/àéâåã";
+
+
+
+$GLOBALS['strAdminCheckUpdates']		= "áãå÷ òãëåðéí";
+
+$GLOBALS['strAdminCheckEveryLogin']		= "áëì äúçáøåú";
+
+$GLOBALS['strAdminCheckDaily']			= "éåîéú";
+
+$GLOBALS['strAdminCheckWeekly']			= "ùáåòéú";
+
+$GLOBALS['strAdminCheckMonthly']		= "çåãùéú";
+
+$GLOBALS['strAdminCheckNever']			= "àó ôòí";
+
+
+
+$GLOBALS['strAdminNovice']			= " ôòåìåú äîçé÷ä ùì äîðäì ãåøùåú àéùåø ëîùðä æäéøåú";
+
+$GLOBALS['strUserlogEmail']			= " úòã àú ëì äàéîééì äéåöà";
+
+$GLOBALS['strUserlogPriority']			= " úòã àú ù÷ìåìé ä÷ãéîåéåú ëì ùòä";
+
+$GLOBALS['strUserlogAutoClean']			= " úòã ðé÷åé àåèåîèé ùì áñéñ äðúåðéí";
+
+
+
+
+
+// User interface settings
+
+$GLOBALS['strGuiSettings']			= "÷áéòåú îîù÷ îùúîù";
+
+
+
+$GLOBALS['strGeneralSettings']			= "÷áéòåú ëììéåú";
+
+$GLOBALS['strAppName']				= "ùí äééùåí ùéåöâ";
+
+$GLOBALS['strMyHeader']				= "ëåúøú äòîåã ùìé ðîöàú áëúåáú:";
+
+$GLOBALS['strMyHeaderError']		= "ëåúøú äòîåã ìà ðîöàä áîé÷åí ùðøùí";
+
+$GLOBALS['strMyFooter']				= "úçúéú äòîåã ùìé ðîöàú áëúåáú:";
+
+$GLOBALS['strMyFooterError']		= "úçúéú äòîåã ìà ðîöàä áîé÷åí ùðøùí";
+
+$GLOBALS['strGzipContentCompression']		= "äùúîù áãçéñú-úëåìä GZIP";
+
+
+
+$GLOBALS['strClientInterface']			= "îîù÷ îôøñí";
+
+$GLOBALS['strClientWelcomeEnabled']		= "àôùø äåãòú îéìåú ä÷ãîä áäúçáøåú äîôøñí";
+
+$GLOBALS['strClientWelcomeText']		= "äåãòú ä÷ãîä/áøëú áøåëéí äáàéí...<br>(àôùøé ùéîåù áúâé HTML)";
+
+
+
+
+
+
+
+// Interface defaults
+
+$GLOBALS['strInterfaceDefaults']		= "áøéøú îçãì îîù÷éú";
+
+
+
+$GLOBALS['strInventory']			= "îöàé";
+
+$GLOBALS['strShowCampaignInfo']			= " äöâ îéãò ðåñó òáåø ÷îôééï áòîåã <i>ñ÷éøú ÷îôééï</i>";
+
+$GLOBALS['strShowBannerInfo']			= " äöâ îéãò ðåñó òáåø áàðø áòîåã <i>ñ÷éøú áàðøéí</i>";
+
+$GLOBALS['strShowCampaignPreview']		= " úöåâä î÷ãîú ùì ëì äáàðøéí áòîåã <i>ñ÷éøú áàðøéí</i>";
+
+$GLOBALS['strShowBannerHTML']			= " äöâ áàðø îîùé áî÷åí ÷åã øâéì ùì  HTML, áîöá úöåâú áàðøéí îñåâ HTML";
+
+$GLOBALS['strShowBannerPreview']		= " úöåâä î÷ãéîä ùì áàðøéí áëåúøú äòîåã äòåñ÷ ááàðøéí";
+
+$GLOBALS['strHideInactive']			= " äñúø ôøèéí ìà ôòéìéí áëì òîåãé úöåâä î÷ãéîä";
+
+$GLOBALS['strGUIShowMatchingBanners']		= " äöâ áàðøéí úåàîéí áòîåãé <i>áàðøéí î÷åùøéí</i>";
+
+$GLOBALS['strGUIShowParentCampaigns']		= " äøàä ÷îôééï-àá áòîåãé <i>áàðøéí î÷åùøéí</i>";
+
+$GLOBALS['strGUILinkCompactLimit']		= " äñúø ÷îôééðéí ìà ÷ùåøéí àå áàðøéí, áòîåãé <i>áàðø î÷åùø</i>, ëàùø éù éåúø î-";
+
+
+
+$GLOBALS['strStatisticsDefaults'] 		= "ñèèéñèé÷ä";
+
+$GLOBALS['strBeginOfWeek']			= "äùáåò îúçéì áéåí";
+
+$GLOBALS['strPercentageDecimals']		= "ð÷åãä òùøåðéú";
+
+
+
+$GLOBALS['strWeightDefaults']			= "îù÷ì äúçìúé (áøéøú îçãì)";
+
+$GLOBALS['strDefaultBannerWeight']		= "îù÷ì áàðø äúçìúé (áøéøú îçãì)";
+
+$GLOBALS['strDefaultCampaignWeight']		= "îù÷ì ÷îôééï äúçìúé (áøéøú îçãì)";
+
+$GLOBALS['strDefaultBannerWErr']		= "îù÷ì äúçìúé ùì áàðø öøéê ìäéåú îñôø çéåáé";
+
+$GLOBALS['strDefaultCampaignWErr']		= "îù÷ì ÷îôééï äúçìúé çééá ìäéåú îñôø çéåáé";
+
+
+
+
+
+// Not used at the moment
+
+$GLOBALS['strTableBorderColor']			= "öáò äîñâøú ùì äèáìä";
+
+$GLOBALS['strTableBackColor']			= "öáò äø÷ò ùì äèáìä";
+
+$GLOBALS['strTableBackColorAlt']		= "öáò äø÷ò  ùì äèáìä(çìåôé)";
+
+$GLOBALS['strMainBackColor']			= "öáò ø÷ò øàùé";
+
+$GLOBALS['strOverrideGD']			= "àëåó úîåðä áúöåøú GD";
+
+$GLOBALS['strTimeZone']				= "àéæåø æîï";
+
+
+
+
+
+?>
