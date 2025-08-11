fb66940275f4bb7272b7b6ab5c4231d440093cd0
me-suzy <me.suzana@gmail.com>
2025-08-11 13:46:46 +0300

    Add Chat Scripts/dcshout2.0/dcshout2.0/shoutbox/admin/footer.php

---
+++ Chat Scripts/dcshout2.0/dcshout2.0/shoutbox/admin/footer.php
@@ -0,0 +1,15 @@
+</td>
+    </tr>
+<tr>
+<td>
+<b>Dc shout by <a href="http://codez.darkdevils.co.uk">Devil Coderz</a>(c)2004-2005</b>
+</td>
+</table>
+  </center>
+</div>
+
+</body>
+
+</html>
+
+
