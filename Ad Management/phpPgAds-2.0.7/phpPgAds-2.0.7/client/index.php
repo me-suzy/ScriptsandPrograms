3c1c88a16a02c461257299b23fd64d1ba9c7deda
me-suzy <me.suzana@gmail.com>
2025-08-11 10:12:18 +0300

    Add Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/client/index.php

---
+++ Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/client/index.php
@@ -0,0 +1,18 @@
+<?php // $Revision: 2.0.2.3 $
+
+/************************************************************************/
+/* phpPgAds                                                             */
+/* ========                                                             */
+/*                                                                      */
+/* Copyright (c) 2001-2005 by the phpPgAds developers                   */
+/* For more information visit: http://phppgads.sourceforge.net          */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+// Redirect to the admin interface
+Header("Location: ../admin/index.php");
+
+?>
