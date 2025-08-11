c3aa3095a519aadf3ab192132ec63c0d3da2c443
me-suzy <me.suzana@gmail.com>
2025-08-11 13:56:22 +0300

    Add Chat Scripts/x7chat1_3_6/X7Chat-v1.3.6B/Docs/Install.Xaraya/xaruser/main.php

---
+++ Chat Scripts/x7chat1_3_6/X7Chat-v1.3.6B/Docs/Install.Xaraya/xaruser/main.php
@@ -0,0 +1,9 @@
+<?
+function chat_user_main()
+{ 
+    	echo '<script language="javascript" type="text/javascript">
+			window.open("http://x7chat.com/Xaraya/Chat/","Chat");
+ 			history.go(-1);
+		</script> ';
+}
+?>
