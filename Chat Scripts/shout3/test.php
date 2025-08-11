f47a0679a3fc50385d9334ad8288766a1d9f522b
me-suzy <me.suzana@gmail.com>
2025-08-11 13:52:10 +0300

    Add Chat Scripts/shout3/test.php

---
+++ Chat Scripts/shout3/test.php
@@ -0,0 +1,11 @@
+<table width=120 border=1 bgcolor="#EACA19">
+<tr><td><?php include("shout.txt"); ?></td></tr>
+<tr><td>
+<form method="POST" action="shout.php">
+name:<input size="15" maxlength="15" type="text" name="name">
+shout:<input size="15" maxlength="25" type="text" name="c">
+<center>
+<input type="submit" value="Shout">
+</center>
+</form>
+</td></tr></table>
