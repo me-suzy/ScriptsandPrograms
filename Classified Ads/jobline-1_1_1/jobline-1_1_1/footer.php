a4449e02f0427498367b82480e2c63d5ce858c91
me-suzy <me.suzana@gmail.com>
2025-08-11 13:59:53 +0300

    Add Classified Ads/jobline-1_1_1/jobline-1_1_1/footer.php

---
+++ Classified Ads/jobline-1_1_1/jobline-1_1_1/footer.php
@@ -0,0 +1,7 @@
+  <p><?php echo _OJ_CREATED_BY; ?>
+     <font class="small"><a href="http://mambo.theyard.org" target="_blank">Olle Johansson</a></font>
+	 <br />
+	 <?php echo _OJ_VISIT; ?> <a href="http://mambo.theyard.org" target="blank">Mambo at the Yard</a> <?php echo _OJ_EXCITING_ADDONS; ?>
+     <br />
+     <font class="small"><?php echo _OJ_VERSION; ?>: <?php echo $cfgjl['version']; ?></font>
+  </p>
