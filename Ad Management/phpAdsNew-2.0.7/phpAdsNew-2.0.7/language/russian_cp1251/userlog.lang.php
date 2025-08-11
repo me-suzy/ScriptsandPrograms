1b78499f816904abeffc443222a3556d8b41a8a7
me-suzy <me.suzana@gmail.com>
2025-08-11 10:03:38 +0300

    Add Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/russian_cp1251/userlog.lang.php

---
+++ Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/russian_cp1251/userlog.lang.php
@@ -0,0 +1,35 @@
+<?php // $Revision: 2.0.2.2 $
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+/* For more information visit: http://www.phpadsnew.com                 */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+// Set translation strings
+
+$GLOBALS['strDeliveryEngine']				= "Äâèæîê äîñòàâêè";
+$GLOBALS['strMaintenance']					= "Îáñëóæèâàíèå";
+$GLOBALS['strAdministrator']				= "Àäìèíèñòðàòîð";
+
+
+$GLOBALS['strUserlog'] = array (
+	phpAds_actionAdvertiserReportMailed 	=> "Îò÷¸ò äëÿ ðåêëàìîäàòåëÿ {id} îòîñëàí ïî ýëåêòðîííîé ïî÷òå",
+	phpAds_actionPublisherReportMailed 		=> "Îò÷¸ò äëÿ èçäàòåëÿ {id} îòîñëàí ïî ýëåêòðîííîé ïî÷òå",
+	phpAds_actionWarningMailed				=> "Ïðåäóïðåæäåíèå î äåàêòèâàöèè êàìïàíèè {id} îòîñëàíî ïî ýëåêòðîííîé ïî÷òå",
+	phpAds_actionDeactivationMailed			=> "Óâåäîìëåíèå î äåàêòèâàöèè êàìïàíèè {id} îòîñëàíî ïî ýëåêòðîííîé ïî÷òå",
+	phpAds_actionPriorityCalculation		=> "Ïðèîðèòåòû ïåðåñ÷èòàíû",
+	phpAds_actionPriorityAutoTargeting		=> "Öåëè êàìïàíèè ïåðåñ÷èòàíû",
+	phpAds_actionDeactiveCampaign			=> "Êàìïàíèÿ {id} äåàêòèâèðîâàíà",
+	phpAds_actionActiveCampaign				=> "Êàìïàíèÿ {id} àêòèâèðîâàíà",
+	phpAds_actionAutoClean					=> "Àâòîî÷èñòêà áàçû äàííûõ"
+);
+
+?>
