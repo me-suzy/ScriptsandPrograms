fd218eda73d30ea9d223812a736a7426517cc22a
me-suzy <me.suzana@gmail.com>
2025-08-11 13:47:39 +0300

    Add Chat Scripts/EzChatBox-1.0/EzChatBox/dsp_ban.php

---
+++ Chat Scripts/EzChatBox-1.0/EzChatBox/dsp_ban.php
@@ -0,0 +1,21 @@
+<?php require("config.php"); ?>
+<html>
+<head>
+<title><?php echo $EZChat_ban_title; ?></title>
+<link href="ez.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<div id="chatbox" align="center">
+<table vspace="5" width="178">
+<tr>
+	<td align="center" class="banned">You have been banned from this chatbox.<br>
+      <br>
+    If you feel that the ban is unjust then please contact the <a href="mailto:<?php echo $EZChat_email; ?>">System Administrator</a> and plead your
+    case.</td>
+</tr>
+</table>
+</div>
+</body>
+</html>
