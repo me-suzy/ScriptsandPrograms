b3dd23c6447d8ac06f61b03e8d5a8e36733a7b7f
me-suzy <me.suzana@gmail.com>
2025-08-11 11:02:00 +0300

    Add Blog/bloghost/admin/editstyle.php

---
+++ Blog/bloghost/admin/editstyle.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+<link rel="stylesheet" href="style.css" type="text/css">
+<center><table border='0' width=100%><tr><td valign='top' width=30%>
+<?PHP
+include "connect.php";
+$blogadmin=$_SESSION['blogadmin'];
+$getadmin="SELECT * from bl_admin where username='$blogadmin'";
+$getadmin2=mysql_query($getadmin) or die("Cannot get admin");
+$getadmin3=mysql_fetch_array($getadmin2);
+if($getadmin3['status']>=1)
+{
+   include "left.php";
+   print "</td>";
+   print "<td valign='top' width=70%>";
+   print "<table class='maintable'><tr class='headline'><td><center>Edit Stylesheet</center></td></tr>";
+   print "<tr class='mainrow'><td>";
+   if(isset($_POST['submit']))
+   {
+      $css=$_POST['css'];
+      $updatecss="Update bl_admin set css='$css' where adminid='$getadmin3[adminid]'";
+      mysql_query($updatecss) or die("Could not update stylesheet");
+      print "CSS Updated.";
+
+
+   }
+   else
+   {
+      print "<form action='editstyle.php' method='post'>";
+      print "<textarea name='css' rows='7' cols='40'>$getadmin3[css]</textarea><br>";
+      print "<input type='submit' name='submit' value='submit'></form>";
+   
+
+
+   }
+   print "</td></tr></table>";
+
+}
+else
+{
+  print "Not logged in.";
+  print "</td></tr></table>";
+ 
+
+}
+?>
