2e2ce4fa70cb169d0a8b513e1b325f729f288cbb
me-suzy <me.suzana@gmail.com>
2025-08-11 10:03:01 +0300

    Add Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/korean/userlog.lang.php

---
+++ Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/korean/userlog.lang.php
@@ -0,0 +1,69 @@
+<?php // $Revision: 2.0 $
+
+
+
+/************************************************************************/
+
+/* phpAdsNew 2                                                          */
+
+/* ===========                                                          */
+
+/*                                                                      */
+
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+
+/* For more information visit: http://www.phpadsnew.com                 */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+
+
+// Set translation strings
+
+
+
+$GLOBALS['strDeliveryEngine']				= "Àü´ÞÀ¯Áö ¿£Áø";
+
+$GLOBALS['strMaintenance']					= "À¯Áöº¸¼ö";
+
+$GLOBALS['strAdministrator']				= "°ü¸®ÀÚ";
+
+
+
+
+
+$GLOBALS['strUserlog'] = array (
+
+	phpAds_actionAdvertiserReportMailed 	=> "±¤°íÁÖ {id}¿¡°Ô º¸°í¼­¸¦ ÀÌ¸ÞÀÏ·Î º¸³À´Ï´Ù.",
+
+	phpAds_actionPublisherReportMailed 		=> "±¤°í°Ô½ÃÀÚ {id}¿¡°Ô º¸°í¼­¸¦ ÀÌ¸ÞÀÏ·Î º¸³À´Ï´Ù.",
+
+	phpAds_actionWarningMailed				=> "Ä·ÆäÀÎ {id}¿¡ ´ëÇÑ È°¼ºÈ­ÇØÁ¦¸¦ ÀÌ¸ÞÀÏ·Î °æ°íÇÕ´Ï´Ù.",
+
+	phpAds_actionDeactivationMailed			=> "Ä·ÆäÀÎ {id}¿¡ ´ëÇÑ È°¼ºÈ­ÇØÁ¦¸¦ ÀÌ¸ÞÀÏ·Î ¾Ë¸³´Ï´Ù.",
+
+	phpAds_actionPriorityCalculation		=> "¿ì¼±¼øÀ§ ´Ù½Ã °è»ê",
+
+	phpAds_actionPriorityAutoTargeting		=> "Ä·ÆäÀÎ ´ë»ó Àç°è»ê",
+
+	phpAds_actionDeactiveCampaign			=> "Ä·ÆäÀÎ {id} È°¼ºÈ­ÇØÁ¦",
+
+	phpAds_actionActiveCampaign				=> "Ä·ÆäÀÎ {id} È°¼ºÈ­",
+
+	phpAds_actionAutoClean					=> "µ¥ÀÌÅÍº£ÀÌ½º ÀÚµ¿ Á¤¸®"
+
+);
+
+
+
+?>
