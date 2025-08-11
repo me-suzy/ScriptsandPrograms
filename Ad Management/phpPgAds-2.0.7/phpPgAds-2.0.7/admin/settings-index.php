342fd24081fb57ffeaeeca50088e12247b6299a6
me-suzy <me.suzana@gmail.com>
2025-08-11 10:10:50 +0300

    Add Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/admin/settings-index.php

---
+++ Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/admin/settings-index.php
@@ -0,0 +1,21 @@
+<?php // $Revision: 2.0.2.1 $ -
+
+/************************************************************************/
+/* phpPgAds                                                             */
+/* ========                                                             */
+/*                                                                      */
+/* Copyright (c) 2001 by the phpPgAds developers                        */
+/* http://www.greatbridge.org/project/phppgads/                         */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+
+// Redirect to admin settings page
+header("Location: settings-db.php");
+exit;
+
+?>
