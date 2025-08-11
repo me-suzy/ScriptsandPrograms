b089dcedee6253900ce6b069207c14094ac4e6ed
me-suzy <me.suzana@gmail.com>
2025-08-11 10:00:16 +0300

    Add Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/chinese_big5/report.lang.php

---
+++ Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/chinese_big5/report.lang.php
@@ -0,0 +1,47 @@
+<?php // $Revision: 2.0 $
+
+
+
+/************************************************************************/
+
+/* phpAdsNew 2                                                          */
+
+/* ===========                                                          */
+
+/*                                                                      */
+
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+
+/* For more information visit: http://www.phpadsnew.com                 */
+
+/*                                                                      */
+
+/*                                                                      */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+
+
+$GLOBALS['strPluginAffiliate'] 		= "²£¥Í©Ò¿ï¼s§iµo§G¤Hªº¾ú¥vÁ`Äý.¦¹³øªí¿é¥X®æ¦¡¬°¥i¥H¦b»sªí³n¥ó¤¤¨Ï¥ÎªºCSV.";
+
+$GLOBALS['strPluginCampaign'] 		= "²£¥Í©Ò¿ï¶µ¥Øªº¾ú¥vÁ`Äý.¦¹³øªí¿é¥X®æ¦¡¬°¥i¥H¦b»sªí³n¥ó¤¤¨Ï¥ÎªºCSV.";
+
+$GLOBALS['strPluginClient'] 		= "²£¥Í©Ò¿ï¼s§i«È¤áªº¾ú¥vÁ`Äý.¦¹³øªí¿é¥X®æ¦¡¬°¥i¥H¦b»sªí³n¥ó¤¤¨Ï¥ÎªºCSV.";
+
+$GLOBALS['strPluginGlobal'] 		= "²£¥Í¥þ§½ªº¾ú¥vÁ`Äý.¦¹³øªí¿é¥X®æ¦¡¬°¥i¥H¦b»sªí³n¥ó¤¤¨Ï¥ÎªºCSV.";
+
+$GLOBALS['strPluginZone'] 		= "²£¥Í©Ò¿ïª©¦ìªº¾ú¥vÁ`Äý.¦¹³øªí¿é¥X®æ¦¡¬°¥i¥H¦b»sªí³n¥ó¤¤¨Ï¥ÎªºCSV.";
+
+
+
+?>
