39eb0c1cc7dbe9a4fe1bc7f7e32982bdd4f9365f
me-suzy <me.suzana@gmail.com>
2025-08-11 13:40:23 +0300

    Add Calendars/showlister-05b/showlister_05b/samples/index.php

---
+++ Calendars/showlister-05b/showlister_05b/samples/index.php
@@ -0,0 +1,83 @@
+<html>
+  <head>
+    <title>Shows :: Sample Pages</title>
+    <link rel="stylesheet" type="text/css" href="showlister.css">
+  </head>
+  <body>
+
+    <div class='box'>
+    <h3>showlister :: sample pages</h3>
+    </div>
+
+    <div class='box'>
+
+      <!-- open table -->
+      <table>
+        <tr>
+          <td>
+            <a href="sample_shows_page.php">sample_shows_page.php</a> 
+          </td>
+          <td>
+            all artists by date
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="sample_shows_page_002.php">sample_shows_page_002.php</a> 
+          </td>
+          <td>
+            all artists by artist, then date
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="sample_shows_page_003.php">sample_shows_page_003.php</a>
+          </td>
+          <td>
+            single artist, by date
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="sample_shows_page_004.php">sample_shows_page_004.php</a>
+          </td>
+          <td>
+            all artists, by date ["details" on its own row]
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="sample_shows_page_005.php">sample_shows_page_005.php</a>
+          </td>
+          <td>
+            all artists, by date [no old shows]
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="sample_shows_page_006.php">sample_shows_page_006.php</a>
+          </td>
+          <td>
+            all artists, by date [alternating row colors]
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="sample_artists_page.php">sample_artists_page.php</a>
+          </td>
+          <td>
+            all artists, with clickable urls & emails
+          </td>
+        </tr>
+      </table>
+    </div>
+
+    <div class='box'>
+    These pages are included to give you an idea of the different sorts of 
+    output showlister can create.  I've tried to provide several so you can 
+    use one as the basis for your own project, saving you as much time and 
+    coding as possible.     
+    </div>
+
+  </body>
+</html>
