2972dae90fcec270b4c817b6934587d134e6f4ed
me-suzy <me.suzana@gmail.com>
2025-08-11 14:00:38 +0300

    Add Classified Ads/jobline-1_1_1/jobline-1_1_1/language/swedish.php

---
+++ Classified Ads/jobline-1_1_1/jobline-1_1_1/language/swedish.php
@@ -0,0 +1,258 @@
+<?php
+// Swedish language file for Jobline
+//
+// Distributed under the terms of the GNU General Public License
+// This software may be used without warrany provided and
+// copyright statements are left intact.
+
+// Frontend language strings
+DEFINE('_JL_ERROR', 'Ett fel har uppstått');
+DEFINE('_JL_NOSUBCATS', 'Inga underkategorier.');
+DEFINE('_JL_CATEGORIES', 'Kategorier');
+DEFINE('_JL_NOTFOUND', 'Hittade inte sidan.');
+DEFINE('_JL_HACKING_ATTEMPT', 'Hacking attempt, quitting.');
+DEFINE('_JL_JOBTYPE_FULLTIME', 'Heltid');
+DEFINE('_JL_JOBTYPE_PARTTIME', 'Deltid');
+DEFINE('_JL_JOBTYPE_INTERNSHIP', 'Praktik');
+DEFINE('_JL_LIST_JOBPOSTINGS', 'Jobbannonser');
+DEFINE('_JL_LIST_DESCRIPTION', 'Här nedan visas en lista över alla tillgängliga jobb.');
+DEFINE('_JL_JOBID', 'ID');
+DEFINE('_JL_DATE_POSTED', 'Datum');
+DEFINE('_JL_JOB_TITLE', 'Jobb');
+DEFINE('_JL_COMPANY_NAME', 'Företag');
+DEFINE('_JL_JOBTYPE', 'Jobbtyp');
+DEFINE('_JL_DATEPOSTED', 'Datum');
+DEFINE('_JL_COMPANYINFO', 'Företagsinformation');
+DEFINE('_JL_DESCRIPTION', 'Beskrivning');
+DEFINE('_JL_HOWTOAPPLY', 'Ansökan');
+DEFINE('_JL_EMAILRESUMETO', 'Skicka resum&eacute; till');
+DEFINE('_JL_WEBSITE', 'Webbplats');
+DEFINE('_JL_APPLYFORJOB', 'Skicka jobbansökan');
+DEFINE('_JL_FIRSTNAME', 'Förnamn');
+DEFINE('_JL_LASTNAME', 'Efternamn');
+DEFINE('_JL_ADDRESS1', 'Adress 1');
+DEFINE('_JL_ADDRESS2', 'Adress 2');
+DEFINE('_JL_CITY', 'Stad');
+DEFINE('_JL_STATE', 'Delstat');
+DEFINE('_JL_ZIPCODE', 'Postnr');
+DEFINE('_JL_COVER_LETTER', 'Ansökningsbrev');
+DEFINE('_JL_RESUME', 'Resum&eacute;');
+DEFINE('_JL_SEND_APPLICATION', 'Skicka ansökan');
+DEFINE('_JL_RECEIVED_APPLICATION', 'Du har fått en ansökan på följande jobb:');
+DEFINE('_JL_APPLICATION', 'Ansökan');
+DEFINE('_JL_APPLICATION_SUBJECT', 'Du har fått en jobbansökan');
+DEFINE('_JL_NOSUCHJOB', 'Hittade inget jobb.');
+DEFINE('_JL_THANKYOU', 'Tack');
+DEFINE('_JL_THANKYOU_MESSAGE', 'Tack för din jobbansökan till detta jobb.');
+DEFINE('_JL_GOBACKTOJOB', 'Gå tillbaka till jobblistan');
+DEFINE('_JL_GOTOLIST', 'Gå till jobblistan');
+DEFINE('_JL_PREVIEW_JOB', 'Förhandsgranska');
+DEFINE('_JL_CANCEL_JOB', 'Avbryt');
+DEFINE('_JL_QUALIFICATIONS', 'Kvalifikationer');
+DEFINE('_JL_COMPENSATION', 'Lön');
+DEFINE('_JL_CONTACTINFO', 'Kontaktinformation');
+DEFINE('_JL_CONTACTPHONE', 'Kontaktpersons telefonnummer');
+DEFINE('_JL_EMAIL', 'Kontaktpersons e-postadress');
+DEFINE('_JL_SAVEOREDIT', 'Spara eller fortsätt redigera?');
+DEFINE('_JL_SAVE_JOB', 'Spara jobbannons');
+DEFINE('_JL_EDIT_JOB', 'Redigera jobbannons');
+DEFINE('_JL_PREVIEW_JOBPOSTING', 'Förhandsgranska jobbannons');
+DEFINE('_JL_JOBPOSTINGSENT', 'Tack för din jobbannons. Våra redaktörer kommer att se över din annons inom kort..');
+DEFINE('_JL_GOTOSEARCH', 'Sök jobb');
+DEFINE('_JL_POSTJOB', 'Lägg upp jobb');
+DEFINE('_JL_ENTERJOBINFO', 'Skriv in jobbinformation');
+DEFINE('_JL_NEW_SEARCH', 'Sök job');
+DEFINE('_JL_NOSEARCHWORD', 'Du måste fylla i ett sökord.');
+DEFINE('_JL_SEARCH', 'Sök');
+DEFINE('_JL_NO_RESULTS', 'Inga resultat funna');
+DEFINE('_JL_NOTHING_FOUND', 'Kunde hitta några resultat baserat på dina sökkriterier.');
+DEFINE('_JL_SEARCH_RESULTS', 'Sökresultat');
+DEFINE('_JL_SEARCH_DESCRIPTION', 'Din sökning gav följande resultat: ');
+DEFINE('_JL_BACKTOSEARCH', 'Tillbaka till sökformuläret');
+DEFINE('_JL_TERMSOFSERVICE', 'Annonseringsvillkor');
+DEFINE('_JL_NOTACCESIBLE', 'Denna jobbannons är inte längre tillgänglig.');
+DEFINE('_JL_NOTOWNER', 'Du är inte ägaren till denna annons' );
+DEFINE('_JL_NOTLOGGEDIN', 'Du måste vara inloggad för att kunna ändra annonser.');
+DEFINE('_JL_NEWJOBPOSTED', 'En ny jobbannons har lagts upp');
+DEFINE('_JL_NEWJOB_MSG', 'En ny jobbannons med titeln \'<<jobtitle>>\' har lagts upp. För att godkänna jobbet, gå till <<livesite>>/administrator');
+DEFINE('_JL_JOBSTATUS_SOURCING', 'Söker');
+DEFINE('_JL_JOBSTATUS_INTERVIEWING', 'Intervjuar');
+DEFINE('_JL_JOBSTATUS_CLOSED', 'Stängt för nya sökande');
+DEFINE('_JL_JOBSTATUS_FINALISTS', 'Finalister utvalda');
+DEFINE('_JL_JOBSTATUS_PENDING', 'Väntar godkännande');
+DEFINE('_JL_JOBSTATUS_HOLD', 'Lagt på is');
+DEFINE('_JL_JOBSTATUS', 'Jobbstatus');
+DEFINE('_JL_LIST_INTERNSHIPS', 'Praktikplatser');
+DEFINE('_JL_LIST_INTERNSHIPS_DESC', 'Nedan visas en lista över alla tillgängliga praktikplatser.');
+DEFINE('_JL_INTERNSHIPID', 'ID');
+DEFINE('_JL_INTERNSHIP_TITLE', 'Praktikplats');
+DEFINE('_JL_GOTOSEARCH_INTERN', 'Sök praktikplatser');
+DEFINE('_JL_POSTINTERNSHIP', 'Lägg upp en praktikplatsannons');
+DEFINE('_JL_POSTINTERNSHIP', 'Lägg upp praktikplats');
+DEFINE('_JL_JOBAPPLICATION', 'Ansök om jobb');
+DEFINE('_JL_NOITEMS', 'Hittade inga jobb');
+DEFINE('_JL_ERRORSETTMPL', 'Kunde inte hitta mallfilen');
+DEFINE('_JL_JOBLOCATION', 'Ort');
+DEFINE('_JL_JOBREFERENCE', 'Referens');
+DEFINE('_JL_JOBDURATION', 'Varaktighet');
+DEFINE('_JL_DAYTELEPHONE', 'Telefon dagtid');
+DEFINE('_JL_EVENINGTELEPHONE', 'Telefon kvällstid');
+DEFINE('_JL_APPLICANTEMAIL', 'E-post');
+DEFINE('_JL_JOBPOSTINGNOTALLOWED', 'Du får inte lägga upp jobbannonser.');
+DEFINE('_JL_SUBMITYOURCV', 'Skicka in din CV');
+DEFINE('_JL_ACCESS_DENIED', 'Du har inte tillgång till denna resurs. Var god logga in eller registrera dig.');
+DEFINE('_JL_START_DATE', 'Startdatum');
+DEFINE('_JL_NEWITEM', 'Ny!');
+
+// Admin footer
+DEFINE('_OJ_VERSION', 'Version');
+DEFINE('_OJ_CREATED_BY', 'Denna komponent skapades åt PICnet av');
+DEFINE('_OJ_VISIT', 'Visit');
+DEFINE('_OJ_EXCITING_ADDONS', 'for more exciting addons.');
+
+// Admin strings
+DEFINE('_JL_A_YES', 'Ja');
+DEFINE('_JL_A_NO', 'Nej');
+DEFINE('_JL_A_CONFIG_SAVED', 'Konfigurationsfilen har sparats');
+DEFINE('_JL_A_CONFIGURATION', 'Jobline Konfiguration');
+DEFINE('_JL_A_INFORMATION', 'Information');
+DEFINE('_JL_A_AREYOUSURE', 'Är du säker?');
+DEFINE('_JL_A_SETTINGS', 'Inställningar');
+DEFINE('_JL_A_SELECTDELITEM', 'Välj en post att ta bort');
+DEFINE('_JL_A_SELECTITEM', 'Välj en post att');
+DEFINE('_JL_A_NOTITLE', 'Du måste fylla i titel');
+DEFINE('_JL_A_ADDING', 'Skapa');
+DEFINE('_JL_A_EDITING', 'Ändrar');
+DEFINE('_JL_A_JOBPOSTING', 'Jobbannons');
+DEFINE('_JL_A_CONTENT', 'Ändra innehåll');
+DEFINE('_JL_A_ENTER_TITLE', 'Titel');
+DEFINE('_JL_A_ENTER_JOBTYPE', 'Jobbtyp');
+DEFINE('_JL_A_ENTER_DESCRIPTION', 'Beskrivning');
+DEFINE('_JL_A_ENTER_QUALIFICATIONS', 'Kvalifikationer');
+DEFINE('_JL_A_ENTER_COMPENSATION', 'Lön');
+DEFINE('_JL_A_SHOW_COMPENSATION' ,'Vis lön?');
+DEFINE('_JL_A_HOW_TO_APPLY', 'Ansökan');
+DEFINE('_JL_A_LISTJOBS', 'Jobbannonser');
+DEFINE('_JL_A_DISPLAY', 'Visa #');
+DEFINE('_JL_A_JOBPOSTINGREJECTED', 'Din jobbannons har nekats');
+DEFINE('_JL_A_REJHI', 'Hej');
+DEFINE('_JL_A_REJECTION', 'Hej <<contactname>>,
+
+Vi har nekat din jobbannons med titeln "<<jobtitle>>" på webbplatsen <<sitename>>. Kontakta vår administratör på <<adminemail>> om du har några frågor.
+
+Tack.');
+DEFINE('_JL_A_JOBPOSTINGAPPROVED', 'Din jobbannons har lagts ut');
+DEFINE('_JL_A_APPROVE', 'Hej <<contactname>>,
+
+Vi har lagt upp din jobbannons med titeln "<<jobtitle>>" på webbplatsen <<sitename>>. Kontakta vår administratör på <<adminemail>> om du har några frågor.
+
+Tack.');
+DEFINE('_JL_A_MAILFROMNAME', 'Namnet som automatiska utskick ska skickas från.');
+DEFINE('_JL_A_MAILFROMADDRESS', 'E-postadress som automatiska utskick ska skickas från.');
+DEFINE('_JL_TITLEASC', 'Titel Stigande');
+DEFINE('_JL_TITLEDESC', 'Titel Fallande');
+DEFINE('_JL_ORDERINGASC', 'Sortering Stigande');
+DEFINE('_JL_ORDERINGDESC', 'Sortering Fallande');
+DEFINE('_JL_JOBIDASC', 'ID Stigande');
+DEFINE('_JL_JOBIDDESC', 'ID Fallande');
+DEFINE('_JL_A_CREATEDASC', 'Datum Stigande');
+DEFINE('_JL_A_CREATEDDESC', 'Datum Fallande');
+DEFINE('_JL_A_ITEMLIMIT', 'Antal annonser att visa på varje sida i listningar.');
+DEFINE('_JL_A_SORTORDER', 'Sorteringsordning på annonser i listningar');
+DEFINE('_JL_A_LISTJOBQUEUE', 'Jobb som väntar godkännande');
+DEFINE('_JL_A_JOBID', 'ID');
+DEFINE('_JL_A_TITLE', 'Titel');
+DEFINE('_JL_A_JOB_TYPE', 'Jobbtyp');
+DEFINE('_JL_A_FUNCTIONS', 'Funktioner');
+DEFINE('_JL_A_DATE_ADDED', 'Datum');
+DEFINE('_JL_A_COMPANY_NAME', 'Företag');
+DEFINE('_JL_A_PUBLISHED', 'Publicerad');
+DEFINE('_JL_A_CHECKEDOUT', 'Utcheckad');
+DEFINE('_JL_A_REORDER', 'Omsortera');
+DEFINE('_JL_A_ENTER_COMPANYNAME', 'Fyll i företagsnamn');
+DEFINE('_JL_A_ENTER_ADDRESS1', 'Adress 1');
+DEFINE('_JL_A_ENTER_ADDRESS2', 'Adress 2');
+DEFINE('_JL_A_ENTER_CITY', 'Stad');
+DEFINE('_JL_A_ENTER_USSTATE', 'Välj delstat');
+DEFINE('_JL_A_ENTER_ZIPCODE', 'Postnummer');
+DEFINE('_JL_A_ENTER_COMPANYURL', 'Företagswebbplats');
+DEFINE('_JL_A_ENTER_CONTACTNAME', 'Kontaktpersons namn');
+DEFINE('_JL_A_ENTER_CONTACTPHONE', 'Kontaktpersons telefonnummer');
+DEFINE('_JL_A_ENTER_CONTACTEMAIL', 'Kontaktpersons e-postadress');
+DEFINE('_JL_A_SHOWCONTACT', 'Visa kontaktinformation på jobbannonsen?');
+DEFINE('_JL_A_ENTER_MEMBERID', 'Medlemsnummer');
+DEFINE('_JL_A_ENTER_CREDITCARD_TYPE', 'Kreditkort');
+DEFINE('_JL_A_ENTER_CREDITCARD_NUMBER', 'Kreditkortsnummer');
+DEFINE('_JL_A_ENTER_CREDITCARD_EXPIRES', 'Utgångstid för kreditkort');
+DEFINE('_JL_A_POSTJOB', 'Godkänn');
+DEFINE('_JL_A_REJECTJOB', 'Neka');
+DEFINE('_JL_A_SELECTITEMPOST', 'Välj post att lägga upp');
+DEFINE('_JL_A_MOVEUP', 'Flytta upp');
+DEFINE('_JL_A_MOVEDOWN', 'Flytta ner');
+DEFINE('_JL_A_TERMSOFSERVICE', 'Annonseringsvillkor');
+DEFINE('_JL_A_PUBLISHINGLIMIT', 'Antal dagar som jobbannonser ska visas (sätt till "0" om du inte vill ha någon begränsning)');
+DEFINE('_JL_A_CLOSE', 'Stäng');
+DEFINE('_JL_A_PRINT', 'Skriv ut');
+DEFINE('_JL_A_PREVIEW', 'Förhandsgranskning av jobbannons');
+DEFINE('_JL_A_NOIDYET', 'Har inget ID än');
+DEFINE('_JL_A_PUBLISHITEM', 'Publicera jobbannons');
+DEFINE('_JL_A_SHOWCCFIELDS', 'Visa kreditkortsfält');
+DEFINE('_JL_A_SHOWUSSTATE', 'Visa val för amerikansk delstat vid adressformuläret.');
+DEFINE('_JL_A_ENTER_JOBSTATUS', 'Fyll i jobbstatus');
+DEFINE('_JL_A_DEFAULTJOBSTATUS', 'Förvalt värde för jobbstatus');
+DEFINE('_JL_A_EMAILAPPLICATION', 'Använd e-postprogram istället för ansökningsformulär.');
+DEFINE('_JL_A_ENTER_JOBLOCATION', 'Ort');
+DEFINE('_JL_A_ENTER_JOBREFERENCE', 'Referens');
+DEFINE('_JL_A_ENTER_JOBDURATION', 'Varaktighet');
+DEFINE('_JL_A_POSTJOBS', 'Begränsa vilka som kan lägga upp annonser till denna åtkomstnivå');
+DEFINE('_JL_A_ITEMSNEWFOR', 'Jobbanonser markeras som nya detta antal dagar.');
+DEFINE('_JL_A_SELECTSTATE', 'Välj amerikansk delstat');
+DEFINE('_JL_A_ORSELECTFROMLIST', 'Eller välj delstat:');
+DEFINE('_JL_A_KEYWORDCATS', 'Nyckelordskategorier');
+DEFINE('_JL_A_EXTRAFIELDS', 'Extra Fält');
+DEFINE('_JL_A_COMPANY_CONTACT_INFO', 'Kontaktinfo');
+DEFINE('_JL_A_PAYMENTINFORMATION', 'Kreditkortsinfo');
+DEFINE('_JL_A_JOBINFORMATION', 'JobbInfo');
+DEFINE('_JL_A_DATEFORMAT', 'Datumformat, lämna fältet tomt för att använda språkfilens datumformat (använder syntaxen för <a target="_blank" href="http://se.php.net/manual/en/function.strftime.php">strftime()</a>)');
+DEFINE('_JL_A_SELECTTEMPLATE', 'Välj vilken malluppsättning som ska användas');
+DEFINE('_JL_A_AUTOAPPROVEJOBS', 'Godkänn jobbannonser automatiskt.');
+DEFINE('_JL_A_ACCESS', 'Åtkomstnivå');
+DEFINE('_JL_A_ACCESS_ALL', 'Publik');
+DEFINE('_JL_A_ACCESS_REGISTERED', 'Registrerad');
+DEFINE('_JL_A_ACCESS_USER', 'Speciell');
+DEFINE('_JL_A_ACCESS_NONE', 'Inga');
+DEFINE('_JL_A_EXTRAINFO', 'Extra fält');
+DEFINE('_JL_A_PUBLISHING', 'Publicering');
+DEFINE('_JL_A_ITEM_DETAILS', 'Huvudinformation');
+DEFINE('_JL_A_COULDNTREADTEMPLATE', 'Kunde inte läsa mallfilen.');
+DEFINE('_JL_A_TEMPLATE_SAVED', 'Mallfilen har sparats.');
+DEFINE('_JL_A_LISTTEMPLATES', 'Lista mallfiler från malluppsättningen');
+DEFINE('_JL_A_CANCEL_TMPL', 'Mallfilen sparades inte.');
+DEFINE('_JL_A_EDITING_TEMPLATE', 'Ändrar mall');
+DEFINE('_JL_A_TEMPLATE_FILE', 'Mallfil');
+DEFINE('_JL_A_PATH', 'Sökväg:');
+DEFINE('_JL_A_WRITEABLE', 'Skrivbar');
+DEFINE('_JL_A_UNWRITEABLE', 'Ej skrivbar');
+DEFINE('_JL_A_NOEXTRAFIELDS', 'Det finns inga extra fält.');
+DEFINE('_JL_A_NO_EDITOR', 'No editor');
+DEFINE('_JL_A_DEFAULT_EDITOR', 'Mambo default editor');
+DEFINE('_JL_A_SELECTEDITOR', 'Use the Mambo default html editor on frontend?.');
+DEFINE('_JL_A_EDITORWIDTH', 'Width of HTML editor window in pixels');
+DEFINE('_JL_A_EDITORHEIGHT', 'Height of HTML editor window in pixels');
+DEFINE('_JL_A_INITIALIZE_HTML_EDITOR', 'Initialize WYSIWYG HTML editor for non-registered users?');
+
+// Error messages
+DEFINE('_JL_ERR_YOURCONFIG', 'Din konfigurationsfil är');
+DEFINE('_JL_ERR_CHMODFILE', 'Du måste ända filrättigheterna till 766 för att inställningarna ska kunna sparas.');
+DEFINE('_JL_ERR_WARNING', 'Varning...');
+DEFINE('_JL_ERR_NOT_WRITEABLE', 'ALLVARLIGT FEL: Kan inte skriva till iInställningsfilen');
+DEFINE('_JL_ERR_OPEN_FILE', 'ALLVARLIG FEL: Kunda inte öppna filen.');
+DEFINE('_JL_ERR_NOTITLE', 'Titel måste anges.');
+DEFINE('_JL_ERR_CHECKED_OUT1', 'Posten');
+DEFINE('_JL_ERR_CHECKED_OUT2', 'ändras för tillfället av en annan administratör.');
+DEFINE('_JL_ERR_TMPL_NOT_WRITEABLE', 'ALLVARLIGT FEL: Kan inte skriva till mallfilen·');
+
+#DEFINE('', '');
+
+
+?>
