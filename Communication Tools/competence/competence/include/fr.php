3f29fa758b140ea23028e2d59282d4e534185289
me-suzy <me.suzana@gmail.com>
2025-08-11 14:07:13 +0300

    Add Communication Tools/competence/competence/include/fr.php

---
+++ Communication Tools/competence/competence/include/fr.php
@@ -0,0 +1,46 @@
+<?
+//---------------------------------------------------------------------------------------------------
+//							
+//  messages en francais
+//
+//---------------------------------------------------------------------------------------------------
+
+$mess[0] = "Dernière version";
+$mess[1] = "Login";
+$mess[2] = " Mot de passe";
+$mess[3] = "Valider";
+$mess[4] = "S enregistrer";
+$mess[5] = "Création d'un utilisateur";
+$mess[6] = "Nom";
+$mess[7] = "Prénom";
+$mess[8] = "Login";
+$mess[9] = "Vérification mot de passe";
+$mess[10] = "Erreur Table ";
+$mess[11] = "Liste des Systèmes - Domaines - Compétences";
+$mess[12] = "Système";
+$mess[13] = "Domaines";
+$mess[14] = "Compétences";
+$mess[15] = "Supprimer";
+$mess[16] = "Ce Login existe déja veuillez en choisir un autre";
+$mess[17] = "Nouvel essai";
+$mess[18] = "Créer";
+$mess[19] = "Création";
+$mess[20] = "Liste des évaluations effectuées";
+$mess[21] = "Création nouvelle évaluation";
+$mess[22] = "Evaluation";
+$mess[23] = "Objectif";
+$mess[25] = "Niveau";
+$mess[26] = "Liste des Objectifs définis";
+$mess[27] = "Afficher les évaluations cochées";
+$mess[28] = "Afficher";
+$mess[29] = "Administration";
+$mess[30] = "modifier competences";
+$mess[31] = "Aide";
+$mess[32] = "Saisie des parametres";
+$mess[33] = "Date Evaluation";
+$mess[34] = "Type evaluation";
+$mess[35] = "Consolidation equipe";
+$mess[36] = "Dernière connection";
+$mess[37] = "Liste des utilisateurs";
+$mess[38] = "Paramètres utilisateur";
+?>
