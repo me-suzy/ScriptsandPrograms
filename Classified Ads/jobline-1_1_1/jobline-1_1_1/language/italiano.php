8c6df0a5fa3de6af0fa258edf0b830ba76765097
me-suzy <me.suzana@gmail.com>
2025-08-11 14:00:30 +0300

    Add Classified Ads/jobline-1_1_1/jobline-1_1_1/language/italiano.php

---
+++ Classified Ads/jobline-1_1_1/jobline-1_1_1/language/italiano.php
@@ -0,0 +1,256 @@
+<?php
+// File in Italiano per JobLine
+// Grazie a Vittorio ZuccalÃ  (http://www.nathanvi.it)
+// Distributed under the terms of the GNU General Public License
+// This software may be used without warrany provided and
+// copyright statements are left intact.
+
+// Frontend language strings
+DEFINE('_JL_ERROR', 'Ho rilevato un errore');
+DEFINE('_JL_NOSUBCATS', 'Nessuna SottoCategoria');
+DEFINE('_JL_CATEGORIES', 'Categorie');
+DEFINE('_JL_NOTFOUND', 'Pagina non trovata!');
+DEFINE('_JL_HACKING_ATTEMPT', 'Stai facendo hacking? Esco!');
+DEFINE('_JL_JOBTYPE_FULLTIME', 'Tempo-Pieno');
+DEFINE('_JL_JOBTYPE_PARTTIME', 'Part-time');
+DEFINE('_JL_JOBTYPE_INTERNSHIP', 'Interinale');
+DEFINE('_JL_LIST_JOBPOSTINGS', 'Invio di Curriculum');
+DEFINE('_JL_LIST_DESCRIPTION', 'Qui puoi trovare una lista delle figure professionali di cui potremmo avere bisogno. Se sei una azienda che cerca figure professionali sentiti libero di inserire un \'Nuovo Lavoro\'. Se sei un privato e vuoi inserire il tuo curriculum clicca sul lavoro che ti interessa e inserisci i tuoi dati.');
+DEFINE('_JL_JOBID', 'Lavoro&nbsp;ID');
+DEFINE('_JL_DATE_POSTED', 'Data&nbsp;');
+DEFINE('_JL_JOB_TITLE', 'Lavoro');
+DEFINE('_JL_COMPANY_NAME', 'SocietÃ ');
+DEFINE('_JL_JOBTYPE', 'Tipo di Lavoro');
+DEFINE('_JL_DATEPOSTED', 'Data');
+DEFINE('_JL_COMPANYINFO', 'Informazioni sulla SocietÃ ');
+DEFINE('_JL_DESCRIPTION', 'Descrizione Lavoro');
+DEFINE('_JL_HOWTOAPPLY', 'Ambiti');
+DEFINE('_JL_EMAILRESUMETO', 'Email a cui sarÃ  inviato il curriculum');
+DEFINE('_JL_WEBSITE', 'Sito Web');
+DEFINE('_JL_APPLYFORJOB', 'Inserisci il curriculum per questo Lavoro');
+DEFINE('_JL_FIRSTNAME', 'Nome');
+DEFINE('_JL_LASTNAME', 'Cognome');
+DEFINE('_JL_ADDRESS1', 'Indirizzo');
+DEFINE('_JL_ADDRESS2', 'Email');
+DEFINE('_JL_CITY', 'CittÃ ');
+DEFINE('_JL_STATE', 'Stato');
+DEFINE('_JL_ZIPCODE', 'CAP');
+DEFINE('_JL_COVER_LETTER', 'Conoscenze di qualsiasi genere');
+DEFINE('_JL_RESUME', 'Esperienze Lavorative e Titolo Scolastico');
+DEFINE('_JL_SEND_APPLICATION', 'Spedisci il Curriculum');
+DEFINE('_JL_RECEIVED_APPLICATION', 'Hai ricevuto una mail per il seguente Lavoro:');
+DEFINE('_JL_APPLICATION', 'Curriculum');
+DEFINE('_JL_APPLICATION_SUBJECT', 'Hai ricevuto un curriculum');
+DEFINE('_JL_NOSUCHJOB', 'Non sono stati trovati Lavori che corrispondano alla ricerca.');
+DEFINE('_JL_THANKYOU', 'Grazie');
+DEFINE('_JL_THANKYOU_MESSAGE', 'Grazie per aver inserito il tuo curriculum per questo Lavoro.');
+DEFINE('_JL_GOBACKTOJOB', 'Torna indietro alla pagina di inserimento curriculum');
+DEFINE('_JL_GOTOLIST', 'Torna indietro alla lista Lavori');
+DEFINE('_JL_PREVIEW_JOB', 'Preview del Lavoro');
+DEFINE('_JL_CANCEL_JOB', 'Torna in dietro');
+DEFINE('_JL_QUALIFICATIONS', 'Qualifiche');
+DEFINE('_JL_COMPENSATION', 'Compenso');
+DEFINE('_JL_CONTACTINFO', 'Contatto');
+DEFINE('_JL_CONTACTPHONE', 'Telefono');
+DEFINE('_JL_EMAIL', 'Email');
+DEFINE('_JL_SAVEOREDIT', 'Vuoi salvarlo o continui le modifiche?');
+DEFINE('_JL_SAVE_JOB', 'Salva il Lavoro');
+DEFINE('_JL_EDIT_JOB', 'Modifica il Lavoro');
+DEFINE('_JL_PREVIEW_JOBPOSTING', 'Preview del Invio Curriculum');
+DEFINE('_JL_JOBPOSTINGSENT', 'Ti ringraziamo per aver inserito un nuovo Lavoro. Un nostro incaricato lo analizzerÃ  presto.');
+DEFINE('_JL_GOTOSEARCH', 'Ricerca un Lavoro');
+DEFINE('_JL_POSTJOB', 'Inserisci un Lavoro');
+DEFINE('_JL_ENTERJOBINFO', 'Inserimento Lavoro');
+DEFINE('_JL_NEW_SEARCH', 'Cerca un Lavoro');
+DEFINE('_JL_NOSEARCHWORD', 'Devi inserire almeno una parola di ricerca.');
+DEFINE('_JL_SEARCH', 'Cerca');
+DEFINE('_JL_NO_RESULTS', 'Nessun risultato');
+DEFINE('_JL_NOTHING_FOUND', 'Non riesco a trovare nulla che corrisponda a ciÃ² che cerchi.');
+DEFINE('_JL_SEARCH_RESULTS', 'Risultati della ricerca.');
+DEFINE('_JL_SEARCH_DESCRIPTION', 'La tua ricerca ha ottenuto il seguente numero di risultati: ');
+DEFINE('_JL_BACKTOSEARCH', 'Torna indietro alla ricerca');
+DEFINE('_JL_TERMSOFSERVICE', 'Termini del Servizio offerto');
+DEFINE('_JL_NOTACCESIBLE', 'Questo Lavoro non sarÃ  disponibile ancora a lungo...');
+DEFINE('_JL_NOTOWNER', 'Non sei il proprietario di questo Curriculum' );
+DEFINE('_JL_NOTLOGGEDIN', 'Devi aver effettuato il Login a sinistra per poter editare questo Curriculum.');
+DEFINE('_JL_NEWJOBPOSTED', 'Un nuovo curriculum Ã¨ stato inviato');
+DEFINE('_JL_NEWJOB_MSG', 'Un nuovo Lavoro denominato \'<<jobtitle>>\' Ã¨ stato inserito. Vai nella sezione administrator <<livesite>>/ per approvarlo.');
+DEFINE('_JL_JOBSTATUS_SOURCING', 'Sourcing');
+DEFINE('_JL_JOBSTATUS_INTERVIEWING', 'Interviewing');
+DEFINE('_JL_JOBSTATUS_CLOSED', 'Closed to New Applicants');
+DEFINE('_JL_JOBSTATUS_FINALISTS', 'Finalists Identified');
+DEFINE('_JL_JOBSTATUS_PENDING', 'Pending Approval');
+DEFINE('_JL_JOBSTATUS_HOLD', 'Hold');
+DEFINE('_JL_JOBSTATUS', 'Job Status');
+DEFINE('_JL_LIST_INTERNSHIPS', 'Internships');
+DEFINE('_JL_LIST_INTERNSHIPS_DESC', 'Below is a list of all current internships.');
+DEFINE('_JL_INTERNSHIPID', 'ID');
+DEFINE('_JL_INTERNSHIP_TITLE', 'Internship Title');
+DEFINE('_JL_GOTOSEARCH_INTERN', 'Search Internships');
+DEFINE('_JL_POSTINTERNSHIP', 'Post an internship');
+DEFINE('_JL_JOBAPPLICATION', 'Application for job');
+DEFINE('_JL_NOITEMS', 'No items found');
+DEFINE('_JL_ERRORSETTMPL', 'Couldn\'t set template');
+DEFINE('_JL_JOBLOCATION', 'Location');
+DEFINE('_JL_JOBREFERENCE', 'Reference');
+DEFINE('_JL_JOBDURATION', 'Duration');
+DEFINE('_JL_DAYTELEPHONE', 'Daytime Telephone');
+DEFINE('_JL_EVENINGTELEPHONE', 'Evening Telephone');
+DEFINE('_JL_APPLICANTEMAIL', 'Email');
+DEFINE('_JL_JOBPOSTINGNOTALLOWED', 'You are not allowed to post jobs.');
+DEFINE('_JL_SUBMITYOURCV', 'Submit your CV');
+DEFINE('_JL_ACCESS_DENIED', 'You are not allowed to view this resource. Please login or register.');
+DEFINE('_JL_START_DATE', 'Start Date');
+DEFINE('_JL_NEWITEM', 'New!');
+
+// Admin footer
+DEFINE('_OJ_VERSION', 'Versione');
+DEFINE('_OJ_CREATED_BY', 'Questo componente Ã¨ stato creato da per PICnet da');
+DEFINE('_OJ_VISIT', 'Visita');
+DEFINE('_OJ_EXCITING_ADDONS', 'per vedere altri interessanti addons.');
+
+// Admin strings
+DEFINE('_JL_A_YES', 'Si');
+DEFINE('_JL_A_NO', 'No');
+DEFINE('_JL_A_CONFIG_SAVED', 'File di configurazione salvato');
+DEFINE('_JL_A_CONFIGURATION', 'Configurazione Jobline');
+DEFINE('_JL_A_INFORMATION', 'Informazioni');
+DEFINE('_JL_A_AREYOUSURE', 'Sei sicuro?');
+DEFINE('_JL_A_SETTINGS', 'Impostazioni');
+DEFINE('_JL_A_SELECTDELITEM', 'Seleziona un oggetto da eliminare');
+DEFINE('_JL_A_SELECTITEM', 'Selezione un oggetto');
+DEFINE('_JL_A_NOTITLE', 'Devi inserire un titolo');
+DEFINE('_JL_A_ADDING', 'Aggiungi');
+DEFINE('_JL_A_EDITING', 'Modifica');
+DEFINE('_JL_A_JOBPOSTING', 'Invio Lavoro');
+DEFINE('_JL_A_CONTENT', 'Modifica il Contenuto');
+DEFINE('_JL_A_ENTER_TITLE', 'Titolo');
+DEFINE('_JL_A_ENTER_JOBTYPE', 'Tipo di Lavoro');
+DEFINE('_JL_A_ENTER_DESCRIPTION', 'Descrizione');
+DEFINE('_JL_A_ENTER_QUALIFICATIONS', 'Qualifiche');
+DEFINE('_JL_A_ENTER_COMPENSATION', 'Compenso compreso tra');
+DEFINE('_JL_A_SHOW_COMPENSATION' ,'Mostra la richiesta di compenso?');
+DEFINE('_JL_A_HOW_TO_APPLY', 'Applicazioni');
+DEFINE('_JL_A_LISTJOBS', 'Invio Lavoro');
+DEFINE('_JL_A_DISPLAY', 'Mostra #');
+DEFINE('_JL_A_JOBPOSTINGREJECTED', 'Il tuo invio di Lavoro Ã¨ stato rifiutato');
+DEFINE('_JL_A_REJHI', 'Buongiorno');
+DEFINE('_JL_A_REJECTION', 'Buongiorno <<contactname>>,
+
+abbiamo rifiutato l\'invio del tuo Lavoro chiamato "<<jobtitle>>"  sul sito <<sitename>>. Se hai qualche informazione da richiedere o richiesta da fare al riguardo, puoi contattare il nostro amministratore di sistema che risponde alla seguente mail <<adminemail>>.
+
+Grazie per la collaborazione.');
+DEFINE('_JL_A_JOBPOSTINGAPPROVED', 'Il tuo Lavoro Ã¨ stato inviato correttamente.');
+DEFINE('_JL_A_APPROVE', 'Buongiorno <<contactname>>,
+
+Abbiamo inviato il Lavoro da te segnalatoci intitolato "<<jobtitle>>" sul sito <<sitename>> . Se hai qualche informazione da richiedere o richiesta da fare al riguardo, puoi contattare il nostro amministratore di sistema che risponde alla seguente mail <<adminemail>>.
+
+Grazie per la collaborazione.');
+DEFINE('_JL_A_MAILFROMNAME', 'Nome del mittente per le mail automatiche');
+DEFINE('_JL_A_MAILFROMADDRESS', 'Indirizzo Email come mittente per le mail automatiche');
+DEFINE('_JL_TITLEASC', 'Titoli dalla A alla Z');
+DEFINE('_JL_TITLEDESC', 'Titolo dalla Z alla A');
+DEFINE('_JL_ORDERINGASC', 'Ordine Ascendente');
+DEFINE('_JL_ORDERINGDESC', 'Ordine Descrescente');
+DEFINE('_JL_JOBIDASC', 'Lavoro ID Ascendente');
+DEFINE('_JL_JOBIDDESC', 'Lavoro ID Discendente');
+DEFINE('_JL_A_CREATEDASC', 'Data Ascendente');
+DEFINE('_JL_A_CREATEDDESC', 'Data Discendente');
+DEFINE('_JL_A_ITEMLIMIT', 'Numero di oggetti mostrati per ogni pagina');
+DEFINE('_JL_A_SORTORDER', 'Ordina gli oggetti nella lista');
+DEFINE('_JL_A_LISTJOBQUEUE', 'Coda di approvazione Lavori');
+DEFINE('_JL_A_JOBID', 'Lavoro&nbsp;ID');
+DEFINE('_JL_A_TITLE', 'Titolo');
+DEFINE('_JL_A_JOB_TYPE', 'Tipo Lavoro');
+DEFINE('_JL_A_FUNCTIONS', 'Funzioni');
+DEFINE('_JL_A_DATE_ADDED', 'Data Aggiunta');
+DEFINE('_JL_A_COMPANY_NAME', 'SocietÃ ');
+DEFINE('_JL_A_PUBLISHED', 'Pubblicato');
+DEFINE('_JL_A_CHECKEDOUT', 'Torna');
+DEFINE('_JL_A_REORDER', 'Riordina');
+DEFINE('_JL_A_ENTER_COMPANYNAME', 'Inserisci Nome SocietÃ ');
+DEFINE('_JL_A_ENTER_ADDRESS1', 'Indirizzo');
+DEFINE('_JL_A_ENTER_ADDRESS2', 'Email');
+DEFINE('_JL_A_ENTER_CITY', 'CittÃ ');
+DEFINE('_JL_A_ENTER_USSTATE', 'Seleziona Provincia');
+DEFINE('_JL_A_ENTER_ZIPCODE', 'CAP');
+DEFINE('_JL_A_ENTER_COMPANYURL', 'Sito Web Soc.');
+DEFINE('_JL_A_ENTER_CONTACTNAME', 'Nome Contatto');
+DEFINE('_JL_A_ENTER_CONTACTPHONE', 'Tel Contatto');
+DEFINE('_JL_A_ENTER_CONTACTEMAIL', 'Email Contatto');
+DEFINE('_JL_A_SHOWCONTACT', 'Mostra Contatto nell\'inserimento Lavoro?');
+DEFINE('_JL_A_ENTER_MEMBERID', 'Utente ID');
+DEFINE('_JL_A_ENTER_CREDITCARD_TYPE', 'Tipo Carta di Credito');
+DEFINE('_JL_A_ENTER_CREDITCARD_NUMBER', 'Numero Carta di Credit');
+DEFINE('_JL_A_ENTER_CREDITCARD_EXPIRES', 'Termine Carta di Credito');
+DEFINE('_JL_A_POSTJOB', 'Invio Lavoro');
+DEFINE('_JL_A_REJECTJOB', 'Rifiuta');
+DEFINE('_JL_A_SELECTITEMPOST', 'Seleziona un oggetto da inviare');
+DEFINE('_JL_A_MOVEUP', 'Sposta sopra');
+DEFINE('_JL_A_MOVEDOWN', 'Sposta sotto');
+DEFINE('_JL_A_TERMSOFSERVICE', 'Termini del Servizio Offerto');
+DEFINE('_JL_A_PUBLISHINGLIMIT', 'Numero di giorni di esposizione del lavoro (inserisci 0 per nessun limite)');
+DEFINE('_JL_A_CLOSE', 'Chiudi');
+DEFINE('_JL_A_PRINT', 'Stampa');
+DEFINE('_JL_A_PREVIEW', 'Preview del Invio Lavoro');
+DEFINE('_JL_A_NOIDYET', 'Non hai ancora un ID');
+DEFINE('_JL_A_PUBLISHITEM', 'Pubblica l\'invio del Lavoro');
+DEFINE('_JL_A_SHOWCCFIELDS', 'Mostra il campo Carta di Credito');
+DEFINE('_JL_A_DEFAULTJOBSTATUS', 'Default job status');
+DEFINE('_JL_A_EMAILAPPLICATION', 'Use email program for applications instead of a form.');
+DEFINE('_JL_A_ENTER_JOBLOCATION', 'Location');
+DEFINE('_JL_A_ENTER_JOBREFERENCE', 'Reference');
+DEFINE('_JL_A_ENTER_JOBDURATION', 'Duration');
+DEFINE('_JL_A_POSTJOBS', 'Restrict who can post jobs to this access level');
+DEFINE('_JL_A_ITEMSNEWFOR', 'Job postings are marked new for this number of days.');
+DEFINE('_JL_A_SELECTSTATE', 'Select US State');
+DEFINE('_JL_A_ORSELECTFROMLIST', 'Or select US state:');
+DEFINE('_JL_A_KEYWORDCATS', 'Keyword categories');
+DEFINE('_JL_A_EXTRAFIELDS', 'Extra fields');
+DEFINE('_JL_A_COMPANY_CONTACT_INFO', 'Contact Information');
+DEFINE('_JL_A_PAYMENTINFORMATION', 'Credit Card Payment information');
+DEFINE('_JL_A_JOBINFORMATION', 'Job Info');
+DEFINE('_JL_A_DATEFORMAT', 'Date format, leave empty to use language default (uses <a target="_blank" href="http://se.php.net/manual/en/function.strftime.php">strftime()</a> syntax)');
+DEFINE('_JL_A_SELECTTEMPLATE', 'Select which template set to use');
+DEFINE('_JL_A_AUTOAPPROVEJOBS', 'Automatically approve posted jobs.');
+DEFINE('_JL_A_ACCESS', 'Access Level');
+DEFINE('_JL_A_ACCESS_ALL', 'Public');
+DEFINE('_JL_A_ACCESS_REGISTERED', 'Registered');
+DEFINE('_JL_A_ACCESS_USER', 'Special');
+DEFINE('_JL_A_ACCESS_NONE', 'None');
+DEFINE('_JL_A_EXTRAINFO', 'Extra fields');
+DEFINE('_JL_A_PUBLISHING', 'Publishing');
+DEFINE('_JL_A_ITEM_DETAILS', 'Item Details');
+DEFINE('_JL_A_COULDNTREADTEMPLATE', 'Couldn\'t read template file.');
+DEFINE('_JL_A_TEMPLATE_SAVED', 'Template file has been saved.');
+DEFINE('_JL_A_LISTTEMPLATES', 'List templates from template set');
+DEFINE('_JL_A_CANCEL_TMPL', 'Template was not saved.');
+DEFINE('_JL_A_EDITING_TEMPLATE', 'Editing template');
+DEFINE('_JL_A_TEMPLATE_FILE', 'Template file');
+DEFINE('_JL_A_PATH', 'Path:');
+DEFINE('_JL_A_WRITEABLE', 'Writeable');
+DEFINE('_JL_A_UNWRITEABLE', 'Unwriteable');
+DEFINE('_JL_A_NOEXTRAFIELDS', 'No extra fields configured.');
+DEFINE('_JL_A_NO_EDITOR', 'No editor');
+DEFINE('_JL_A_DEFAULT_EDITOR', 'Mambo default editor');
+DEFINE('_JL_A_SELECTEDITOR', 'Use the Mambo default html editor on frontend?.');
+DEFINE('_JL_A_EDITORWIDTH', 'Width of HTML editor window in pixels');
+DEFINE('_JL_A_EDITORHEIGHT', 'Height of HTML editor window in pixels');
+DEFINE('_JL_A_INITIALIZE_HTML_EDITOR', 'Initialize WYSIWYG HTML editor for non-registered users?');
+
+// Module strings
+
+// Error messages
+DEFINE('_JL_ERR_YOURCONFIG', 'Il tuo file di configurazione Ã¨');
+DEFINE('_JL_ERR_CHMODFILE', 'Hai bisoogno di cambiare i permessi a 766 prima di modificare la configurazione.');
+DEFINE('_JL_ERR_WARNING', 'Attenzione...');
+DEFINE('_JL_ERR_NOT_WRITEABLE', 'ERRORE IRREVERSIBILE: Il file di configurazione non Ã¨ apribile in scrittura');
+DEFINE('_JL_ERR_OPEN_FILE', 'ERRORE IRREVERSIBILE: Il file non puÃ² essere aperto. Forse non esiste');
+DEFINE('_JL_ERR_NOTITLE', 'E\' richiesto un titolo.');
+DEFINE('_JL_ERR_CHECKED_OUT1', 'L\'oggetto');
+DEFINE('_JL_ERR_CHECKED_OUT2', 'Ã¨ attualmente in fase di modifica presso un altro amministratore.');
+DEFINE('_JL_ERR_TMPL_NOT_WRITEABLE', 'FATAL ERROR: Template file not writeable·');
+
+#DEFINE('', '');
+
+?>
