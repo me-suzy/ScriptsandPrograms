7e41830ff797a9df3cd6d587ff23100f8cb95e0c
me-suzy <me.suzana@gmail.com>
2025-08-11 14:06:42 +0300

    Add Communication Tools/comments/comments/templates/admin/nav.php

---
+++ Communication Tools/comments/comments/templates/admin/nav.php
@@ -0,0 +1,5 @@
+<?
+print<<<EOF
+<p><a href="{$COM_CONF['admin_script_url']}">Home</a> | <a href="{$COM_CONF['admin_script_url']}?action=bannedlist">Banned IPs</a> | <a href="{$COM_CONF['admin_script_url']}?action=logout">Logout</a> </p>
+EOF;
+?>
