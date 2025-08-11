76157d61bedffe1339e260f8e38b9d2a089a1e48
me-suzy <me.suzana@gmail.com>
2025-08-11 14:10:08 +0300

    Add Communication Tools/e-post/content.php

---
+++ Communication Tools/e-post/content.php
@@ -0,0 +1,10 @@
+<?
+if ($page=="home") include "home.php";
+if ($page=="e-post") include "e-post/e-post.php";
+if ($page=="e-open") include "e-post/e-open.php";
+if ($page=="e-new") include "e-post/e-new.php";
+if ($page=="reg") include "register.php";
+if ($page=="pref") include "preferences.php";
+if ($page=="users") include "users.php";
+if ($page=="friends") include "friends.php";
+?>
