703e44a4602c1edbdedf527bdd67a28700734b7b
me-suzy <me.suzana@gmail.com>
2025-08-11 14:00:20 +0300

    Add Classified Ads/jobline-1_1_1/jobline-1_1_1/uninstall.jobline.php

---
+++ Classified Ads/jobline-1_1_1/jobline-1_1_1/uninstall.jobline.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 *	Jobline Component for Mambo 4.5
+ 	 *
+	 *	Copyright (C) 2004 Olle Johansson
+	 *	Distributed under the terms of the GNU General Public License
+	 *	This software may be used without warrany provided and
+	 *  copyright statements are left intact.
+     */
+function com_uninstall() {
+	global $database;
+	return "Jobline Successfully Uninstalled<br />
+	Visit <a href=\"http://mambo.theyard.org\" target=\"blank\">Mambo at the Yard</a> for more exciting Mambo addons.";
+}
+?>
