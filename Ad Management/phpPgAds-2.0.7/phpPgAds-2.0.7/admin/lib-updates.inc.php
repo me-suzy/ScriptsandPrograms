691fca240967a3ad0934b46d446a5ad5a4e66104
me-suzy <me.suzana@gmail.com>
2025-08-11 10:10:07 +0300

    Add Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/admin/lib-updates.inc.php

---
+++ Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/admin/lib-updates.inc.php
@@ -0,0 +1,88 @@
+<?php // $Revision: 2.1.2.5 $
+
+/************************************************************************/
+/* phpPgAds                                                             */
+/* ========                                                             */
+/*                                                                      */
+/* Copyright (c) 2001-2005 by the phpPgAds developers                   */
+/* For more information visit: http://phppgads.sourceforge.net          */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+
+// Prevent full path disclosure
+if (!defined('phpAds_path')) die();
+
+
+
+// Set define to prevent duplicate include
+define ('LIBUPDATES_INCLUDED', true);
+
+
+// Include required files
+require (phpAds_path.'/libraries/lib-xmlrpc.inc.php');
+
+
+
+/*********************************************************/
+/* XML-RPC server settings                               */
+/*********************************************************/
+
+$phpAds_updatesServer = array(
+	'host'	 => 'www.phppgads.com',
+	'script' => '/updateAdsNew/xmlrpc.php',
+	'port'	 => 80
+);
+
+
+
+/*********************************************************/
+/* Check for updates via XML-RPC                         */
+/*********************************************************/
+
+function phpAds_checkForUpdates($already_seen = 0)
+{
+	global $phpAds_config, $phpAds_updatesServer;
+	global $xmlrpcerruser;
+
+	// Create client object
+	$client = new xmlrpc_client($phpAds_updatesServer['script'],
+		$phpAds_updatesServer['host'], $phpAds_updatesServer['port']);
+	
+	// Create XML-RPC request message
+	$msg = new xmlrpcmsg("updateAdsNew.check", array(
+		new xmlrpcval($phpAds_config['config_version'], "string"),
+		new xmlrpcval($already_seen, "string"),
+		new xmlrpcval($phpAds_config['updates_dev_builds'] ? 'dev' : '', "string")
+	));
+
+	// Send XML-RPC request message
+	if($response = $client->send($msg, 10))
+	{
+		// XML-RPC server found, now checking for errors
+		if (!$response->faultCode())
+		{
+			$ret = array(0, phpAds_xmlrpcDecode($response->value()));
+			
+			phpAds_dbQuery("
+				UPDATE
+					".$phpAds_config['tbl_config']."
+				SET
+					updates_last_seen = '".$ret[1]['config_version']."',
+					updates_timestamp = ".time()."
+			");
+		}
+		else
+			$ret = array($response->faultCode(), $response->faultString());
+		
+		return $ret;
+	}
+	
+	return array(-1, 'No response from the server');
+}
+
+?>
