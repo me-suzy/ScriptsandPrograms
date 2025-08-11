36698a53e2162f815d92719d7853d51c36e3a75c
me-suzy <me.suzana@gmail.com>
2025-08-11 13:01:06 +0300

    Add Blog/webspotblogging/Upload/inc/footer.inc.php

---
+++ Blog/webspotblogging/Upload/inc/footer.inc.php
@@ -0,0 +1,21 @@
+<?
+/**
+* WebspotBlogging
+* Copyright 2005
+*
+* Website : http://blogging.webspot.co.uk/
+* Licence : http://blogging.webspot.co.uk/eula.php
+*
+**/
+?>
+</td></div></tr>
+</table>
+<?
+echo "<div style='float:right;font-family:Verdana;font-size:7.5pt;'>";
+echo "<BR>";
+echo "Powered By <a href='http://www.webspot.co.uk/' target='_blank'>WebspotBlogging</a> v2.0<BR>";
+echo "Copyright &copy; 2005 <a href='http://www.webspot.co.uk/' target='_blank'><b>Webspot.co.uk</b></a>";
+echo "</div><BR><BR>";
+?>
+</BODY>
+</HTML>
