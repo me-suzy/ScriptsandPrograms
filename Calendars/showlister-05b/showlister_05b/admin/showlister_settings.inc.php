3aa84a531dd8422be4333f5e139e23118a30bbfd
me-suzy <me.suzana@gmail.com>
2025-08-11 13:40:18 +0300

    Add Calendars/showlister-05b/showlister_05b/admin/showlister_settings.inc.php

---
+++ Calendars/showlister-05b/showlister_05b/admin/showlister_settings.inc.php
@@ -0,0 +1,11 @@
+<?php
+    // =============================
+    // database connection variables
+    // =============================
+    $site = "localhost";
+    $user = "root";
+    $pass = "";
+    $database = "showlister_04b";
+    $database_table ="showlister_shows";
+    $database_table_artists ="showlister_artists";
+?>
