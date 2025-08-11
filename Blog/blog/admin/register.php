c0bbfdbb91e8e98f74b1377ee5b16a3132173bdc
me-suzy <me.suzana@gmail.com>
2025-08-11 10:49:15 +0300

    Add Blog/blog/admin/register.php

---
+++ Blog/blog/admin/register.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="style.css" type="text/css">
+<center>
+<table class='maintable'>
+<tr class='headline'><td><center>Register Admin</center></td></tr>
+<tr class='mainrow'><td>
+<form action='reguser.php' method='post'>
+Admin name:<br>
+<input type='text' name='username' size='20'><br>
+Password:<br>
+<input type='password' name='password' size='20'><br>
+Re-type Password:<br>
+<input type='password' name='pass2' size='20'><br>
+<input type='submit' name='submit' value='submit'>
+</form>
+</center>
