b9c77250d8d1996d5c5c951f711275d74d89c89d
me-suzy <me.suzana@gmail.com>
2025-08-11 13:19:22 +0300

    Add Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/no_NO/topic.inc.php

---
+++ Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/no_NO/topic.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+$topic = array
+(
+'100' => 'Brukstips',
+'101' => 'Dra & Slipp',
+'103' => 'Funksjoner for verktøyrad',
+'200' => 'Kommandoer',
+'210' => 'Sesjonshåndtering',
+'220' => 'Installasjon & Vedlikehold',
+'230' => 'Kontohåndtering',
+'240' => 'Brukerhåndtering',
+'250' => 'Gruppehåndtering',
+'260' => 'Mappefunksjoner',
+'270' => 'Lenkefunksjoner',
+'300' => 'Teknisk info',
+'302' => 'Sikkerhetsmekanisme',
+'303' => 'Drakter',
+'304' => 'Skribenter',
+'305' => 'Migrering',
+);
+
+?>
