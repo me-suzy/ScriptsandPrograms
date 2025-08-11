beb0e94ffea8a156503c6b8d42a558f0f6071a37
me-suzy <me.suzana@gmail.com>
2025-08-11 09:55:18 +0300

    Add Ad Management/fad/inc/footer.php

---
+++ Ad Management/fad/inc/footer.php
@@ -0,0 +1,24 @@
+<?php
+/*********************************************************
+			This Free Script was downloaded at			
+			Free-php-Scripts.net (HelpPHP.net)			
+	This script is produced under the LGPL license		
+		Which is included with your download.			
+	Not like you are going to read it, but it mostly	
+	States that you are free to do whatever you want	
+				With this script!						
+*********************************************************/
+@mysql_close();
+?>
+<table width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><div align="center"></div></td>
+  </tr>
+</table>
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bordercolor="#999933">
+  <tr> 
+    <td><div align="center"><font color="#000000" size="2" face="Verdana, Arial, Helvetica, sans-serif"><strong><a href="http://www.free-php-scripts.net" target="_blank" >Support Site</a> - <a href="terms.php"> TOS </a></strong></font></div>      <div align="center"></div></td>
+  </tr>
+</table>
+</body>
+</html>
