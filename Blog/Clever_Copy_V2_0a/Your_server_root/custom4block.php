fefcbe35a8055e912df2b7fea7a852c815439b3b
me-suzy <me.suzana@gmail.com>
2025-08-11 11:17:15 +0300

    Add Blog/Clever_Copy_V2_0a/Your_server_root/custom4block.php

---
+++ Blog/Clever_Copy_V2_0a/Your_server_root/custom4block.php
@@ -0,0 +1,36 @@
+<?php
+$getcustom="SELECT * from CC_custom_blocks WHERE blocknum = '4'";
+$getcustom2=mysql_query($getcustom) or die($custom_block_error);
+$getcustom3=mysql_fetch_array($getcustom2);
+if ($getprefs3[showseparator]==1)
+        {
+        echo "<hr color=$getprefs3[separatorlinecolor] size = '1'>";
+    }
+    if ($getprefs3[useblockwrapper]==1)
+         {
+         echo "<table border='0' cellspacing='3' width='100%' style='border-collapse: collapse; border: 1px solid $getprefs3[blockbordercolor]'>";
+         if ($getprefs3[block_use_heading_graphic] ==1)
+              {
+              echo "<tr bgcolor=$getprefs3[block_heading_background_color]><td height=$getprefs3[block_heading_height] background='bkgd.gif'>";
+         }
+         else
+             {
+             echo "<tr bgcolor=$getprefs3[block_heading_background_color]><td height=$getprefs3[block_heading_height]>";
+         }
+         echo "<font face=$getprefs3[block_heading_font_face] size=$getprefs3[block_heading_font_size] color=$getprefs3[block_heading_font_color]><$getprefs3[block_heading_font_decoration]>&nbsp;$getblocks3[custom_block4_label]</font></b></center></td></tr>";
+         echo "<tr><td bgcolor=$getprefs3[block_background_color]>";
+         $content = $getcustom3[customblock];
+         $content = stripslashes($content);
+         echo $content;
+         echo "</td></tr></table><br>";
+    }
+    else
+        {
+         echo "<center><font face = $getprefs3[block_heading_font_face] size = $getprefs3[block_heading_font_size] color= $getprefs3[block_heading_font_color]><$getprefs3[block_heading_font_decoration]>$getblocks3[custom_block4_label]</$getprefs3[block_heading_font_decoration]></center>"; 
+         echo "<hr color=$getprefs3[separatorlinecolor] size = '1'><br></font>";
+         $content = $getcustom3[customblock];
+         $content = stripslashes($content);
+         echo $content;
+         echo "<br>";
+}
+?>
