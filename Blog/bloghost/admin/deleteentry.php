f053c44f9b3bf58529771da1cec037942bd92523
me-suzy <me.suzana@gmail.com>
2025-08-11 11:01:02 +0300

    Add Blog/bloghost/admin/deleteentry.php

---
+++ Blog/bloghost/admin/deleteentry.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+<link rel="stylesheet" href="style.css" type="text/css">
+<center><table border='0' width=100%><tr><td valign='top' width=30%>
+<?PHP
+include "connect.php";
+$blogadmin=$_SESSION['blogadmin'];
+$getadmin="SELECT * from bl_admin where username='$blogadmin'";
+$getadmin2=mysql_query($getadmin) or die("Cannot get admin");
+$getadmin3=mysql_fetch_array($getadmin2);
+$membername=$_GET['membername'];
+if($getadmin3['status']==3 || $_SESSION['blogadmin']== $membername)
+{
+   include "left.php";
+   print "</td>";
+   print "<td valign='top' width=70%>";
+   print "<table class='maintable'><tr class='headline'><td><center>Main Admin</center></td></tr>";
+   print "<tr class='mainrow'><td>";
+
+   $ID=$_GET['ID'];
+   if(isset($_POST['submit']))
+   {
+      $delcomments="DELETE from bl_comments where eparent='$ID'";
+      mysql_query($delcomments) or die("Could not delete comments");
+      $delentry="DELETE from bl_blog where entryid='$ID'";
+      mysql_query($delentry) or die("Could not delete entry");
+      print "Entry Deleted.";
+
+   }
+   else
+   {
+      print "Are you sure you want to delete this entry?";
+      print "<form action='deleteentry.php?ID=$ID&membername=$membername' method='post'>";
+      print "<input type='submit' name='submit' value='Delete'></form>";
+
+   }
+   print "</td></tr></table>";
+}
+else
+{
+  print "Not logged in.";
+  print "</td></tr></table>";
+ 
+
+}
+?>
