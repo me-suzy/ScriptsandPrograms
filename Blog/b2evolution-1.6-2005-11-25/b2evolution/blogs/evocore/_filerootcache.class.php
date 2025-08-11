b7133e81b4106bfa20fe5a7204c1a994ff51e20b
me-suzy <me.suzana@gmail.com>
2025-08-11 10:26:44 +0300

    Add Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/evocore/_filerootcache.class.php

---
+++ Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/evocore/_filerootcache.class.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * This file implements the FileRootCache class.
+ *
+ * This file is part of the b2evolution/evocms project - {@link http://b2evolution.net/}.
+ * See also {@link http://sourceforge.net/projects/evocms/}.
+ *
+ * @copyright (c)2003-2005 by Francois PLANQUE - {@link http://fplanque.net/}.
+ *
+ * @license http://b2evolution.net/about/license.html GNU General Public License (GPL)
+ * {@internal
+ * b2evolution is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * b2evolution is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with b2evolution; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * }}
+ *
+ * @package evocore
+ *
+ * {@internal Below is a list of authors who have contributed to design/coding of this file: }}
+ * @author fplanque: François PLANQUE.
+ *
+ * @version $Id: _filerootcache.class.php,v 1.3 2005/11/18 07:53:05 blueyed Exp $
+ *
+ */
+if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+
+
+/**
+ * Includes
+ */
+require_once dirname(__FILE__).'/_fileroot.class.php';
+
+
+/**
+ * This class provides info about File Roots.
+ *
+ * These are root directories available for media file storage, under access permission.
+ *
+ * @package evocore
+ */
+class FileRootCache
+{
+	var $cache = array();
+
+
+	/**
+	 * Get a FileRoot (cached).
+	 *
+	 * @param string Root type: 'user', 'group', 'collection' or 'absolute'
+	 * @param integer ID of the user, the group or the collection the file belongs to...
+	 * @return FileRoot
+	 */
+	function & get_by_type_and_ID( $root_type, $root_in_type_ID )
+	{
+		$root_ID = FileRoot::gen_ID( $root_type, $root_in_type_ID );
+
+		if( ! isset( $this->cache[$root_ID] ) )
+		{	// Not in Cache, let's instantiate:
+			$this->cache[$root_ID] = new FileRoot( $root_type, $root_in_type_ID ); // COPY
+		}
+
+		return $this->cache[$root_ID];
+	}
+
+
+	/**
+	 * Get the absolute path (FileRoot::ads_path) to a given root (with ending slash).
+	 *
+	 * @return string
+	 */
+	function get_root_dir( $root_type, $root_in_type_ID )
+	{
+		$tmp_FileRoot = & $this->get_by_type_and_ID( $root_type, $root_in_type_ID );
+		return $tmp_FileRoot->ads_path;
+	}
+}
+
+
+/*
+ * $Log: _filerootcache.class.php,v $
+ * Revision 1.3  2005/11/18 07:53:05  blueyed
+ * use $_FileRoot / $FileRootCache for absolute path, url and name of roots.
+ *
+ * Revision 1.2  2005/09/06 17:13:55  fplanque
+ * stop processing early if referer spam has been detected
+ *
+ * Revision 1.1  2005/07/29 17:56:18  fplanque
+ * Added functionality to locate files when they're attached to a post.
+ * permission checking remains to be done.
+ *
+ */
+?>
