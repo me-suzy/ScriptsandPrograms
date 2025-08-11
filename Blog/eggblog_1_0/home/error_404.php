c111e3507e0078ec2d0ac6eedabb07b448cd6f91
me-suzy <me.suzana@gmail.com>
2025-08-11 11:40:44 +0300

    Add Blog/eggblog_1_0/home/error_404.php

---
+++ Blog/eggblog_1_0/home/error_404.php
@@ -0,0 +1,7 @@
+<?php
+require "../_etc/header.php";
+echo "		<h2>HTTP 404 error</h2>
+		<p><b>THis page cannot be found.</b></p>
+		<p>Please <a href=\"javascript:history.go(-1)\">go back</a> and try again.</p>\n";
+require "../_etc/footer.php";
+?>
