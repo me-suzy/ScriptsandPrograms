9b145d68564146e064940cc31b677f94ddc65ca1
me-suzy <me.suzana@gmail.com>
2025-08-11 14:07:08 +0300

    Add Communication Tools/competence/competence/include/en.php

---
+++ Communication Tools/competence/competence/include/en.php
@@ -0,0 +1,44 @@
+<?
+//---------------------------------------------------------------------------------------------------
+//								
+//
+//---------------------------------------------------------------------------------------------------
+$mess[0] = "Last version";
+$mess[1] = "Login";
+$mess[2] = "Password";
+$mess[3] = "Valide";
+$mess[4] = "Register";
+$mess[5] = "Create user";
+$mess[6] = "Last Name";
+$mess[7] = "First Name";
+$mess[8] = "Login";
+$mess[9] = "Check password";
+$mess[10] = "Error:Table ";
+$mess[11] = "List  Stages - Fieds - Knowledge";
+$mess[12] = "Stage";
+$mess[13] = "Field";
+$mess[14] = "Knowledge";
+$mess[15] = "Supress";
+$mess[16] = "This login exist choose an other";
+$mess[17] = "Try again";
+$mess[18] = "Create";
+$mess[19] = "Creation";
+$mess[20] = "List evaluations avalaibles";
+$mess[21] = "Create a new evaluation";
+$mess[22] = "Evaluation";
+$mess[23] = "Objective";
+$mess[25] = "Level";
+$mess[26] = "Defined Objectives list";
+$mess[27] = "Show the checks evaluations";
+$mess[28] = "Show";
+$mess[29] = "Administration";
+$mess[30] = "Update knowledge";
+$mess[31] = "Help";
+$mess[32] = "Parameters input";
+$mess[33] = "Evaluation Date";
+$mess[34] = "Type evaluation";
+$mess[35] = "Team Consolidation";
+$mess[36] = "Last connexion";
+$mess[37] = "Users List";
+$mess[38] = "User Parameters";
+?>
