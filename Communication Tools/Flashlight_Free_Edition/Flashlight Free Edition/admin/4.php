dd85d25024e5f8dae6eb2febf56438bcf62c6da7
me-suzy <me.suzana@gmail.com>
2025-08-11 14:11:26 +0300

    Add Communication Tools/Flashlight_Free_Edition/Flashlight Free Edition/admin/4.php

---
+++ Communication Tools/Flashlight_Free_Edition/Flashlight Free Edition/admin/4.php
@@ -0,0 +1,8 @@
+<div id="textbox" align="center">
+<form name="search" action="admin.php?action=u4" method="post" onSubmit="return validateID(document.search.id.value)">
+<b>Search for a User</b><br /><br />
+By User ID:<br /><input type="text" name="id" size="4"><br /><br />
+By First Name:<br /><input type="text" name="first" size="20"><br /><br />
+By Last Name:<br /><input type="text" name="last" size="20"><br /><br />
+<input type="submit" value="Search">
+</div>
