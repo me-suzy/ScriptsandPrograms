7168501a7b2594d6c2b807252990ac80e177ba48
me-suzy <me.suzana@gmail.com>
2025-08-11 10:52:15 +0300

    Add Blog/blog192/blog192/lib/session.php

---
+++ Blog/blog192/blog192/lib/session.php
@@ -0,0 +1,41 @@
+<?php
+/*
+#===========================================================================
+#= Project: PluggedOut Blog
+#= File   : lib/session.php
+#= Version: 1.9.2 (2005-09-13)
+#= Author : Jonathan Beckett
+#= Email  : jonbeckett@pluggedout.com
+#= Website: http://www.pluggedout.com/index.php?pk=dev_blog
+#= Support: http://www.pluggedout.com/development/forums/viewforum.php?f=26
+#===========================================================================
+#= Copyright (c) 2005 Jonathan Beckett
+#= You are free to use and modify this script as long as this header
+#= section stays intact. This file is part of PluggedOut Blog.
+#=
+#= This program is free software; you can redistribute it and/or modify
+#= it under the terms of the GNU General Public License as published by
+#= the Free Software Foundation; either version 2 of the License, or
+#= (at your option) any later version.
+#=
+#= This program is distributed in the hope that it will be useful,
+#= but WITHOUT ANY WARRANTY; without even the implied warranty of
+#= MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#= GNU General Public License for more details.
+#=
+#= You should have received a copy of the GNU General Public License
+#= along with CMS files; if not, write to the Free Software
+#= Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#===========================================================================
+*/
+
+// Initialise the session
+session_start();
+
+// Look for the cookies - populate the session variables if possible
+if (isset($_COOKIE["blog_userid"])){
+	$_SESSION["blog_userid"]==$_COOKIE["blog_userid"];
+	$_SESSION["blog_username"]==$_COOKIE["blog_username"];
+}
+
+?>
