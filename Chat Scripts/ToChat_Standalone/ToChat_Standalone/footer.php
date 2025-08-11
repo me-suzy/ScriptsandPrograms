81e0fed752bce2f5a1c5acd58c71b7e1c00315df
me-suzy <me.suzana@gmail.com>
2025-08-11 13:54:23 +0300

    Add Chat Scripts/ToChat_Standalone/ToChat_Standalone/footer.php

---
+++ Chat Scripts/ToChat_Standalone/ToChat_Standalone/footer.php
@@ -0,0 +1,2 @@
+<div align="center">
+<a href="http://www.dwebmaster.com">Daily Webmaster News </a> | <a href="http://www.to.ma">Webdesign Portal </a>| <a href="http://www.cmsdemo.org">CMS Demo</a> | <a href="http://www.dwebmaster.com/webmaster-directory/">Webmaster Directory</a></div>
