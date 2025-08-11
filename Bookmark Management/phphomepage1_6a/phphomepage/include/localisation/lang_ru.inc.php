9ed63ad36ebf55c494a536a1cba1febca0771f5f
me-suzy <me.suzana@gmail.com>
2025-08-11 13:14:16 +0300

    Add Bookmark Management/phphomepage1_6a/phphomepage/include/localisation/lang_ru.inc.php

---
+++ Bookmark Management/phphomepage1_6a/phphomepage/include/localisation/lang_ru.inc.php
@@ -0,0 +1,94 @@
+<?
+/**
+ * [En]File of Russian language
+ *
+ * @copyright    22/03/2004
+ * @since	     03/08/2001
+ * @version      1.5
+ * @module       lang_ru
+ * @modulegroup  localisation
+ * @package      php_homepage
+ * @access	     public
+ * @author       Eric BLAS <webmaster@phphomepage.net>
+ * @author       Lord <Kox@smtp.ru>
+ */
+
+$lang_ConnexBase              = "Ïðîáëåìû ñ áàçîé<br>\n Ïîïðîáóéòå ÷óòü ïîçæå";
+$lang_ErrorNomFichier         = "Âû îáÿçàòåëüíî äîëæíû óêàçàòü èìÿ.<br>Íàïðèìåð:";
+$lang_Fichier                 = "Ôàéë";
+$lang_Introuvable             = "is untraceable.";
+$lang_Accueil                 = "Äîáðî ïîæàëîâàòü íà";
+$lang_NvellePage              = "Ñîçäàòü èëè ìîäåôèöèðîâàòü ñòðàíè÷êó:";
+$lang_GenereTable             = "Íàæìèòå ÷òî áû ñîçäàòü òàáëèöó";
+$lang_ErreurNom               = "Âû äîëæíî ñíîâî ââåñòè <b>ÈÌß</b> â îäíî èç ïîëåé ôîðìû";
+$lang_Frame                   = "Ýòà ôîðìà äëÿ íàâèãàöèè â óïðàâëåíèè ôðýéìàìè.";
+$lang_1                       = "1 . Äëÿ ñîçäàíèÿ çàãîëîâêà";
+$lang_2                       = "2 . Äîáàâèòü ññûëêó";
+$lang_3                       = "3 . Ñîçäàòü âàø ôîðìàò";
+$lang_4                       = "4 . Ãåíåðèðîâàíèå âàøåé ñòðàíè÷êè";
+$lang_Constituer1             = "Âàøà Ñòðàíè÷êà \"";
+$lang_Constituer2             = " \" ñîñòàâëåí èç çàãîëîâêîâ è ññûëîê ñîãëàñíî:";
+$lang_Position                = "Ïîçèöèÿ =";
+$lang_Realiser                = "Ñîçäà ñ";
+$lang_CreerMEP                = "Ñîçäàòü íîâûé ôîðìàò";
+$lang_ModifOK                 = "Ñòèëü îôîðìëåíèÿ áûë èçìåí¸í";
+$lang_CreerOK                 = "Ñòèëü îôîðìëåíèÿ áûë ñîçäàí";
+$lang_Rubrique                = "Çàãîëîâêè";
+$lang_NvelleRubrique          = "Ñîçäàòü íîâûé çàãîëîâîê";
+$lang_PlacerRubrique          = "Ïîìåñòèòü çàãîëîâîê íà ñòðàíèöó:";
+$lang_SuppRubrique            = "Óäàëèòü çàãîëîâîê";
+$lang_ModifUneRubrique        = "Ðåäàêòèðîâàòü çàãîëîâîê";
+$lang_Place                   = " Ìåñòî :";
+$lang_ChoixRubrique           = "Âûáåðèòå çàãîëîâîê";
+$lang_ErrorRubNom             = "Âû äîëæíû ââåñòè èìÿ!";
+$lang_ErrorRubPosition        = "Âû äîëæíû ââåñòè ïîçèöèþ!";
+$lang_ErrorRubPositionSup     = 'You must enter a position lower than '.(($cfg_NbrLignes * $cfg_NbrColonnes) + 1).'!';
+$lang_RubOK                   = "Çàãîëîâîê áûë ñîçäàí";
+$lang_ErrorRubSupp            = "Âû äîëæíû âûáðàòü çàãîëîâîê äëÿ ïåðåìåùåíèÿ!";
+$lang_RubSupp                 = "Çàãîëîâîê áûë ïåðåíåñ¸í";
+$lang_ErrorRubModif           = "Âû äîëæíû âûáðàòü çàãîëîâîê äëÿ èçìåíåíèÿ!";
+$lang_ErrorRubNomPlace        = "Âû äîëæíû óêàçàòü èìÿ è ìåñòî!";
+$lang_ModifRubOK              = "Çàãîëîâîê áûë èçìåí¸í";
+$lang_ErrorLienRub            = "Âû äîëæíû ñîçäàòü õîòÿ áû <B>1</B> çàãîëîâîê";
+$lang_LienChoix               = "Âûáåðèòå ññûëêó";
+$lang_LienNew                 = "Ñîçäàòü íîâóþ ññûëêó";
+$lang_LienSup                 = "Óäàëèòü ññûëêó";
+$lang_ModifUnLien             = 'Modify a link';
+$lang_Lien                    = "Ññûëêè";
+$lang_ErrorLienSup            = "Âû äîëæíû âûáðàòü ññûëêó äëÿ ïåðåìåùåíèÿ!";
+$lang_LienSupOK               = "Ññûäêà áûëà óäàëåíà";
+$lang_LienNom                 = "Âû äîëæíû óêàçàòü èìÿ!";
+$lang_LienURL                 = "Âû äîëæíû óêàçàòü ññûëêó!";
+$lang_LienRub                 = "Âû äîëæíû âûáðàòü çàãîëîâîê!";
+$lang_ErrorLienModif          = 'You must choose a link !';
+$lang_LienOK                  = "Ññûëêà óñïåøíî äîáàâëåíà";
+$lang_ModifLienOK             = 'The link was modified';
+$lang_Target                  = "Îòêðûòü íîâîå îêíî";
+$lang_Oui                     = "Äà";
+$lang_Non                     = "Íåò";
+
+// Navigation
+$lang_PourNom                 = "Äëÿ ñòðàíèöû : ";
+$lang_AjoutRubrique           = "Äîáàâèòü çàãîëîâêè";
+$lang_AjoutLiens              = "Äîáàâèòü ññûëêè";
+$lang_ModifRubrique           = "Ðåäàêòèðîâàòü çàãîëîâêè";
+$lang_ModifLiens              = "Ðåäàêòèðîâàòü ññûëêè";
+$lang_MiseEnPage              = "Ñòèëü îôîðìëåíèÿ";
+$lang_AffHomepage             = "Ïîêàçàòü âàøó ñòðàíè÷êó";
+$lang_RetourIndex             = "Âûáðàòü íîâóþ ñòðàíè÷êó";
+
+// Formulaire
+$lang_Nom                     = "Íàçâàíèå:";
+$lang_RVB                     = "Êðàñíûé - Çåë¸íûé - Ñèíèé";
+$lang_CoulFond                = "Öâåò ôîíà";
+$lang_CoulRub                 = "Öâåò çàãîëîâêà";
+$lang_TailleRub               = "Ðàçìåð çàãîëîâêà";
+$lang_CoulLien                = "Öâåò ññûëîê";
+$lang_TailleLien              = "Ðàçìåð ññûëîê";
+$lang_Police                  = "Øðèôò";
+$lang_Title                   = "Îïèñàíèå ñòðàíèöû";
+// Bouton
+$lang_Creer                   = "Ñîçäàòü";
+$lang_Modifier                = "Ðåäàêòèðîâàòü";
+$lang_Supprimer               = "Óäàëèòü";
+?>
