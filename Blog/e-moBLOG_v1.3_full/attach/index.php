fd558f28995b092144e3cd84e99ba88b1804c546
me-suzy <me.suzana@gmail.com>
2025-08-11 11:37:53 +0300

    Add Blog/e-moBLOG_v1.3_full/attach/index.php

---
+++ Blog/e-moBLOG_v1.3_full/attach/index.php
@@ -0,0 +1,25 @@
+<?php
+/***************************************************************************
+ *   index.php
+ *
+ *   copyright © 2004 Axel Achten / e-motionalis.net
+ *   contact: thefiddler@e-motionalis.net
+ *   this file is a part of the " e-moBLOG " weblog engine
+ *
+ *   This program is a free software. You can modify it as you wish, though
+ *   we would just appreciate if you could keep the copyright notice on the
+ *   pages (including the engine version and link)  even if you should feel
+ *   free to add your own copyright if you modified and enhanced the code.
+ *
+ *   Please note though that, this software being copyrighted means that the
+ *   whole code (or part of it) is.  You should thus not sell any version of
+ *   this program, neither any modified version of it using part of the fol-
+ *   lowing code. Moreover, please do not use it for commercial purposes.
+ *
+ ***************************************************************************/
+
+
+echo "";
+exit();
+
+?>
