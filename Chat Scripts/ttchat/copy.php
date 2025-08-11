ef22c180b5863823d3d51b59d92e9bed161e0fad
me-suzy <me.suzana@gmail.com>
2025-08-11 13:54:49 +0300

    Add Chat Scripts/ttchat/copy.php

---
+++ Chat Scripts/ttchat/copy.php
@@ -0,0 +1,37 @@
+<? 
+/*
+This is TigerTom's Chat Room Software (TTChat).
+
+http://www.tigertom.com
+http://www.ttfreeware.com
+
+Copyright (c) 2005 T. O' Donnell
+
+Released under the GNU General Public License, with the
+following proviso: 
+
+That the HTML of hyperlinks to the authors' websites
+this software generates shall remain intact and unaltered, 
+in any version of this software you make.
+ 
+If this is not strictly adhered to, your licence shall be 
+rendered null, void and invalid.
+*/
+
+include("chat.php"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+<title><? echo chat_name; ?></title>
+<link rel="STYLESHEET" type="text/css" href="chatstyles.css">
+</head>
+
+<body style="margin: 0px">
+
+<p align="center">
+<!--// Removing or altering the hyperlinks to our sites in any way invalidates your licence to use this script //-->
+<FONT style="font-size: 7pt"><a href="http://www.ttfreeware.com">TTChat</a> originally developed for <a href="http://www.tigertom.com">TigerTom</a> by <a href="http://www.lpin.net">LP Informática</a></FONT></p>
+
+</body>
+</html>
