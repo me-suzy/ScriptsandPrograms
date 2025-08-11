5708a00342b59489b44293fc51614674ba34ae3c
me-suzy <me.suzana@gmail.com>
2025-08-11 10:21:20 +0300

    Add Affiliate Programs/overstock_dfs/man.php

---
+++ Affiliate Programs/overstock_dfs/man.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+<TITLE>Site Administration</TITLE>
+</HEAD>
+<body bgcolor='#CCCCFF'><font face="verdana">
+<h2> Overstock Datafeed Script </h2>
+<a href="usedcats.php" target="principal"><b>Categories</b></a><br>
+<a href="password.php" target="principal"><b>Change Password</b></a><br>
+<a href="overid.php" target="principal"><b>Overstock Id</b></a><br>
+<a href="logout.php" target="principal"><b>Log Out</b></a><br>
+</font>
+</body>
+</html>
