47e50edabfc779035783cee8a0ed81d642c3928b
me-suzy <me.suzana@gmail.com>
2025-08-11 13:31:27 +0300

    Add Calendars/calendarix_0_6_20050830/calendarix_0_6_20050830/cal_languages/c.months.php

---
+++ Calendars/calendarix_0_6_20050830/calendarix_0_6_20050830/cal_languages/c.months.php
@@ -0,0 +1,14 @@
+<?php
+$mth[1]="Ò»ÔÂ January";
+$mth[2]="¶þÔÂ February";
+$mth[3]="ÈýÔÂ March";
+$mth[4]="ËÄÔÂ April";
+$mth[5]="ÎåÔÂ May";
+$mth[6]="ÁùÔÂ June";
+$mth[7]="ÆßÔÂ July";
+$mth[8]="°ËÔÂ August";
+$mth[9]="¾ÅÔÂ September";
+$mth[10]="Ê®ÔÂ October";
+$mth[11]="Ê®Ò»ÔÂ November";
+$mth[12]="Ê®¶þÔÂ December";
+?>
