922ac6caf461e77d277f471cfae5e9ec2d6d3f65
me-suzy <me.suzana@gmail.com>
2025-08-11 10:00:28 +0300

    Add Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/chinese_gb2312/maintenance.lang.php

---
+++ Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/chinese_gb2312/maintenance.lang.php
@@ -0,0 +1,206 @@
+<?php // $Revision: 2.0 $
+
+
+
+/************************************************************************/
+
+/* phpAdsNew 2                                                          */
+
+/* ===========                                                          */
+
+/*                                                                      */
+
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+
+/* For more information visit: http://www.phpadsnew.com                 */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+
+
+// Main strings
+
+$GLOBALS['strChooseSection']			= "Ñ¡Ôñ²¿·Ö";
+
+
+
+
+
+// Priority
+
+$GLOBALS['strRecalculatePriority']		= "ÖØÐÂ¼ÆËãÓÅÏÈÈ¨";
+
+$GLOBALS['strHighPriorityCampaigns']		= "¸ßÓÅÏÈÈ¨ÏîÄ¿";
+
+$GLOBALS['strAdViewsAssigned']			= "·ÖÅäµÄ¹ã¸æ·ÃÎÊÊý";
+
+$GLOBALS['strLowPriorityCampaigns']		= "µÍÓÅÏÈÈ¨ÏîÄ¿";
+
+$GLOBALS['strPredictedAdViews']			= "Ô¤¶¨µÄ¹ã¸æ·ÃÎÊÊý";
+
+$GLOBALS['strPriorityDaysRunning']		= $phpAds_productname."¿ÉÒÔÔÚÏÖÔÚÓÐ{days}ÌìÊý¾ÝµÄ»ù´¡ÉÏÔ¤¹ÀÃ¿ÌìµÄÓÅÏÈÈ¨.";
+
+$GLOBALS['strPriorityBasedLastWeek']		= "ÔÚÉÏÖÜºÍ±¾ÖÜÊý¾Ý»ù´¡ÉÏµÄÔ¤¹ÀÖµ.";
+
+$GLOBALS['strPriorityBasedLastDays']		= "ÔÚÇ°¼¸ÌìÊý¾ÝµÄ»ù´¡ÉÏµÄÔ¤¹ÀÖµ.";
+
+$GLOBALS['strPriorityBasedYesterday']		= "ÔÚ×òÌìÊý¾ÝµÄ»ù´¡ÉÏµÄÔ¤¹ÀÖµ";
+
+$GLOBALS['strPriorityNoData']			= "ÏÖÔÚÃ»ÓÐ×ã¹»µÄÊý¾ÝÀ´¾«È·¹ÀËã´Ë¹ã¸æ·þÎñÆ÷½ñÌìµã»÷ÊýµÄÔ¤¼ÆÖµ¡£ËùÒÔÖ»ÔÚÊµÊ±Êý¾ÝµÄ»ù´¡ÉÏ·ÖÅäÓÅÏÈÈ¨.";
+
+$GLOBALS['strPriorityEnoughAdViews']		= "ÓÐ×ã¹»µÄ·ÃÎÊÊýÀ´ÍêÈ«Âú×ãÔ¤¶¨µÄËùÓÐ¸ßÓÅÏÈÈ¨µÄ·½°¸.";
+
+$GLOBALS['strPriorityNotEnoughAdViews']		= "»¹²»ÖªµÀÊÇ·ñÓÐ×ã¹»µÄÍÆ²¥ÊýÀ´ÍêÈ«Âú×ãÔ¤¶¨µÄËùÓÐ¸ßÓÅÏÈÈ¨µÄ·½°¸,ËùÒÔËùÓÐµÍÓÅÏÈÈ¨µÄ·½°¸ÒÑ¾­ÔÝÊ±Í£ÓÃÁË.";
+
+
+
+
+
+// Banner cache
+
+$GLOBALS['strRebuildBannerCache']		= "ÖØ½¨¹ã¸æ»º´æÇø";
+
+$GLOBALS['strBannerCacheExplaination']		= "¹ã¸æ»º´æÇø°üº¬ÁËÓÃÀ´ÏÔÊ¾¹ã¸æµÄHTML´úÂëµÄÒ»¸ö¸±±¾.Ê¹ÓÃ¹ã¸æ»º´æÇø¿ÉÒÔÌá¸ß¹ã¸æ·¢·ÅµÄËÙ¶È,ÒòÎª²»ÔÙÐèÒªÃ¿´Î·¢·ÅµÄÊ±ºò¶¼ÒªÉú³öÄÇ¸öÒ»´ÎHTML´úÂë.ÒòÎª´Ë¹ã¸æ»º´æÇø°üº¬ÁË".$phpAds_productname."ºÍ¹ã¸æµÄURLµØÖ·,ËùÒÔ»º´æÇøÐèÒªÔÚÃ¿´Î".$phpAds_productname."ÒÆ¶¯µ½ÍøÒ³·þÎñÆ÷µÄÁíÒ»¸öÎ»ÖÃµÄÊ±ºòÖØ½¨.";
+
+
+
+
+
+// Cache
+
+$GLOBALS['strCache']				= "·¢ËÍ»º´æÇø";
+
+$GLOBALS['strAge']				= "ÊÙÃü";
+
+$GLOBALS['strRebuildDeliveryCache']		= "ÖØ½¨·¢ËÍ»º´æÇø";
+
+$GLOBALS['strDeliveryCacheExplaination']	= "·¢ËÍ»º´æÇø¿ÉÒÔÌá¸ß¹ã¸æµÄ·¢·ÅËÙ¶È.·¢ËÍ»º´æÇø°üº¬Á¬½Óµ½´Ë°æÎ»µÄËùÓÐ¹ã¸æµÄÒ»¸ö¸±±¾,µ±¹ã¸æÊµ¼Ê·¢·Å¸øÓÃ»§µÄÊ±ºò¼õÉÙÁËÒ»Ð©Êý¾Ý¿âµÄ²éÑ¯.´Ë»º´æÇøÍ¨³£ÔÚÒ»¸ö°æÎ»»òÕß°æÎ»µÄÒ»¸ö¹ã¸æ¸Ä¶¯µÄÊ±ºòÖØ½¨,Ëü¿ÉÄÜ»á¹ýÆÚ.ËùÒÔ»º´æÇø»áÃ¿¸öÐ¡Ê±×Ô¶¯ÖØ½¨Ò»´Î,µ«ÄúÒ²¿ÉÒÔÊÖ¹¤ÖØ½¨.";
+
+$GLOBALS['strDeliveryCacheSharedMem']		= "ÏÖÔÚÊ¹ÓÃ¹²ÏíÄÚ´æÀ´´æ·Å·¢ËÍ»º´æÇø.";
+
+$GLOBALS['strDeliveryCacheDatabase']		= "ÏÖÔÚÊ¹ÓÃÊý¾Ý¿âÀ´´æ·Å·¢ËÍ»º´æÇø.";
+
+$GLOBALS['strDeliveryCacheFiles']		= "ÏÖÔÚÊ¹ÓÃÄú·þÎñÆ÷ÉÏµÄ¶à¸öÎÄ¼þÀ´´æ·Å·¢ËÍ»º´æÇø.";
+
+
+
+
+
+// Storage
+
+$GLOBALS['strStorage']				= "´æ´¢";
+
+$GLOBALS['strMoveToDirectory']			= "°Ñ´æ´¢ÔÚÊý¾Ý¿âÖÐµÄÍ¼Æ¬ÒÆ¶¯µ½Ò»¸öÄ¿Â¼";
+
+$GLOBALS['strStorageExplaination']		= "±¾µØ¹ã¸æÊ¹ÓÃµÄÍ¼Æ¬´æ´¢ÔÚÊý¾Ý¿â»òÕßÒ»¸öÄ¿Â¼ÖÐ.Èç¹ûÄú°ÑÍ¼Æ¬´æ´¢µ½Ò»¸öÄ¿Â¼,¼õÐ¡ÁËÊý¾Ý¿âµÄ¸ºÔØ»áÌá¸ßÔËÐÐËÙ¶È.";
+
+
+
+
+
+// Storage
+
+$GLOBALS['strStatisticsExplaination']		= "ÄúÒÑ¾­ÆôÓÃÁË<i>¼ò½à±¨±í</i>,µ«ÊÇÀÏµÄ±¨±í»¹ÊÇÏêÏ¸¸ñÊ½.ÄúÏë°ÑÏêÏ¸¸ñÊ½µÄ±¨±í×ª»»³ÉÐÂµÄ¼ò½à¸ñÊ½Âð?";
+
+
+
+
+
+// Product Updates
+
+$GLOBALS['strSearchingUpdates']			= "²é¿´Éý¼¶. ÇëÉÔºò...";
+
+$GLOBALS['strAvailableUpdates']			= "¿ÉÓÃÉý¼¶";
+
+$GLOBALS['strDownloadZip']			= "ÏÂÔØ(.zip)";
+
+$GLOBALS['strDownloadGZip']			= "ÏÂÔØ(.tar.gz)";
+
+
+
+$GLOBALS['strUpdateAlert']			= $phpAds_productname."ÓÐÐÂµÄ°æ±¾¿ÉÓÃ\\n\\nÄúÏëÖªµÀ¸ü¶à¹ØÓÚ´Ë´ÎÉý¼¶µÄÏûÏ¢Âð?";
+
+$GLOBALS['strUpdateAlertSecurity']		= $phpAds_productname."ÓÐÐÂµÄ°æ±¾¿ÉÓÃ\\n\\nÇ¿ÁÒÍÆ¼öÄú¾¡¿ìÉý¼¶,\\nÒòÎª´Ë´ÎÐÂ°æ±¾°üº¬ÁËÒ»¸ö»ò¶à¸ö°²È«²¹¶¡!";
+
+
+
+$GLOBALS['strUpdateServerDown']			= "ÒòÎªÎ´ÖªÔ­Òò²»ÄÜµÃµ½Éý¼¶ÐÅÏ¢,ÇëÉÔºòÔÙÊÔ.";
+
+
+
+$GLOBALS['strNoNewVersionAvailable']		= "ÄúµÄ".$phpAds_productname."ÊÇ×îÐÂ°æµÄ.ÏÖÔÚÃ»ÓÐ¿ÉÓÃµÄÉý¼¶°æ±¾.";
+
+
+
+$GLOBALS['strNewVersionAvailable']		= "<b>Ò»¸öÐÂ°æ±¾".$phpAds_productname."¿ÉÓÃ</b><br>ÍÆ¼öÄú°²×°´Ë´Î¸üÐÂ,ÒòÎª¿ÉÄÜÐÞ²¹ÁËÒ»Ð©´æÔÚµÄÎÊÌâºÍÔö¼ÓÁËÒ»Ð©ÐÂµÄ¹¦ÄÜ.¸ü¶àµÄÓÐ¹ØÉý¼¶µÄÐÅÏ¢Çë²Î¿¼ÏÂÃæµÄÏà¹ØÎÄµµ.";
+
+
+
+$GLOBALS['strSecurityUpdate']			= "<b>Ç¿ÁÒÍÆ¼öÄú¾¡¿ì°²×°´Ë´Î¸üÐÂ£¬ÒòÎª°üº¬ÁËÒ»Ð©°²È«²¹¶¡</b>ÄúÏÖÔÚÊ¹ÓÃµÄ".$phpAds_productname."µÄ°æ±¾µÄÂ©¶´¿ÉÄÜ±»¹¥»÷ºÍ²»°²È«.¸ü¶àµÄÓÐ¹ØÉý¼¶µÄÐÅÏ¢Çë²Î¿¼ÏÂÃæµÄÏà¹ØÎÄµµ.";
+
+
+
+$GLOBALS['strNotAbleToCheck']			= "<b>ÒòÎªÄúµÄ·þÎñÆ÷XMLÀ©Õ¹¹¦ÄÜ²»ÄÜÊ¹ÓÃ,".$phpAds_productname."ÎÞ·¨¼ì²éÊÇ·ñÓÐÐÂµÄ°æ±¾¿ÉÒÔÊ¹ÓÃ.</b>";
+
+
+
+$GLOBALS['strForUpdatesLookOnWebsite']		= "Èç¹ûÄúÏëÖªµÀÊÇ·ñÓÐÐÂµÄ°æ±¾¿ÉÓÃ,Çë·ÃÎÊÎÒÃÇµÄÍøÕ¾.";
+
+
+
+$GLOBALS['strClickToVisitWebsite']		= "Çëµã»÷ÕâÀïÀ´·ÃÎÊÎÒÃÇµÄÍøÕ¾.";
+
+$GLOBALS['strCurrentlyUsing']			= "ÄúÏÖÔÚÊ¹ÓÃµÄÊÇ";
+
+$GLOBALS['strRunningOn']			= "ÔËÐÐÔÚ";
+
+$GLOBALS['strAndPlain']				= "ºÍ";
+
+
+
+
+
+// Stats conversion
+
+$GLOBALS['strConverting']			= "×ª»»ÖÐ";
+
+$GLOBALS['strConvertingStats']			= "Í³¼ÆÊý¾Ý×ª»»ÖÐ...";
+
+$GLOBALS['strConvertStats']			= "×ª»»Í³¼ÆÊý¾Ý";
+
+$GLOBALS['strConvertAdViews']			= "·ÃÎÊÊýÒÑ¾­×ª»»Íê±Ï,";
+
+$GLOBALS['strConvertAdClicks']			= "µã»÷ÊýÒÑ¾­×ª»»Íê±Ï...";
+
+$GLOBALS['strConvertNothing']			= "Ã»ÓÐÊý¾Ý¿ÉÒÔ×ª»»...";
+
+$GLOBALS['strConvertFinished']			= "Íê±Ï...";
+
+
+
+$GLOBALS['strConvertExplaination']		= "ÄúÏÖÔÚÊ¹ÓÃ¼ò½à¸ñÊ½À´±£´æ±¨±í,µ«ÊÇ»¹ÓÐÒ»Ð©±¨±íÊÇÏêÏ¸¸ñÊ½.<br>ÏêÏ¸¸ñÊ½µÄ±¨±íÃ»ÓÐ×ª»»³É¼ò½à¸ñÊ½½«²»ÄÜÔÚÒ³ÃæÉÏÖ±½ÓÊ¹ÓÃ.<br>ÔÚ×ª»»ÄúµÄ±¨±íÖ®Ç°,±£´æÊý¾Ý¿âµÄÒ»¸ö±¸·Ý!<br>ÊÇ·ñÈ·ÈÏÒª°ÑÏêÏ¸¸ñÊ½µÄ±¨±í×ª»»³ÉÐÂµÄ¼ò½à¸ñÊ½?<br>";
+
+
+
+$GLOBALS['strConvertingExplaination']		= "ËùÓÐÊ£ÓàµÄÏêÏ¸¸ñÊ½µÄ±¨±íÕýÔÚ×ª»»³É¼ò½à¸ñÊ½<br>¸ù¾ÝÏêÏ¸¸ñÊ½µÄÊýÄ¿¶øÐèÒªÊ±¼ä²»Í¬£¬¿ÉÄÜÐèÒª¼¸·ÖÖÓ¡£ÔÚä¯ÀÀÆäËûÒ³ÃæÖ®Ç°Ò»¶¨ÒªµÈµ½×ª»»Íê³É¡£<br>ÏÂÃæÄú½«¿´µ½Êý¾Ý¿âÐÞ¸ÄµÄ¼ÇÂ¼.<br>";
+
+
+
+$GLOBALS['strConvertFinishedExplaination']  	= "Ê£ÓàµÄÏêÏ¸¸ñÊ½µÄ±¨±íÒÑ¾­×ª»»³É¹¦¡£Êý¾ÝÏÖÔÚÒªÔÙ´ÎÊ¹ÓÃ£¬ÏÂÃæÄú½«¿´µ½Êý¾Ý¿âÐÞ¸ÄµÄ¼ÇÂ¼<br>";
+
+
+
+
+
+?>
+
