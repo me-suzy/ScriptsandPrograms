6ff3cc54c7d1d8c0952059b9dd6c853d255b1a95
me-suzy <me.suzana@gmail.com>
2025-08-11 13:56:20 +0300

    Add Chat Scripts/x7chat1_3_6/X7Chat-v1.3.6B/Docs/Install.Xaraya/xarversion.php

---
+++ Chat Scripts/x7chat1_3_6/X7Chat-v1.3.6B/Docs/Install.Xaraya/xarversion.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * File: $Id:
+ */
+$modversion['name']           = 'X7chat';
+$modversion['id']             = '138';
+$modversion['version']        = '1.3.6B';
+$modversion['displayname']    = xarML('X7 Chat');
+$modversion['description']    = 'A PHP based chat room.';
+$modversion['credits']        = 'The X7 Group';
+$modversion['help']           = '';
+$modversion['changelog']      = '';
+$modversion['license']        = 'GNU GPL';
+$modversion['official']       = 0;
+$modversion['author']         = 'X7 Group';
+$modversion['contact']        = 'http://www.x7chat.com/';
+$modversion['admin']          = 0;
+$modversion['user']           = 1;
+$modversion['securityschema'] = array('chat::' => '::');
+$modversion['class']          = 'Content';
+$modversion['category']       = 'Complete';
+?>
