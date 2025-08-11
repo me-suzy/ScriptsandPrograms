014bf1a3eddb6ec5db569a388ca01baea99b10f0
me-suzy <me.suzana@gmail.com>
2025-08-11 14:17:01 +0300

    Add Content Management/attendance/admin/index.php

---
+++ Content Management/attendance/admin/index.php
@@ -0,0 +1,12 @@
+<?php
+
+//define("PB_CRYPT_LINKS" , 1);
+//require "config.php";
+define("_LIBPATH","./lib/");
+require_once _LIBPATH . "site.php";
+
+
+$site = new CSite("./site.xml",true);
+$site->Run();
+
+?>
