17847ddedca89004c36a6177e91e3ec62b45c3d2
me-suzy <me.suzana@gmail.com>
2025-08-11 13:44:53 +0300

    Add Chat Scripts/Chatterbox_V1-0/root/chat/menu.php

---
+++ Chat Scripts/Chatterbox_V1-0/root/chat/menu.php
@@ -0,0 +1,7 @@
+<HTML>
+<FRAMESET rows="*,0,0" border="1" FRAMEBORDER="0">
+<FRAME name="rtmenu" src="rt_menu.php" marginwidth=10 marginheight=0 noresize FRAMEBORDER=1 scrolling='no' border=1>
+<FRAME name="chatter" src="chatter.php?user=<?php print($_GET['user']); ?>" marginwidth=10 marginheight=0 noresize FRAMEBORDER=0 scrolling='no' border=1>
+<FRAME name="process" src="process.php?first=1" marginwidth=10 marginheight=0 noresize FRAMEBORDER=0 scrolling='no' border=1>
+</FRAMESET>
+</HTML>
