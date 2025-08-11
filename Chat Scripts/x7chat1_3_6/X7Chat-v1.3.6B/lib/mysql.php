e65b60354f13e30c3d69abc1d73ea83c847dd3b6
me-suzy <me.suzana@gmail.com>
2025-08-11 13:56:52 +0300

    Add Chat Scripts/x7chat1_3_6/X7Chat-v1.3.6B/lib/mysql.php

---
+++ Chat Scripts/x7chat1_3_6/X7Chat-v1.3.6B/lib/mysql.php
@@ -0,0 +1,55 @@
+<?
+/////////////////////////////////////////////////////////////// 
+//							 							     //
+//		X7 Chat Version 1.3.0 Beta		   				     //          
+//		Released February 2, 2004		     				 //
+//		Copyright (c) 2004 By the X7 Group	    			 //
+//		Website: http://www.x7chat.com		     			 //
+//							   							     //
+//		This program is free software.  You may	     		 //
+//		modify and/or redistribute it under the	    		 //
+//		terms of the included license as written    		 //
+//		and published by the X7 Group.		    			 //
+//							   							     //
+//		By using this software you agree to the	    		 //
+//		terms and conditions set forth in the	    		 //
+//		enclosed file "license.txt".  If you did     		 //
+//		not recieve the file "license.txt" please    	     //
+//		visist our website and obtain an official    		 // 
+//		copy of X7 Chat.		           				     //
+//							    							 //
+//		Removing this copyright and/or any other    		 //
+//		X7 Group or X7 chat copyright from any	    		 //
+//		of the files included in this distribution  		 //
+//		is forbidden and doing so will terminate    		 //
+//		your right to use this software.	     			 //
+//							     							 //
+///////////////////////////////////////////////////////////////
+?>
+<?
+
+function DoQuery($query){
+$q = @mysql_query($query);
+if(mysql_error() == ""){
+return $q;
+}else{
+return mysql_error();
+}
+}
+
+function Do_Fetch_Row($q){
+$row = @mysql_fetch_row($q);
+return $row;
+}
+
+function DoConnect($HOST,$USER,$PASS){
+$con = @mysql_connect($HOST,$USER,$PASS);
+return $con;
+}
+
+function DoSelectDb($DB){
+$dbcon = @mysql_select_db($DB);
+return $dbcon;
+}
+
+?> 
