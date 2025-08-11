f94ff49711613c65883862515344b2fc0be450a5
me-suzy <me.suzana@gmail.com>
2025-08-11 13:54:44 +0300

    Add Chat Scripts/ttchat/chat_msgs.php

---
+++ Chat Scripts/ttchat/chat_msgs.php
@@ -0,0 +1,23 @@
+<? 
+/*
+This is TigerTom's Chat Room Software (TTChat).
+
+http://www.tigertom.com
+http://www.ttfreeware.com
+
+Copyright (c) 2005 T. O' Donnell
+
+Released under the GNU General Public License, with the
+following proviso: 
+
+That the HTML of hyperlinks to the authors' websites
+this software generates shall remain intact and unaltered, 
+in any version of this software you make.
+ 
+If this is not strictly adhered to, your licence shall be 
+rendered null, void and invalid.
+*/
+
+include("chat.php");
+showchat(); ?>
+
