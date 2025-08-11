79573d5ed9c09d12e3547f71c3af89f3bc6d4c01
me-suzy <me.suzana@gmail.com>
2025-08-11 11:32:54 +0300

    Add Blog/Clever_Copy_V2_0a/Your_server_root/install/index.php

---
+++ Blog/Clever_Copy_V2_0a/Your_server_root/install/index.php
@@ -0,0 +1,6 @@
+<html><head><title>Clever Copy Install Wizard</title>
+<link rel="stylesheet" href="../personalities/cc.css" type="text/css"></head>
+<?php
+include "languages/default.php";
+include "install.php";
+?>
