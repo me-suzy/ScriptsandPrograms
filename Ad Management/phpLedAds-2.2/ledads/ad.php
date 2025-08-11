61b2f9c2bf4acb9894ecdbe9016eb8ec7a2ec4e3
me-suzy <me.suzana@gmail.com>
2025-08-11 10:06:52 +0300

    Add Ad Management/phpLedAds-2.2/ledads/ad.php

---
+++ Ad Management/phpLedAds-2.2/ledads/ad.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ *****************************************************************
+ *			       	phpLedAds 2.x
+ *
+ * This program is distributed as freeware. We are not
+ * responsible for any damages that the program causes	
+ * to your system. It may be used and modified free of 
+ * charge, as long as the copyright notice
+ * in the program that give me credit remain intact.
+ * If you find any bugs in this program. Please feel free 
+ * to report it to bugs@ledscripts.com.
+ * If you have any troubles installing this program. Please feel
+ * free to post a message on our Support Forum.
+ * Selling this script is absolutely forbidden and illegal.
+ *
+ *****************************************************************
+ *
+ *	               COPYRIGHT NOTICE:
+ *	
+ *	         Copyright 2004 Jon Coulter
+ *	
+ *	      Author:  Jon Coulter
+ *	      Web Site: http://www.ledscripts.com
+ *	      E-Mail: support@ledscripts.com
+ *	      Support: http://www.ledscripts.com/
+ *
+ *       This program is protected by the U.S. Copyright Law
+ *****************************************************************
+*/
+
+	// just require in the other one
+	require(dirname(__FILE__) . '/ledad.php');	
+?>
