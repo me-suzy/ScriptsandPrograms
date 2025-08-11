e52fa933a59542ed30e99ef3846ae0c4ff687067
me-suzy <me.suzana@gmail.com>
2025-08-11 13:21:14 +0300

    Add Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/skins/Modern/hook.inc.php

---
+++ Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/skins/Modern/hook.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* See class SB_HookInterface in ../../inc/page.inc.php for possible methods to be overridden.
+*/
+
+class SB_Hook extends SB_HookInterface
+{
+    function designedBy()
+    {
+        echo SB_T("Skin created by")?> <a href='http://brablc.com/' <?php echo SB_Page::target()?>>Ondrej Brablc</a><?php
+    }
+
+    function getStyle($styleID)
+    {
+        switch ($styleID)
+        {
+            case 'google_color_border': return 'B2CCF7';
+            case 'sf_logo_src': return 'http://sourceforge.net/sflogo.php?group_id=76467&amp;type=4';
+        }
+
+        return parent::getStyle($styleID);
+    }
+}
+
+?>
