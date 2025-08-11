c1a58bee62bf5ae65745eb4b8619607508a248ef
me-suzy <me.suzana@gmail.com>
2025-08-11 13:47:37 +0300

    Add Chat Scripts/EzChatBox-1.0/EzChatBox/config.php

---
+++ Chat Scripts/EzChatBox-1.0/EzChatBox/config.php
@@ -0,0 +1,43 @@
+<?php
+// Config file for EZChatBox
+// please configure using the variables below
+
+// EzChatBox location (no trailing slash)
+$EZChat_location = "EzChatBox";
+
+// Set the page titles
+$EZChat_title = "EZChatBox v1.0";
+$EZChat_ban_title = "EZChatBox - Ban List";
+$EZChat_up_title = "EZChatBox - Processing Message...";
+
+// Chatbox image title
+$EZChat_image = "logo.jpg";
+$EZChat_inc_tagline = "Yes";
+$EZChat_tagline = "The easy way to chat";
+
+// If EzChatBox is on a page of it's own this value
+// must be Yes. If it is included then the value should be No.
+// This decides whether to add HTML content at the top of
+// the page.
+$EZChat_html = "Yes";
+
+// Chatbox refresh page settings
+$EZChat_refresh = "Yes";
+$EZChat_time = "30";
+$EZChat_stringLength = "120";
+
+// Upload page time to refresh
+$EZChat_uptime = "3";
+
+// Time stamp hour correction
+$EZChat_timeChange = "0";
+
+// Admin email (for banned users to plead case)
+$EZChat_email = "email@yourserver.com";
+
+//files
+$EZChat_file_ban = "banned.ban";
+$EZChat_file_ips = "iplist.html";
+$EZChat_file_content = "dsp_chat_content.php";
+$EZChat_ban_relocate = "dsp_ban.php";
+?>
