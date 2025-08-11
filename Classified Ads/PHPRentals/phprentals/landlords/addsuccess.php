279d14ad0a9a87aa4a20a8aa3f8b8c6178ae6da6
me-suzy <me.suzana@gmail.com>
2025-08-11 14:01:23 +0300

    Add Classified Ads/PHPRentals/phprentals/landlords/addsuccess.php

---
+++ Classified Ads/PHPRentals/phprentals/landlords/addsuccess.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=ISO-8859-1">
+<TITLE>Success</TITLE>
+</HEAD>
+<BODY NOF="(MB=(DefaultMasterBorder, 0, 0, 0, 0), L=(addsuccessLayout, 600, 38))" BGCOLOR="#FFFFFF" TEXT="#000000" TOPMARGIN=0 LEFTMARGIN=0 MARGINWIDTH=0 MARGINHEIGHT=0>
+    
+<?
+include ("".$_SERVER['DOCUMENT_ROOT']."/phprentals/includes/header.php");
+?>
+<CENTER>
+	
+	<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 NOF=LY>
+        <TR VALIGN=TOP ALIGN=LEFT>
+            <TD WIDTH=600>
+                <TABLE ID="Table1" BORDER=0 CELLSPACING=0 CELLPADDING=3 WIDTH="100%">
+                    <TR>
+                        <TD WIDTH=594>
+                            <P><FONT SIZE="-1" FACE="Arial,Helvetica,Geneva,Sans-serif,sans-serif">Your listing has been added and will appear immediately. To add another listing </FONT><A HREF="/phprentals/addlisting.php"><FONT SIZE="-1" FACE="Arial,Helvetica,Geneva,Sans-serif,sans-serif">click here</FONT></A><FONT SIZE="-1" FACE="Arial,Helvetica,Geneva,Sans-serif,sans-serif">. To view your listing </FONT><A HREF="/phprentals/landlords/index.php"><FONT SIZE="-1" FACE="Arial,Helvetica,Geneva,Sans-serif,sans-serif">click here</FONT></A><FONT SIZE="-1" FACE="Arial,Helvetica,Geneva,Sans-serif,sans-serif">.</FONT></P>
+                        </TD>
+                    </TR>
+                </TABLE>
+            </TD>
+        </TR>
+    </TABLE>
+
+<?
+include ("".$_SERVER['DOCUMENT_ROOT']."/phprentals/includes/footer.php");
+?>
+
+</BODY>
+</HTML>
+ 
