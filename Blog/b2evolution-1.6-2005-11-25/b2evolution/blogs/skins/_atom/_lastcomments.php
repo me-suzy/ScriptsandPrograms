d8bc0326c6dffca369fe1d476acb6c6cb87e8c15
me-suzy <me.suzana@gmail.com>
2025-08-11 10:32:19 +0300

    Add Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/skins/_atom/_lastcomments.php

---
+++ Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/skins/_atom/_lastcomments.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * This template generates an Atom feed for the requested blog's latest comments
+ *
+ * See {@link http://atompub.org/2005/07/11/draft-ietf-atompub-format-10.html}
+ *
+ * This file is not meant to be called directly.
+ * It is meant to be called automagically by the main template (_main.php).
+ *
+ * This file is part of the b2evolution project - {@link http://b2evolution.net/}
+ *
+ * @copyright (c)2003-2005 by Francois PLANQUE - {@link http://fplanque.net/}
+ *
+ * @license http://b2evolution.net/about/license.html GNU General Public License (GPL)
+ * {@internal
+ * b2evolution is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * b2evolution is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with b2evolution; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * }}
+ *
+ * @package evoskins
+ * @subpackage atom
+ *
+ * {@internal Below is a list of authors who have contributed to design/coding of this file: }}
+ * @author fplanque: François PLANQUE - {@link http://fplanque.net/}
+ * }}
+ *
+ * @version $Id: _lastcomments.php,v 1.2 2005/11/23 19:38:32 fplanque Exp $
+ */
+if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+
+$CommentList = & new CommentList( $blog, "'comment'", $show_statuses, '',	'',	'DESC',	'',	20 );
+
+?>
+	<link rel="alternate" type="text/html" href="<?php $Blog->disp( 'lastcommentsurl', 'xml' ) ?>" />
+	<link rel="self" type="text/html" href="<?php $Blog->disp( 'comments_atom_url', 'xmlattr' ) ?>" />
+	<id><?php $Blog->disp( 'comments_atom_url', 'xmlattr' ) /* TODO: may need a regenerate_url() */ ?></id>
+	<generator uri="http://b2evolution.net/" version="<?php echo $app_version ?>"><?php echo $app_name ?></generator>
+	<updated><?php echo gmdate('Y-m-d\TH:i:s\Z'); ?></updated>
+	<?php while( $Comment = $CommentList->get_next() )
+	{ // Loop through comments: ?>
+	<entry>
+		<title type="text"><?php echo format_to_output( T_('In response to:'), 'xml' ) ?> <?php $Comment->Item->title( '', '', false, 'xml' ) ?></title>
+		<link rel="alternate" type="text/html" href="<?php $Comment->permalink() ?>" />
+		<author>
+			<name><?php $Comment->author( '', '#', '', '#', 'xml' ) ?></name>
+			<?php $Comment->author_url( '', '<uri>', "</uri>\n", false ) ?>
+		</author>
+		<id><?php $Comment->permalink() ?></id>
+    <published><?php $Comment->date( 'isoZ', true ); ?></published>
+		<updated><?php $Comment->date( 'isoZ', true ); ?></updated>
+		<content type="html"><![CDATA[<?php $Comment->content() ?>]]></content>
+	</entry>
+	<?php } // End of comment loop. ?>
