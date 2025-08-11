ce307d8963098583648f5f1e8c86ac00fea88801
me-suzy <me.suzana@gmail.com>
2025-08-11 10:44:00 +0300

    Add Blog/bdblog/bdblog/config/interface.php

---
+++ Blog/bdblog/bdblog/config/interface.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ Interface configuration
+*/
+
+// Messages
+define('MSG_PICK_A_TASK','Görev seçiniz...');
+define('MSG_SELECT_AREA','Alaný seçiniz');
+
+define('MSG_SITEPAGES_TITLE','Site sayfalarý');
+
+// Buttons
+define('BUTTON_OK','<input type="submit" value="  Tamam  ">');
+define('BUTTON_UPDATE','<input type="submit" value="  Güncelle  ">');
+define('BUTTON_DELETE','<input type="submit" value="  Sil  ">');
+
+// Icons
+define('ICON_ARROW','images/Arrow.gif');
+define('ICON_PATH','images/icons/Path16x16.gif');
+define('ICON_ARROWUP','images/icons/ArrowUp16x16.gif');
+define('ICON_ARROWDOWN','images/icons/ArrowDown16x16.gif');
+
+define('ICON_DEFAULT16X16','images/icons/CogWheel16x16.gif');
+
+define('ICON_PAGES16X16','images/icons/Pages16x16.gif');
+define('ICON_PAGES32X32','images/icons/Pages32x32.gif');
+define('ICON_PAGES48X48','images/icons/Pages48x48.gif');
+
+define('ICON_AUTHS16X16','images/icons/Auth16x16.gif');
+define('ICON_AUTHS32X32','images/icons/Auth32x32.gif');
+define('ICON_AUTHS48X48','images/icons/Auth48x48.gif');
+
+define('ICON_SCROLLING16X16','images/icons/ScrollingNews16x16.gif');
+define('ICON_SCROLLING32X32','images/icons/ScrollingNews32x32.gif');
+define('ICON_SCROLLING48X48','images/icons/ScrollingNews48x48.gif');
+
+define('ICON_FLASH16X16','images/icons/News16x16.gif');
+define('ICON_FLASH32X32','images/icons/News32x32.gif');
+define('ICON_FLASH48X48','images/icons/News48x48.gif');
+?>
