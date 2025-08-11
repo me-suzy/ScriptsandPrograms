575fb6d9b88b44dbf40d929fea67d65b2f078815
me-suzy <me.suzana@gmail.com>
2025-08-11 14:04:06 +0300

    Add Click Tracking/fclicksqllite/fclicksql/admin/inc/home.inc.php

---
+++ Click Tracking/fclicksqllite/fclicksql/admin/inc/home.inc.php
@@ -0,0 +1,20 @@
+<?
+// Anti Hack
+if(!stristr($_SERVER["PHP_SELF"], "admin.php")) header("Location: index.html");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Fast Click SQL <?=$CFG['SERIES']?> v<?=$CFG[VERSION]?> Administration</title>
+</head>
+
+<frameset rows="60,*">
+  <frame name="nav" src="admin.php?page=home_nav" scrolling="no" noresize frameborder="0" target="main">
+  <frame name="body" src="admin.php?page=home_body" frameborder="0">
+  <noframes>
+  <body bgcolor="#FFFFFF">
+  <p>Fast Click SQL requires you to use a frames enabled browser..</p>
+  </body>
+  </noframes>
+</frameset>
+</html>
