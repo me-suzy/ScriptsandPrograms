997dbc9704c6e5e7e6f5cda337e989b3d229d7ef
me-suzy <me.suzana@gmail.com>
2025-08-11 13:43:21 +0300

    Add Calendars/zentimetracking/ZenTimeTracking/topinstallmenu1.php

---
+++ Calendars/zentimetracking/ZenTimeTracking/topinstallmenu1.php
@@ -0,0 +1,9 @@
+<?
+print('<html><head><title>- - - Zen Time Tracking - - -</title>');
+print('<link rel="stylesheet" type="text/css" href="incl/style.css"></head>');
+print('<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0">');
+
+print('<table border="0" cellspacing="0" width="100%" bgcolor="#9999CC">');
+print('<tr><td align="center"  height="10%" valign="middle"><font face="Arial" color="#FFFFFF"><br><h3>Zen Time Tracking</h3></font></td></tr>');
+print('</table>');
+?>
