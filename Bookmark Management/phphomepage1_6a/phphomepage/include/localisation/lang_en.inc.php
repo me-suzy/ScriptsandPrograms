8306740262d30359f8a0f7c99fe3121bd5da0eaf
me-suzy <me.suzana@gmail.com>
2025-08-11 13:14:05 +0300

    Add Bookmark Management/phphomepage1_6a/phphomepage/include/localisation/lang_en.inc.php

---
+++ Bookmark Management/phphomepage1_6a/phphomepage/include/localisation/lang_en.inc.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * [en]File of English language
+ *
+ * @copyright    22/03/2004
+ * @since	     03/08/2001
+ * @version      1.5
+ * @module       lang_en
+ * @modulegroup  localisation
+ * @package      php_homepage
+ * @access	     public
+ * @author       Eric BLAS <webmaster@phphomepage.net>
+ */
+
+$lang_ConnexBase              = 'Problem with the database<br>'."\n".' Try later';
+$lang_ErrorNomFichier         = 'It is necessary to give a name of fichier.<br>For example:';
+$lang_Fichier                 = 'The file';
+$lang_Introuvable             = 'is untraceable.';
+$lang_Accueil                 = 'Welcome on';
+$lang_NvellePage              = 'Create or modify a homepage:';
+$lang_GenereTable             = 'Cliquer to create here your tables';
+$lang_ErreurNom               = 'You must re-enter a <b>NAME</b> in one of the fields of form!';
+$lang_Frame                   = 'This application functions with navigators managers the frames.';
+$lang_1                       = '1 . To create headings';
+$lang_2                       = '2 . To add links';
+$lang_3                       = '3 . To create your formatting';
+$lang_4                       = '4 . Finally to generate your homepage';
+$lang_Constituer1             = 'Your homepage \'';
+$lang_Constituer2             = ' \' is composed of headings and links according to:';
+$lang_Position                = 'position =';
+$lang_Realiser                = 'Realized with';
+$lang_CreerMEP                = 'Create a new formatting';
+$lang_ModifOK                 = 'The formatting was modified';
+$lang_CreerOK                 = 'The formatting was created';
+$lang_Rubrique                = 'Headings';
+$lang_NvelleRubrique          = 'Create a new heading';
+$lang_PlacerRubrique          = 'Place heading on the page:';
+$lang_SuppRubrique            = 'Remove a heading';
+$lang_ModifUneRubrique        = 'Modify a heading';
+$lang_Place                   = ' Place :';
+$lang_ChoixRubrique           = 'Choose the heading';
+$lang_ErrorRubNom             = 'You must enter a name!';
+$lang_ErrorRubPosition        = 'You must enter a position!';
+$lang_ErrorRubPositionSup     = 'You must enter a position lower than '.(($cfg_NbrLignes * $cfg_NbrColonnes) + 1).'!';
+$lang_RubOK                   = 'The heading was created';
+$lang_ErrorRubSupp            = 'You must choose a heading to be removed!';
+$lang_RubSupp                 = 'The heading was removed';
+$lang_ErrorRubModif           = 'You must choose a heading to be modified!';
+$lang_ErrorRubNomPlace        = 'You must seize a name and a place!';
+$lang_ModifRubOK              = 'The heading was modified';
+$lang_ErrorLienRub            = 'It is initially necessary to create at least <B>1</B> heading';
+$lang_LienChoix               = 'Choose a link';
+$lang_LienNew                 = 'Create a new link';
+$lang_LienSup                 = 'Remove a link';
+$lang_ModifUnLien             = 'Modify a link';
+$lang_Lien                    = 'Links';
+$lang_ErrorLienSup            = 'You must choose a link to be removed!';
+$lang_LienSupOK               = 'The link was removed';
+$lang_LienNom                 = 'You must seize a name!';
+$lang_LienURL                 = 'You must seize a URL!';
+$lang_LienRub                 = 'You must choose a heading!';
+$lang_ErrorLienModif          = 'You must choose a link !';
+$lang_LienOK                  = 'The link was created';
+$lang_ModifLienOK             = 'The link was modified';
+$lang_Target                  = 'Open a new window';
+$lang_Oui                     = 'Yes';
+$lang_Non                     = 'No';
+
+// Navigation
+$lang_PourNom                 = 'For the page of: ';
+$lang_AjoutRubrique           = 'Add headings';
+$lang_AjoutLiens              = 'Add links';
+$lang_ModifRubrique           = 'Modify headings';
+$lang_ModifLiens              = 'Modify links';
+$lang_MiseEnPage              = 'Formatting';
+$lang_AffHomepage             = 'Display your homepage';
+$lang_RetourIndex             = 'Choose a new homepage';
+
+// Formulaire
+$lang_Nom                     = 'Its name:';
+$lang_RVB                     = 'Red - Green - Blue';
+$lang_CoulFond                = 'Background color';
+$lang_CoulRub                 = 'Color of the headings';
+$lang_TailleRub               = 'Headings size';
+$lang_CoulLien                = 'Color of the links';
+$lang_TailleLien              = 'Links size';
+$lang_Police                  = 'Font';
+$lang_Title                   = 'Page title (optional)';
+
+// Bouton
+$lang_Creer                   = 'Create';
+$lang_Modifier                = 'Modify';
+$lang_Supprimer               = 'Remove';
+?>
