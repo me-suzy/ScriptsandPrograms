4221f3b993e424788ceed2543fe5927792039268
me-suzy <me.suzana@gmail.com>
2025-08-11 09:53:28 +0300

    Add Ad Management/advanced/status.js

---
+++ Ad Management/advanced/status.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+// **********************************************************************************//
+// ** Please leave this message alone, no one will be able to see it, except coders.*//
+// This script is copyrighted by PHP scripts, a Marsal Design Company.***************//
+// To get your own verison of this system, please do download it from our site*******//
+// located at http://www.free-php-scripts.net****************************************//
+// Script is free, no advertisement, no nothing....**********************************//
+// **********************************************************************************//
+
+function clearit(){
+var statusmsg=""
+window.status=statusmsg
+return true
+}
+
+function visit(){
+var statusmsg="Please Visit Sponsor!"
+window.status=statusmsg
+return true
+}
+
