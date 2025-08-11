522516ce24a40f132e27c4fc1dc49d40f3da2b7d
me-suzy <me.suzana@gmail.com>
2025-08-11 10:03:14 +0300

    Add Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/polish/userlog.lang.php

---
+++ Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/polish/userlog.lang.php
@@ -0,0 +1,35 @@
+<?php // $Revision: 2.1.2.2 $
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+/* For more information visit: http://www.phpadsnew.com                 */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+// Set translation strings
+
+$GLOBALS['strDeliveryEngine']				= "Dostarczanie";
+$GLOBALS['strMaintenance']				= "Utrzymanie";
+$GLOBALS['strAdministrator']				= "Administrator";
+
+
+$GLOBALS['strUserlog'] = array (
+	phpAds_actionAdvertiserReportMailed 		=> "Raport dla reklamodawcy {id} wys³any przez e-mail",
+	phpAds_actionPublisherReportMailed 		=> "Raport dla wydawcy {id} wys³any przez e-mail",
+	phpAds_actionWarningMailed			=> "Ostrze¿enie o deaktywacji kampanii {id} wys³ane przez e-mail",
+	phpAds_actionDeactivationMailed			=> "Informacja o deaktywacji kampanii {id} wys³ana przez e-mail",
+	phpAds_actionPriorityCalculation		=> "Priorytety przeliczone",
+	phpAds_actionPriorityAutoTargeting		=> "Cele kampanii przeliczone",
+	phpAds_actionDeactiveCampaign			=> "Kampania {id} deaktywowana",
+	phpAds_actionActiveCampaign			=> "Kampania {id} aktywowana",
+	phpAds_actionAutoClean				=> "Automatyczne czyszczenie bazy danych"
+);
+
+?>
