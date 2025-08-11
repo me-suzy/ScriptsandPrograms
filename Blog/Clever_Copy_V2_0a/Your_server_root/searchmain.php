785be5f801bd8fc2278dfb26c1cf7f5e1e7e17cd
me-suzy <me.suzana@gmail.com>
2025-08-11 11:22:50 +0300

    Add Blog/Clever_Copy_V2_0a/Your_server_root/searchmain.php

---
+++ Blog/Clever_Copy_V2_0a/Your_server_root/searchmain.php
@@ -0,0 +1,7 @@
+<?php
+echo "<br><center><form action='mainsearch.php' method='post'>";
+echo "$search_terms_label<br>";
+echo "<input name='searchterm' type='text' size='20' class='text'><br><br>";
+echo "<input type='submit' value=$search_button_label class='buttons'></form>";
+echo "</font>";
+?>
