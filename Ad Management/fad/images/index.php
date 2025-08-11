80ba30d9c513cac5381e84152c769222bd1bb2af
me-suzy <me.suzana@gmail.com>
2025-08-11 09:55:11 +0300

    Add Ad Management/fad/images/index.php

---
+++ Ad Management/fad/images/index.php
@@ -0,0 +1,13 @@
+<?
+//***********************************************************************************//
+//** Please leave this message alone, no one will be able to see it, except coders.**//
+// This script is copyrighted by PHP scripts, a Marsal Design Company.***************//
+// To get your own verison of this system, please do download it from our site*******//
+// located at http://www.free-php-scripts.net****************************************//
+// Script is free, no advertisement, no nothing....**********************************//
+//***********************************************************************************//
+
+//This is used as a security measure, no one will be able to access the image folder directly
+header("Location: ../index.php");
+exit();
+?>
