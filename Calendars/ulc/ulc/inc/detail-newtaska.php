e7b9619e7675d428277f7b9655a9036ac865ed2d
me-suzy <me.suzana@gmail.com>
2025-08-11 13:41:58 +0300

    Add Calendars/ulc/ulc/inc/detail-newtaska.php

---
+++ Calendars/ulc/ulc/inc/detail-newtaska.php
@@ -0,0 +1,21 @@
+<form class=new action="form.php?new=taska&ts=<? echo $CALPARAMS["date"]["ts"]; ?>" method=post>
+<table>
+<tr><td></td>
+</td><td class=t><? echo $CALANGUAGE["dayevent.newtaska"]; ?></td></tr>
+<tr><td class=g>
+<? echo $CALANGUAGE["formevent.title"]; ?>
+</td><td class=d>
+<input name=titre maxlength=64 size=32 value="<? if(isset($_COOKIE["titre"])) echo $_COOKIE["titre"]; ?>">
+</td></tr>
+<tr><td class=g>
+<? echo $CALANGUAGE["formevent.after"]; ?>
+</td><td class=d>
+<input id=debut name=debut maxlength=10 size=10 value="<? echo date("d/m/Y",$CALPARAMS["date"]["ts"]); ?>"> <i>(<? echo $CALANGUAGE["formevent.dateformat"]; ?>)</i>
+</td></tr>
+<tr><td>
+</td><td class=d>
+<input type=submit value="<? echo $CALANGUAGE["formevent.submit"]; ?>"> 
+<i>(<? echo $CALANGUAGE["formevent.submit.text"]; ?>)</i>
+</td></tr>
+</table>
+</form>
