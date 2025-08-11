2047d38796656155adb5d10dde84ec215fdfb39a
me-suzy <me.suzana@gmail.com>
2025-08-11 13:19:00 +0300

    Add Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/lt_LT/para.inc.php

---
+++ Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/lt_LT/para.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+$para = array();
+
+$para['index::tip'] = <<<_P
+Pasinaudokite Å¡iuo puslapiu, kad galÄtumÄt pasileisti ar Ä¯diegti SiteBar nuorodÅ³ serverÄ¯. rekomenduojama Ä¯traukti vienÄ iÅ¡ pateiktÅ³ nuorodÅ³ su "*" Å¾enkleliu Å¡alia Ä¯ savo nuorodÅ³ saraÅ¡Ä narÅ¡yklÄje, bet ne Å¡Ä¯ puslapÄ¯. Å Ä¯ puslapÄ¯ atversite paspaudÄ SiteBar lange ant virÅ¡uje esanÄio logotipo.
+_P;
+
+$para['index::any_browser'] = <<<_P
+Pasinaudoti <a title="SiteBar" href="%s/sitebar.php">SiteBar</a>* Å¡iame lange - ar atidaryti jÄ¯ naujame lange <a title="SiteBar" href="%s">SiteBar</a>-kaip* naujÄ narÅ¡yklÄs langÄ.
+_P;
+
+$para['index::ie_search'] = <<<_P
+Laikinai <a title="SiteBar" href="javascript:void(_search=open('%s/sitebar.php','_search'))"> SiteBar</a>* naudoti kaip paieÅ¡kos langÄ narÅ¡yklÄje. <p class='comment'>Patariama naudoti, kai neuÅ¾tenka teisiÅ³ Ä¯diegti prieÅ¡ tai siÅ«lytÄ Ä®diegÄjÄ.</p>
+_P;
+
+$para['index::copyright2'] = <<<_P
+AutorinÄs teisÄs © 2003,2004 <a href='http://brablc.com/'>OndÅej Brablc</a> ir <a href='http://sitebar.org/team.php'>SiteBar Team</a>. Palaikymas <a href='http://sourceforge.net/forum/forum.php?forum_id=261003'>forum</a>.
+_P;
+
+$para['command::contact'] = <<<_P
+Å½inutÄ:
+
+%s
+
+
+-- 
+SiteBar serveryje %s.
+_P;
+
+$para['command::contact_group'] = <<<_P
+GrupÄ: %s 
+Å½inutÄ: 
+
+%s 
+
+
+-- 
+SiteBar serveryje %s.
+_P;
+
+$para['command::purge_cache'] = <<<_P
+<h3>Ar tikrai norite paÅ¡alinti visas ikonas iÅ¡ laikinos atminties?</h3>
+_P;
+
+$para['usermanager::signup_info'] = <<<_P
+Vartotojas "%s" <%s> uÅ¾siregistravo JÅ«sÅ³ SiteBar esanÄiame %s.
+_P;
+
+?>
