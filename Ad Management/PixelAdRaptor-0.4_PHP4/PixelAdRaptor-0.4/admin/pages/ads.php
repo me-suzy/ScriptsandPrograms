c57d90c990a0ee9404cb0d6fa4d773e14557a672
me-suzy <me.suzana@gmail.com>
2025-08-11 10:18:55 +0300

    Add Ad Management/PixelAdRaptor-0.4_PHP4/PixelAdRaptor-0.4/admin/pages/ads.php

---
+++ Ad Management/PixelAdRaptor-0.4_PHP4/PixelAdRaptor-0.4/admin/pages/ads.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *
+ * Administration's ads section handler
+ *
+ * @date 2005-10-15
+ * @file ads.php
+ *
+ * Copyright (C) 2005  Karolis Tamutis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */ 
+
+/**
+ *
+ * Get approved ads
+ */
+$q = 'SELECT id, name, email, FLOOR(x / 10) + 1 as x, FLOOR(y / 10) + 1 as y, title, link, size, file, date, hits, price FROM ads WHERE active = 1 ORDER BY size DESC';
+$ads = $api['database']->getArray($api['database']->query($q));
+
+if (!empty($ads)) {
+
+    if (!get_magic_quotes_gpc()) {
+    
+        foreach ($ads as $key => $value) {
+        
+            $ads[$key] = array_map('stripslashes', $value);
+        
+        }
+    
+    }
+    
+    $api['template']->set('page', 'ads.tpl');
+    
+    $api['template']->set('ads', $ads);
+
+}
+
+if (isset($_GET['act']) && $_GET['act'] == 'delete' && isset($_GET['id']) && is_numeric($_GET['id'])) {
+
+    $q = 'SELECT file FROM ads WHERE id = "'.$_GET['id'].'"';
+    $file = $api['database']->getRow($api['database']->query($q));
+    
+    unlink('../images/'.$file['file']);
+    
+    $q = 'DELETE FROM ads WHERE id = "'.$_GET['id'].'"';
+    $api['database']->query($q);
+    
+    header('location: index.php?page=ads');
+
+}
+
+
+if (isset($_GET['act']) && $_GET['act'] == 'disable' && isset($_GET['id']) && is_numeric($_GET['id'])) {
+
+    $q = 'UPDATE ads SET active = 0 WHERE id = "'.$_GET['id'].'"';
+    $api['database']->query($q);
+    
+    header('location: index.php?page=ads');
+
+}
+
+?>
