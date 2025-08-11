5c7d87f47a1f2216cb9d07f533437fca8f068ef0
me-suzy <me.suzana@gmail.com>
2025-08-11 13:19:12 +0300

    Add Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/nl_NL/topic.inc.php

---
+++ Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/nl_NL/topic.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+$topic = array
+(
+'100' => 'Gebruikerstips',
+'101' => 'Drag & Drop',
+'102' => 'Favorieten toevoegen',
+'103' => 'Werkbalk functies',
+'200' => 'Commando&#39;s',
+'210' => 'Sessie management',
+'220' => 'Installatie & onderhoud',
+'230' => 'Account beheer',
+'240' => 'Gebruikers beheer',
+'250' => 'Groep beheer',
+'260' => 'Map instellingen',
+'270' => 'Favorieten instellingen',
+'300' => 'Technische info',
+'301' => 'Compatibilteit',
+'302' => 'Beveiligings methode',
+'303' => 'Opmaken',
+);
+
+?>
