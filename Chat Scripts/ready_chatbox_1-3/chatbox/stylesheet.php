d6cc2e80d76fc15dddf273a198e21e4abb893390
me-suzy <me.suzana@gmail.com>
2025-08-11 13:51:52 +0300

    Add Chat Scripts/ready_chatbox_1-3/chatbox/stylesheet.php

---
+++ Chat Scripts/ready_chatbox_1-3/chatbox/stylesheet.php
@@ -0,0 +1,51 @@
+<style type="text/css">
+<!--
+.topic {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+	color: #CCCCCC;
+	background-color: #003366;
+	font-weight: 500;
+	padding-top: 1px;
+	padding-bottom: 1px;
+	padding-left: 5px;
+}
+.cell_bg {
+	background-color: #0060BF;
+}
+.alt_a {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	color: #333333;
+	background-color: #006FDD;
+	border: 1px solid #333333;
+}
+.name {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	color: #333333;
+	font-weight: bold;
+	text-decoration: none;
+}
+.input {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 11px;
+	color: #333333;
+	background-color: #007AF4;
+	border: 1px solid #333333;
+}
+.href1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 13px;
+	color: #CCCCCC;
+	text-decoration: none;
+	font-weight: bold;
+}
+.log {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	font-style: italic;
+	color: #D7D7D7;
+}
+-->
+</style>
