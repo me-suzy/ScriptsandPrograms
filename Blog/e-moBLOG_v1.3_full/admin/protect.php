13162bd9192a84b8d39364325104a7810979c32b
me-suzy <me.suzana@gmail.com>
2025-08-11 11:37:49 +0300

    Add Blog/e-moBLOG_v1.3_full/admin/protect.php

---
+++ Blog/e-moBLOG_v1.3_full/admin/protect.php
@@ -0,0 +1,30 @@
+<?php
+/***************************************************************************
+ *   admin/protect.php
+ *
+ *   copyright © 2004 Axel Achten / e-motionalis.net
+ *   contact: thefiddler@e-motionalis.net
+ *   this file is a part of the " e-moBLOG " weblog engine
+ *
+ *   This program is a free software. You can modify it as you wish, though
+ *   we would just appreciate if you could keep the copyright notice on the
+ *   pages (including the engine version and link)  even if you should feel
+ *   free to add your own copyright if you modified and enhanced the code.
+ *
+ *   Please note though that, this software being copyrighted means that the
+ *   whole code (or part of it) is.  You should thus not sell any version of
+ *   this program, neither any modified version of it using part of the fol-
+ *   lowing code. Moreover, please do not use it for commercial purposes.
+ *
+ ***************************************************************************/
+
+session_start();
+ 
+if ($_SESSION['sessok'] != "authok" || !isset($_SESSION['sessok'])) {
+	
+	header("Location: index.php");
+    exit;
+    
+}
+
+?>
