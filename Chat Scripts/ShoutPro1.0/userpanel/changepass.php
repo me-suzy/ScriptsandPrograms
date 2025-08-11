3d9dd5cc0b187851d5144179c6be7b1c81298321
me-suzy <me.suzana@gmail.com>
2025-08-11 13:53:05 +0300

    Add Chat Scripts/ShoutPro1.0/userpanel/changepass.php

---
+++ Chat Scripts/ShoutPro1.0/userpanel/changepass.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ShoutPro 1.0 - User Panel - changepass.php
+ShoutPro is released under the GNU General Public Liscense.  A full copy of this license is included with this distribution under the file LICENSE.TXT.  By using ShoutPro or the source code, you acknowledge you have read and agree to the license.
+
+This file is changepass.php.  It is part of the User Panel addon to ShoutPro.  There is no need to modify anything in this file.  All modifications should be done in the file upconfig.php.
+*/
+
+//upconfig.php is essential for using this addon script.
+require("upconfig.php");
+
+//include.php calls default ShoutPro functions and settings into effect.  ShoutPro cannot run without it.
+require("$path/include.php");
+
+if ($userpanelon != "yes"){
+	die ("Sorry the user panel has not been enabled in config.php");
+}
+?>
+
+
+<html><head><title>Change your Password</title>
+
+<style type="text/css">
+<!--
+<?php echo($textboxstyle); ?>
+-->
+</style>
+
+<!-- Begin Link Styles -->
+<style type="text/css">
+ a:link{<?php echo($link_style); ?>}
+ a:visited{<?php echo($link_visited_style); ?>}
+ a:hover{<?php echo($link_hover_style); ?>}   
+ a:active{<?php echo($link_active_style); ?>}
+</style>
+<!-- End Link Styles -->
+
+<!-- Begin Scrollbar Styles -->
+<style>  
+<!-- 
+<?php
+if($scrollbar_styles_on != "off"){
+echo("BODY{ scrollbar-face-color:".$scrollbar_face_color."; scrollbar-arrow-color:".$scrollbar_arrow_color."; scrollbar-track-color:".$scrollbar_track_color."; scrollbar-shadow-color:".$scrollbar_shadow_color."; scrollbar-highlight-color:".$scrollbar_highlight_color."; scrollbar-3dlight-color:".$scrollbar_3dlight_color."; scrollbar-darkshadow-Color:".$scrollbar_darkshadow_color."; }");
+}
+?>
+-->   
+</style>    
+<!-- End Scrollbar Styles -->
+
+</head>
+<?php
+echo("<body bgcolor=".$bgcolor." text=".$textcolor." bottommargin=".$bottommargin." topmargin=".$topmargin." leftmargin=".$leftmargin." rightmargin=".$rightmargin.">");
+?>
+
+<font style='font-size: <?php echo($textsize); ?>pt' face='<?php echo($fontface); ?>'>
+You are about to change your password.  Please enter your username, current password, and desired new password.<p>
+<form name="register" action="dochange.php" method="POST">
+<?php
+if (!$_COOKIE['shoutpro_username']){
+	echo("Username: <input class='textbox' name='name' type='text' size='10' value=''><br />\n"); 
+} else {
+	echo("Username: <input class='textbox' name='name' type='text' size='10' value='".$_COOKIE['shoutpro_username']."'><br />\n"); 
+	}
+?>
+Old Password: <input class='textbox' type="password" name="oldpass"><br />
+New Password: <input class='textbox' type="password" name="newpass1"><br />
+Confirm New Password: <input class='textbox' type="password" name="newpass2"><br />
+<input class='textbox' type='submit' value='Register'>
+</form>
+<p><a href="index.php">User Panel Home</a>::<a href="javascript:window.close()">Close Window</a>
