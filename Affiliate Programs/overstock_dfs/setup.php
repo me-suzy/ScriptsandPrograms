c3b3c4c17fb79ea8f699fe6b4ca01cbbd4258df2
me-suzy <me.suzana@gmail.com>
2025-08-11 10:21:28 +0300

    Add Affiliate Programs/overstock_dfs/setup.php

---
+++ Affiliate Programs/overstock_dfs/setup.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<style type="text/css">
+body {font-family: arial}
+</style></head>
+
+<body bgcolor='#CCCCFF'>
+
+<form action=dbsettings.php method='post'><center>
+<table>
+<tr><td ><b>Database </b></td><td><input type='text' name='database'></td></tr>
+<tr><td ><b>Database Username</b></td><td><input type='text' name='username'></td></tr>
+<tr><td ><b>Database Password</b></td><td><input type='password' name='password'></td></tr>
+<tr><td ><b>Administration Username</b></td><td><input type='text' name='adusername'></td></tr>
+<tr><td ><b>Administration Password</b></td><td><input type='password' name='adpassword'></td></tr>
+
+<tr><td colspan=2><input type='submit' name='Submit'></td></tr>
+</table>
+</form>
