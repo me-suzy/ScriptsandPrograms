95f06a544fd968e80c49db40255ad7b51487f1cb
me-suzy <me.suzana@gmail.com>
2025-08-11 13:13:36 +0300

    Add Bookmark Management/phphomepage1_6a/phphomepage/local.inc.php

---
+++ Bookmark Management/phphomepage1_6a/phphomepage/local.inc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * [fr]Fichier qui contient divers paramètres locaux
+ * [en]File which contains various local parameters
+ *
+ * @copyright    20/03/2004
+ * @since	     09/08/2001
+ * @version      1.6
+ * @module       local
+ * @modulegroup  librairie
+ * @package      php_homepage
+ * @access	     public
+ * @author       Eric BLAS <eric.blas@blas-intra.net>
+ */
+/**
+ * [fr]Variable pour fixer le dossier d'include
+ * Laisser vide si vous pouvez utiliser un fichier .htaccess, sinon mettre les chemins réels
+ * [en]Variable to fix the file of include
+ * To leave empty if you could use a .htaccess file, if not put the real path
+ *
+ * @const LOCAL_INCLUDE 
+ */
+define('LOCAL_INCLUDE', './include/');
+?>
