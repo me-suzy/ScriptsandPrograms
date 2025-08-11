4d30e5ee4476acd92ab80d6cdf53e682645fb6c7
me-suzy <me.suzana@gmail.com>
2025-08-11 14:02:57 +0300

    Add Click Tracking/downloadpro10/linkdemo.php

---
+++ Click Tracking/downloadpro10/linkdemo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>LinkDemo</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<p>This is an example download page, to demonstrate how to link to download pro supported files, and how to display their counts.</p>
+<p><a href="download.php?file=1">Download my file now!</a> [<?php include("http://explodingpanda.com/test/download.php?showme=1");?>] 
+Downloads so far. </p>
+</body>
+</html>
