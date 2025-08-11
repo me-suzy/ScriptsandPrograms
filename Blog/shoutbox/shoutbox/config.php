82d44a1f9d7eda6f99af3c95c302b920369151a1
me-suzy <me.suzana@gmail.com>
2025-08-11 12:57:27 +0300

    Add Blog/shoutbox/shoutbox/config.php

---
+++ Blog/shoutbox/shoutbox/config.php
@@ -0,0 +1,26 @@
+<?php 
+
+   //Edit everything below to match your mySQL database configuration.
+
+   //database host
+   $db_host = "localhost"; 
+
+   //database name
+   $db_name = "database_name_here";
+
+   //database user
+   $db_user = "database_user_here"; 
+
+   //database user password
+   $db_pass = "database_user_password_here";  
+
+
+   //Shoutbox Configuration
+
+   //Message length limit (in characters)
+   $max_length = 150;
+
+   //Shoutbox message limit (Dont set this too high!)
+   $dmessage = 20;
+
+?> 
