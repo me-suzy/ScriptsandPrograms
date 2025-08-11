05de8ef958858c1a422e5a896b43481e1f2affcb
me-suzy <me.suzana@gmail.com>
2025-08-11 13:36:06 +0300

    Add Calendars/calogic122/calogic122/admin/settings_setup.php

---
+++ Calendars/calogic122/calogic122/admin/settings_setup.php
@@ -0,0 +1,20 @@
+<?php
+
+############################################################
+#
+#  THIS IS A DYNAMICALLY CREATED FILE
+#  DO NOT ALTER ANYTHING WITH IN THIS FILE
+#  TO ALTER VALUES IN THIS FILE USE THE SETUP.PHP PROGRAM
+#
+############################################################
+
+/*** SET VARIABLES HERE *****/ 
+$dbhost = "my.sqlhost.com";
+$dbname = "calogic";
+$dbuser = "calogic";
+$dbpass = "mypassword";
+
+$tabpre = "clc";
+$idxfile = "index.php";
+
+?>
