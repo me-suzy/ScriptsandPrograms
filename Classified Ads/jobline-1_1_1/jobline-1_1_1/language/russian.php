8060449b50c119f612d8b4133a84814d6588cc75
me-suzy <me.suzana@gmail.com>
2025-08-11 14:00:35 +0300

    Add Classified Ads/jobline-1_1_1/jobline-1_1_1/language/russian.php

---
+++ Classified Ads/jobline-1_1_1/jobline-1_1_1/language/russian.php
@@ -0,0 +1,258 @@
+<?php
+// Russian language file for Jobline
+//
+// Distributed under the terms of the GNU General Public License
+// This software may be used without warrany provided and
+// copyright statements are left intact.
+
+// Frontend language strings
+DEFINE('_JL_ERROR', 'Îøèáêà');
+DEFINE('_JL_NOSUBCATS', 'Íåò ïîäêàòåãîðèé.');
+DEFINE('_JL_CATEGORIES', 'Êàòåãîðèè');
+DEFINE('_JL_NOTFOUND', 'Ñòðàíèöà íå íàéäåíà.');
+DEFINE('_JL_HACKING_ATTEMPT', 'Ïîïûòêà âçëîìà, ïðîãðàììà áóäåò çàêðûòà.');
+DEFINE('_JL_JOBTYPE_FULLTIME', 'Ïîëíûé ðàáî÷èé äåíü');
+DEFINE('_JL_JOBTYPE_PARTTIME', 'Íåïîëíûé ðàáî÷èé äåíü');
+DEFINE('_JL_JOBTYPE_INTERNSHIP', 'Ïðàêòèêà/Ñòàæèðîâêà');
+DEFINE('_JL_LIST_JOBPOSTINGS', 'Îáúÿâëåíèÿ î ðàáîòå');
+DEFINE('_JL_LIST_DESCRIPTION', 'Ñïèñîê âñåõ îáúÿâëåíèé î ðàáîòå íà íàñòîÿùèé ìîìåíò.');
+DEFINE('_JL_JOBID', 'ID&nbsp;ðàáîòû');
+DEFINE('_JL_DATE_POSTED', 'Äàòà ïóáëèêàöèè');
+DEFINE('_JL_JOB_TITLE', 'Íàçâàíèå ðàáîòû');
+DEFINE('_JL_COMPANY_NAME', 'Ôèðìà/Êîìïàíèÿ');
+DEFINE('_JL_JOBTYPE', 'Òèï ðàáîòû');
+DEFINE('_JL_DATEPOSTED', 'Äàòà ïóáëèêàöèè');
+DEFINE('_JL_COMPANYINFO', 'Èíôîðìàöèÿ î ôèðìå/êîìïàíèè');
+DEFINE('_JL_DESCRIPTION', 'Îïèñàíèå ðàáîòû');
+DEFINE('_JL_HOWTOAPPLY', 'Êàê ïîäàòü çàÿâëåíèå íà ðàáîòó');
+DEFINE('_JL_EMAILRESUMETO', 'Email äëÿ ðåçþìå');
+DEFINE('_JL_WEBSITE', 'Âåáñàéò');
+DEFINE('_JL_APPLYFORJOB', 'Ïîñëàòü çàÿâëåíèå íà ðàáîòó');
+DEFINE('_JL_FIRSTNAME', 'Èìÿ');
+DEFINE('_JL_LASTNAME', 'Ôàìèëèÿ');
+DEFINE('_JL_ADDRESS1', 'Àäðåñ 1');
+DEFINE('_JL_ADDRESS2', 'Àäðåñ 2');
+DEFINE('_JL_CITY', 'Ãîðîä');
+DEFINE('_JL_STATE', 'Øòàò (ÑØÀ)');
+DEFINE('_JL_ZIPCODE', 'Èíäåêñ');
+DEFINE('_JL_COVER_LETTER', 'Òåêñò çàÿâëåíèÿ');
+DEFINE('_JL_RESUME', 'Ðåçþìå');
+DEFINE('_JL_SEND_APPLICATION', 'Îòïðàâèòü');
+DEFINE('_JL_RECEIVED_APPLICATION', 'Âû ïîëó÷èëè ðåçþìå(çàÿâëåíèå) íà ðàáîòó:');
+DEFINE('_JL_APPLICATION', 'Çàÿâëåíèå');
+DEFINE('_JL_APPLICATION_SUBJECT', 'Âû ïîëó÷èëè çàÿâëåíèå íà ðàáîòó');
+DEFINE('_JL_NOSUCHJOB', 'Ðàáîòà íå íàéäåíà.');
+DEFINE('_JL_THANKYOU', 'Áëàãîäàðèì Âàñ');
+DEFINE('_JL_THANKYOU_MESSAGE', 'Áëàãîäàðèì Âàñ çà çàÿâëåíèå íà ðàáîòó.');
+DEFINE('_JL_GOBACKTOJOB', 'Âåðíóòüñÿ ê îáúÿâëåíèþ î ðàáîòå');
+DEFINE('_JL_GOTOLIST', 'Âåðíóòüñÿ ê ñïèñêó îáúÿâëåíèé');
+DEFINE('_JL_PREVIEW_JOB', 'Ïðåäïðîñìîòð îáúÿâëåíèÿ');
+DEFINE('_JL_CANCEL_JOB', 'Îòìåíà');
+DEFINE('_JL_QUALIFICATIONS', 'Îïûò/Êâàëèôèêàöèÿ');
+DEFINE('_JL_COMPENSATION', 'Çàð.ïëàòà');
+DEFINE('_JL_CONTACTINFO', 'Èíôîðìàöèÿ äëÿ êîíòàêòà');
+DEFINE('_JL_CONTACTPHONE', 'Òåëåôîí äëÿ êîíòàêòà');
+DEFINE('_JL_EMAIL', 'Email äëÿ êîíòàêòà');
+DEFINE('_JL_SAVEOREDIT', 'Ñîõðàíèòü èëè ïðîäîëæèòü ðåäàêòèðîâàíèå?');
+DEFINE('_JL_SAVE_JOB', 'Ñîõðàíèòü îáúÿâëåíèå î ðàáîòå');
+DEFINE('_JL_EDIT_JOB', 'Ðåäàêòèðîâàòü îáúÿâëåíèå î ðàáîòå');
+DEFINE('_JL_PREVIEW_JOBPOSTING', 'Ïðåäïðîñìîòð îáúÿâëåíèÿ î ðàáîòå');
+DEFINE('_JL_JOBPOSTINGSENT', 'Áëàãîäàðèì Âàñ çà îáúÿâëåíèå î ðàáîòå. Íàøè ìîäåðàòîðû îïóáëèêóþò Âàøå îáúÿâëåíèå â êðàò÷àéøèå ñðîêè.');
+DEFINE('_JL_GOTOSEARCH', 'Ïîèñê ðàáîòû');
+DEFINE('_JL_POSTJOB', 'Äîáàâèòü ðàáîòó(äëÿ ðàáîòàäàòåëÿ)');
+DEFINE('_JL_ENTERJOBINFO', 'Ââåäèòå èíôîðìàöèþ î ðàáîòå');
+DEFINE('_JL_NEW_SEARCH', 'Ïîèñê îáúÿâëåíèÿ î ðàáîòå');
+DEFINE('_JL_NOSEARCHWORD', 'Ââåäèòå ñëîâî äëÿ ïîèñêà.');
+DEFINE('_JL_SEARCH', 'Ïîèñê');
+DEFINE('_JL_NO_RESULTS', 'Íåò ðåçóëüòàòîâ');
+DEFINE('_JL_NOTHING_FOUND', 'Ïî Âàøåìó ïîèñêó íè÷åãî íå íàéäåíî.');
+DEFINE('_JL_SEARCH_RESULTS', 'Ðåçóëüòàòû ïîèñêà');
+DEFINE('_JL_SEARCH_DESCRIPTION', 'Ïî Âàøåìó çàïðîñó íàéäåíî ïóíêòîâ: ');
+DEFINE('_JL_BACKTOSEARCH', 'Âåðíóòüñÿ â ïîèñê');
+DEFINE('_JL_TERMSOFSERVICE', 'Óñëîâèÿ óñëóã');
+DEFINE('_JL_NOTACCESIBLE', 'Ýòà ðàáîòà áîëüøå íå äîñòóïíà.');
+DEFINE('_JL_NOTOWNER', 'Âû íå èçäàòåëü-âëàäåëåö ýòîãî ïóíêòà' );
+DEFINE('_JL_NOTLOGGEDIN', 'Âàì íåîáõîäèìî çàéòè êàê çàðåãèñòðèðîâàííûé ïîëüçîâàòåëü äëÿ ðåäàêòèðîâàíèÿ ýòîé ïîçèöèè.');
+DEFINE('_JL_NEWJOBPOSTED', 'Íîâîå îáúÿâëåíèå î ðàáîòå îòïðàâëåíî');
+DEFINE('_JL_NEWJOB_MSG', 'Íîâîå îáúÿâëåíèå î ðàáîòå \'<<jobtitle>>\' îòïðàâëåíî. Àäìèíèñòðàòîð âåáñàéòà <<livesite>> ðàññìîòðèò îáúÿâëåíèå â êðàò÷àéøèé ñðîê.');
+DEFINE('_JL_JOBSTATUS_SOURCING', 'Èùåì / Òðåáóåòñÿ');
+DEFINE('_JL_JOBSTATUS_INTERVIEWING', 'Èíòåðâüþ');
+DEFINE('_JL_JOBSTATUS_CLOSED', 'Çàêðûòî äëÿ íîâûõ ïðåòåíäåíòîâ');
+DEFINE('_JL_JOBSTATUS_FINALISTS', 'Ôèíàëèñò èçáðàí');
+DEFINE('_JL_JOBSTATUS_PENDING', 'Â ïðîöåññå ðàññìîòðåíèÿ');
+DEFINE('_JL_JOBSTATUS_HOLD', 'Çàäåðæàòü');
+DEFINE('_JL_JOBSTATUS', 'Ñòàòóñ îáúÿâëåíèÿ');
+DEFINE('_JL_LIST_INTERNSHIPS', 'Ïðàêòèêà/Ñòàæèðîâêà');
+DEFINE('_JL_LIST_INTERNSHIPS_DESC', 'Ñïèñîê ñòàæèðîâêè/ïðàêòèêè äîñòóïíûé íà íàñòîÿùèé ìîìåíò.');
+DEFINE('_JL_INTERNSHIPID', 'ID');
+DEFINE('_JL_INTERNSHIP_TITLE', 'Íàçâàíèå ïðàêòèêè/ñòàæèðîâêè');
+DEFINE('_JL_GOTOSEARCH_INTERN', 'Ïîèñê ïðàêòèêè/ñòàæèðîâêè');
+DEFINE('_JL_POSTINTERNSHIP', 'Îáúÿâèòü î ïðàêòèêå/ñòàæèðîâêå');
+DEFINE('_JL_POSTINTERNSHIP', 'Post an internship');
+DEFINE('_JL_JOBAPPLICATION', 'Application for job');
+DEFINE('_JL_NOITEMS', 'No items found');
+DEFINE('_JL_ERRORSETTMPL', 'Couldn\'t set template');
+DEFINE('_JL_JOBLOCATION', 'Location');
+DEFINE('_JL_JOBREFERENCE', 'Reference');
+DEFINE('_JL_JOBDURATION', 'Duration');
+DEFINE('_JL_DAYTELEPHONE', 'Daytime Telephone');
+DEFINE('_JL_EVENINGTELEPHONE', 'Evening Telephone');
+DEFINE('_JL_APPLICANTEMAIL', 'Email');
+DEFINE('_JL_JOBPOSTINGNOTALLOWED', 'You are not allowed to post jobs.');
+DEFINE('_JL_SUBMITYOURCV', 'Submit your CV');
+DEFINE('_JL_ACCESS_DENIED', 'You are not allowed to view this resource. Please login or register.');
+DEFINE('_JL_START_DATE', 'Start Date');
+
+// Admin footer
+DEFINE('_OJ_VERSION', 'Âåðñèÿ');
+DEFINE('_OJ_CREATED_BY', 'Ýòîò êîìïîíåíò ñîçäàë ');
+DEFINE('_OJ_VISIT', 'Ïîñåòèòü');
+DEFINE('_OJ_EXCITING_ADDONS', 'äëÿ ñêà÷èâàíèÿ äðóãèõ äîáàâëåíèé ê ïðîãðàììå.');
+
+// Admin strings
+DEFINE('_JL_A_YES', 'Äà');
+DEFINE('_JL_A_NO', 'Íåò');
+DEFINE('_JL_A_CONFIG_SAVED', 'Ôàéë  êîíôèãóðàöèè ñîõðàí¸í');
+DEFINE('_JL_A_CONFIGURATION', 'Êîíôèãóðàöèÿ Jobline');
+DEFINE('_JL_A_INFORMATION', 'Èíôîðìàöèÿ');
+DEFINE('_JL_A_AREYOUSURE', 'Âû óâåðåíû?');
+DEFINE('_JL_A_SETTINGS', 'Óñòàíîâêè');
+DEFINE('_JL_A_SELECTDELITEM', 'Âûáåðèòå ïóíêò äëÿ óäàëåíèÿ');
+DEFINE('_JL_A_SELECTITEM', 'Âûáåðèòå ïóíêò äëÿ');
+DEFINE('_JL_A_NOTITLE', 'Ââåäèòå íàçâàíèå');
+DEFINE('_JL_A_ADDING', 'Äîáàâèòü');
+DEFINE('_JL_A_EDITING', 'Ðåäàêòèðîâàòü');
+DEFINE('_JL_A_JOBPOSTING', 'Îáúÿâëåíèå î ðàáîòå');
+DEFINE('_JL_A_CONTENT', 'Ðåäàêòèðîâàòü ñîäåðæèìîå');
+DEFINE('_JL_A_ENTER_TITLE', 'Íàçâàíèå');
+DEFINE('_JL_A_ENTER_JOBTYPE', 'Òèï ðàáîòû');
+DEFINE('_JL_A_ENTER_DESCRIPTION', 'Îïèñàíèå');
+DEFINE('_JL_A_ENTER_QUALIFICATIONS', 'Îïûò/Êâàëèôèêàöèÿ');
+DEFINE('_JL_A_ENTER_COMPENSATION', 'Çàð.ïëàòà');
+DEFINE('_JL_A_SHOW_COMPENSATION' ,'Ïîêàçàòü çàð.ïëàòó?');
+DEFINE('_JL_A_HOW_TO_APPLY', 'Êàê ïîäàòü çàÿâëåíèå');
+DEFINE('_JL_A_LISTJOBS', 'Îáúÿâëåíèÿ î ðàáîòå');
+DEFINE('_JL_A_DISPLAY', 'Ïîêàçàòü #');
+DEFINE('_JL_A_JOBPOSTINGREJECTED', 'Âàì îòêàçàííî â ðàçìåùåíèè îáúÿâëåíèå î ðàáîòå');
+DEFINE('_JL_A_REJHI', 'Çäðàâñòâóéòå');
+DEFINE('_JL_A_REJECTION', 'Çäðàâñòâóéòå <<contactname>>,
+
+Âàì îòêàçàííî â ðàçìåùåíèè îáúÿâëåíèÿ î ðàáîòå ñ íàçâàíèåì: "<<jobtitle>>" íà âåáñàéòå: <<sitename>>. Åñëè ó Âàñ âîçíèêíóò âîïðîñû ïî ýòîìó ïîâîäó, ïîæàëóéñòà ñâÿæèòåñü ñ íàøèì àäìèíèñòðàòîðîì <<adminemail>>.
+
+Ñ óâàæåíèåì àäìèíèñòðàöèÿ âåáñàéòà <<sitename>>.');
+DEFINE('_JL_A_JOBPOSTINGAPPROVED', 'Âàøå îáúÿâëåíèå ðàçìåùåíî íà âåáñàéòå');
+DEFINE('_JL_A_APPROVE', 'Çäðàâñòâóéòå <<contactname>>,
+
+Âàøå îáúÿâëåíèå ñ íàçâàíèåì "<<jobtitle>>" ðàçìåùåíî íà âåáñàéòå <<sitename>>. Åñëè ó Âàñ âîçíèêíóò âîïðîñû ïî ýòîìó ïîâîäó, ïîæàëóéñòà ñâÿæèòåñü ñ íàøèì àäìèíèñòðàòîðîì <<adminemail>>.
+
+Ñ óâàæåíèåì àäìèíèñòðàöèÿ âåáñàéòà <<sitename>>.');
+DEFINE('_JL_A_MAILFROMNAME', 'Èìÿ äëÿ àâòîîòâåòà');
+DEFINE('_JL_A_MAILFROMADDRESS', 'Email äëÿ àâòîîòâåòà');
+DEFINE('_JL_TITLEASC', 'Íàçâàíèå(ïî âîçðàñòàíèþ)');
+DEFINE('_JL_TITLEDESC', 'Íàçâàíèå(ïî óáûâàíèþ)');
+DEFINE('_JL_ORDERINGASC', 'Ïîðÿäîê(ïî âîçðàñòàíèþ)');
+DEFINE('_JL_ORDERINGDESC', 'Ïîðÿäîê(ïî óáûâàíèþ)');
+DEFINE('_JL_JOBIDASC', 'ID (ïî âîçðàñòàíèþ)');
+DEFINE('_JL_JOBIDDESC', 'ID (ïî óáûâàíèþ)');
+DEFINE('_JL_A_CREATEDASC', 'Äàòà ðàçìåùåíèÿ(ïî âîçðàñòàíèþ)');
+DEFINE('_JL_A_CREATEDDESC', 'Äàòà ðàçìåùåíèÿ(ïî óáûâàíèþ)');
+DEFINE('_JL_A_ITEMLIMIT', 'Êîëëè÷åñòâî îáúÿâëåíèé íà îäíîì ëèñòå');
+DEFINE('_JL_A_SORTORDER', 'Ñîðòèðîâàòü ïî');
+DEFINE('_JL_A_LISTJOBQUEUE', 'Î÷åðåäü íà ðàçìåùåíèå îáúÿâëåíèé î ðàáîòå');
+DEFINE('_JL_A_JOBID', 'ID&nbsp;ðàáîòû');
+DEFINE('_JL_A_TITLE', 'Íàçâàíèå');
+DEFINE('_JL_A_JOB_TYPE', 'Òèï ðàáîòû');
+DEFINE('_JL_A_FUNCTIONS', 'Äîëæíîñòü');
+DEFINE('_JL_A_DATE_ADDED', 'Äàòà äîáàâëåíèÿ');
+DEFINE('_JL_A_COMPANY_NAME', 'Ôèðìà/Êîìïàíèÿ');
+DEFINE('_JL_A_PUBLISHED', 'Îïóáëèêîâàííî');
+DEFINE('_JL_A_CHECKEDOUT', 'Ïðîâåðåííî');
+DEFINE('_JL_A_REORDER', 'Ïåðåñîðòèðîâàòü');
+DEFINE('_JL_A_ENTER_COMPANYNAME', 'Íàçâàíèå Ôèðìû/Êîìïàíèè');
+DEFINE('_JL_A_ENTER_ADDRESS1', 'Àäðåñ 1');
+DEFINE('_JL_A_ENTER_ADDRESS2', 'Àäðåñ 2');
+DEFINE('_JL_A_ENTER_CITY', 'Ãîðîä');
+DEFINE('_JL_A_ENTER_USSTATE', 'Øòàä(ÑØÀ)');
+DEFINE('_JL_A_ENTER_ZIPCODE', 'Èíäåêñ');
+DEFINE('_JL_A_ENTER_COMPANYURL', 'Âåáñàéò');
+DEFINE('_JL_A_ENTER_CONTACTNAME', 'Êîíòàêòíîå ëèöî');
+DEFINE('_JL_A_ENTER_CONTACTPHONE', 'Òåëåôîí');
+DEFINE('_JL_A_ENTER_CONTACTEMAIL', 'Email');
+DEFINE('_JL_A_SHOWCONTACT', 'Ïîêàçàòü êîíòàêò â îáúâëåíèè?');
+DEFINE('_JL_A_ENTER_MEMBERID', 'ID ïîëüçîâàòåëÿ');
+DEFINE('_JL_A_ENTER_CREDITCARD_TYPE', 'Òèï êðåäèòíîé êàðòû');
+DEFINE('_JL_A_ENTER_CREDITCARD_NUMBER', 'Íîìåð êðåäèòíîé êàðòû');
+DEFINE('_JL_A_ENTER_CREDITCARD_EXPIRES', 'Ñðîê äåéñòâèÿ êðåäèòíîé êàðòû');
+DEFINE('_JL_A_POSTJOB', 'Ðàçìåñòèòü îáúÿâëåíèå î ðàáîòå');
+DEFINE('_JL_A_REJECTJOB', 'Îòêàçàòü â ðàçìåùåíèè');
+DEFINE('_JL_A_SELECTITEMPOST', 'Âûáåðèòå îáúÿâëåíèå äëÿ ðàçìåùåíèÿ');
+DEFINE('_JL_A_MOVEUP', 'Ñäâèíóòü ââåðõ');
+DEFINE('_JL_A_MOVEDOWN', 'Ñäâèíóòü âíèç');
+DEFINE('_JL_A_TERMSOFSERVICE', 'Óñëîâèÿ óñëóã');
+DEFINE('_JL_A_PUBLISHINGLIMIT', 'Ñðîê îêîí÷àíèÿ ïóáëèêàöèè (â äíÿõ),(0 - áåç îãðàíè÷åíèÿ)');
+DEFINE('_JL_A_CLOSE', 'Çàêðûòü');
+DEFINE('_JL_A_PRINT', 'Ïå÷àòü');
+DEFINE('_JL_A_PREVIEW', 'Ïðåäïðîñìîòð îáúÿâëåíèÿ');
+DEFINE('_JL_A_NOIDYET', 'Áåç ID íîìåðà');
+DEFINE('_JL_A_PUBLISHITEM', 'Îïóáëèêîâàòü îáúÿâëåíèå î ðàáîòå');
+DEFINE('_JL_A_SHOWCCFIELDS', 'Ïîêàçàòü äàííûå êðåäèòíîé êàðòû');
+DEFINE('_JL_A_SHOWUSSTATE', 'Ïîêàçàòü Øòàä(ÑØÀ) óêàçàííûé â àäðåñå');
+DEFINE('_JL_A_ENTER_JOBSTATUS', 'Ââåñòè ñàòóñ îáúÿâëåíèÿ î ðàáîòå');
+DEFINE('_JL_A_DEFAULTJOBSTATUS', 'Default job status');
+DEFINE('_JL_A_EMAILAPPLICATION', 'Use email program for applications instead of a form.');
+DEFINE('_JL_A_ENTER_JOBLOCATION', 'Location');
+DEFINE('_JL_A_ENTER_JOBREFERENCE', 'Reference');
+DEFINE('_JL_A_ENTER_JOBDURATION', 'Duration');
+DEFINE('_JL_A_POSTJOBS', 'Restrict who can post jobs to this access level');
+DEFINE('_JL_A_ITEMSNEWFOR', 'Job postings are marked new for this number of days.');
+DEFINE('_JL_A_SELECTSTATE', 'Select US State');
+DEFINE('_JL_A_ORSELECTFROMLIST', 'Or select US state:');
+DEFINE('_JL_A_KEYWORDCATS', 'Keyword categories');
+DEFINE('_JL_A_EXTRAFIELDS', 'Extra fields');
+DEFINE('_JL_A_COMPANY_CONTACT_INFO', 'Contact Information');
+DEFINE('_JL_A_PAYMENTINFORMATION', 'Credit Card Payment information');
+DEFINE('_JL_A_JOBINFORMATION', 'Job Info');
+DEFINE('_JL_A_DATEFORMAT', 'Date format, leave empty to use language default (uses <a target="_blank" href="http://se.php.net/manual/en/function.strftime.php">strftime()</a> syntax)');
+DEFINE('_JL_A_SELECTTEMPLATE', 'Select which template set to use');
+DEFINE('_JL_A_AUTOAPPROVEJOBS', 'Automatically approve posted jobs.');
+DEFINE('_JL_A_ACCESS', 'Access Level');
+DEFINE('_JL_A_ACCESS_ALL', 'Public');
+DEFINE('_JL_A_ACCESS_REGISTERED', 'Registered');
+DEFINE('_JL_A_ACCESS_USER', 'Special');
+DEFINE('_JL_A_ACCESS_NONE', 'None');
+DEFINE('_JL_A_EXTRAINFO', 'Extra fields');
+DEFINE('_JL_A_PUBLISHING', 'Publishing');
+DEFINE('_JL_A_ITEM_DETAILS', 'Item Details');
+DEFINE('_JL_A_COULDNTREADTEMPLATE', 'Couldn\'t read template file.');
+DEFINE('_JL_A_TEMPLATE_SAVED', 'Template file has been saved.');
+DEFINE('_JL_A_LISTTEMPLATES', 'List templates from template set');
+DEFINE('_JL_A_CANCEL_TMPL', 'Template was not saved.');
+DEFINE('_JL_A_EDITING_TEMPLATE', 'Editing template');
+DEFINE('_JL_A_TEMPLATE_FILE', 'Template file');
+DEFINE('_JL_A_PATH', 'Path:');
+DEFINE('_JL_A_WRITEABLE', 'Writeable');
+DEFINE('_JL_A_UNWRITEABLE', 'Unwriteable');
+DEFINE('_JL_A_NOEXTRAFIELDS', 'No extra fields configured.');
+DEFINE('_JL_A_NO_EDITOR', 'No editor');
+DEFINE('_JL_A_DEFAULT_EDITOR', 'Mambo default editor');
+DEFINE('_JL_A_SELECTEDITOR', 'Use the Mambo default html editor on frontend?.');
+DEFINE('_JL_A_EDITORWIDTH', 'Width of HTML editor window in pixels');
+DEFINE('_JL_A_EDITORHEIGHT', 'Height of HTML editor window in pixels');
+DEFINE('_JL_A_INITIALIZE_HTML_EDITOR', 'Initialize WYSIWYG HTML editor for non-registered users?');
+
+// Module strings
+
+// Error messages
+DEFINE('_JL_ERR_YOURCONFIG', 'Âàø ôàéë êîíôèãóðàöèè ');
+DEFINE('_JL_ERR_CHMODFILE', 'Óñòàíîâèòå chmod 766 äëÿ ñîõðàíåíèÿ êîíôèãóðàöèè.');
+DEFINE('_JL_ERR_WARNING', 'Ïðåäóïðåæäåíèå...');
+DEFINE('_JL_ERR_NOT_WRITEABLE', 'ÍÅÈÑÏÐÀÂÈÌÀß ÎØÈÁÊÀ: Ôàéë êîíôèãóðàöèè íå ìîæåò áûòü ïåðåçàïèñàí');
+DEFINE('_JL_ERR_OPEN_FILE', 'ÍÅÈÑÏÐÀÂÈÌÀß ÎØÈÁÊÀ: Íåâîçìîæíî îòêðûòü ôàéë.');
+DEFINE('_JL_ERR_NOTITLE', 'Ââåäèòå íàçâàíèå.');
+DEFINE('_JL_ERR_CHECKED_OUT1', 'Ïóíêò');
+DEFINE('_JL_ERR_CHECKED_OUT2', 'áûë ìîäèôèöèðîâàí äðóãèì àäìèíèñòðàòîðîì.');
+DEFINE('_JL_ERR_TMPL_NOT_WRITEABLE', 'FATAL ERROR: Template file not writeable·');
+
+#DEFINE('', '');
+
+?>
