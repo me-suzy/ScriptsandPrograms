bffa173d0176892c20e9fa2bec5dcfc783352309
me-suzy <me.suzana@gmail.com>
2025-08-11 13:14:11 +0300

    Add Bookmark Management/phphomepage1_6a/phphomepage/include/localisation/lang_fr.inc.php

---
+++ Bookmark Management/phphomepage1_6a/phphomepage/include/localisation/lang_fr.inc.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * [fr]Fichier de langue française
+ *
+ * @copyright    22/03/2004
+ * @since	     03/08/2001
+ * @version      1.5
+ * @module       lang_fr
+ * @modulegroup  localisation
+ * @package      php_homepage
+ * @access	     public
+ * @author       Eric BLAS <webmaster@phphomepage.net>
+ */
+
+$lang_ConnexBase              = 'Probl&egrave;me avec la base de donn&eacute;es <br>'."\n".' Veuillez essayer plus tard.';
+$lang_ErrorNomFichier         = 'Il faut donner un nom de fichier.<br>Par exemple :';
+$lang_Fichier                 = 'Le fichier';
+$lang_Introuvable             = 'est introuvable.';
+$lang_Accueil                 = 'Bienvenue sur';
+$lang_NvellePage              = 'Pour cr&eacute;er ou modifier une page de d&eacute;marrage :';
+$lang_GenereTable             = 'Cliquer ici pour cr&eacute;er vos tables';
+$lang_ErreurNom               = 'Vous devez rentrer un <b>NOM</b> dans un des champs de formulaire !';
+$lang_Frame                   = 'Cette application fonctionne avec des navigateurs g&eacute;rants les frames.';
+$lang_1                       = '1 . Cr&eacute;er des rubriques';
+$lang_2                       = '2 . Ajouter des liens';
+$lang_3                       = '3 . Cr&eacute;er votre mise en page';
+$lang_4                       = '4 . Enfin g&eacute;n&eacute;rer votre page de d&eacute;marrage';
+$lang_Constituer1             = 'Votre page de d&eacute;marrage \'';
+$lang_Constituer2             = ' \' est contitu&eacute; des rubriques et des liens suivant :';
+$lang_Position                = 'position =';
+$lang_Realiser                = 'R&eacute;alis&eacute; avec';
+$lang_CreerMEP                = 'Cr&eacute;er une nouvelle mise en page';
+$lang_ModifOK                 = 'La mise en page a &eacute;t&eacute; modifi&eacute;e';
+$lang_CreerOK                 = 'La mise en page a &eacute;t&eacute; cr&eacute;&eacute;e';
+$lang_Rubrique                = 'Les rubriques';
+$lang_NvelleRubrique          = 'Cr&eacute;er une nouvelle rubrique';
+$lang_PlacerRubrique          = 'Place de la rubrique sur la page :';
+$lang_SuppRubrique            = 'Supprimer une rubrique';
+$lang_ModifUneRubrique        = 'Modifier une rubrique';
+$lang_Place                   = ' Place :';
+$lang_ChoixRubrique           = 'Choisir la rubrique';
+$lang_ErrorRubNom             = 'Vous devez entrer un nom !';
+$lang_ErrorRubPosition        = 'Vous devez entrer une position !';
+$lang_ErrorRubPositionSup     = 'Vous devez entrer une position inférieur à '.(($cfg_NbrLignes * $cfg_NbrColonnes) + 1).' !';
+$lang_RubOK                   = 'La rubrique a &eacute;t&eacute; cr&eacute;&eacute;e';
+$lang_ErrorRubSupp            = 'Vous devez choisir une rubrique &agrave; supprimer !';
+$lang_RubSupp                 = 'La rubrique a &eacute;t&eacute; supprim&eacute;';
+$lang_ErrorRubModif           = 'Vous devez choisir une rubrique &agrave; modifier !';
+$lang_ErrorRubNomPlace        = 'Vous devez saisir un nom et une place !';
+$lang_ModifRubOK              = 'La rubrique a &eacute;t&eacute; modifi&eacute;';
+$lang_ErrorLienRub            = 'Il faut d\'abord cr&eacute;er au moins <B>1</B> rubrique';
+$lang_LienChoix               = 'Choisir un lien';
+$lang_LienNew                 = 'Cr&eacute;er un nouveau lien';
+$lang_LienSup                 = 'Supprimer un lien';
+$lang_ModifUnLien             = 'Modifier un lien';
+$lang_Lien                    = 'Les Liens';
+$lang_ErrorLienSup            = 'Vous devez choisir un lien &agrave; supprimer !';
+$lang_LienSupOK               = 'Le lien a &eacute;t&eacute; supprim&eacute;';
+$lang_LienNom                 = 'Vous devez saisir un nom !';
+$lang_LienURL                 = 'Vous devez saisir une URL !';
+$lang_LienRub                 = 'Vous devez choisir une rubrique !';
+$lang_ErrorLienModif          = 'Vous devez choisir un lien !';
+$lang_LienOK                  = 'Le lien a &eacute;t&eacute; cr&eacute;&eacute;e';
+$lang_ModifLienOK             = 'Le lien a &eacute;t&eacute; modifi&eacute;';
+$lang_Target                  = 'Ouvrir une nouvelle fen&ecirc;tre';
+$lang_Oui                     = 'Oui';
+$lang_Non                     = 'Non';
+
+// Navigation
+$lang_PourNom                 = 'Pour la page de : ';
+$lang_AjoutRubrique           = 'Ajouter des rubriques';
+$lang_AjoutLiens              = 'Ajouter des liens';
+$lang_ModifRubrique           = 'Modifier des rubriques';
+$lang_ModifLiens              = 'Modifier des liens';
+$lang_MiseEnPage              = 'Mise en page';
+$lang_AffHomepage             = 'Afficher sa page de d&eacute;marrage';
+$lang_RetourIndex             = 'Choisir une nouvelle page de d&eacute;marrage';
+
+// Formulaire
+$lang_Nom                     = 'Son nom : ';
+$lang_RVB                     = 'Rouge - Vert - Bleu';
+$lang_CoulFond                = 'Couleur de fond';
+$lang_CoulRub                 = 'Couleur des rubriques';
+$lang_TailleRub               = 'Taille des rubriques';
+$lang_CoulLien                = 'Couleur des liens';
+$lang_TailleLien              = 'Taille des liens';
+$lang_Police                  = 'Police';
+$lang_Title                   = 'Titre de la page (facultatif)';
+
+// Bouton
+$lang_Creer                   = 'Cr&eacute;er';
+$lang_Modifier                = 'Modifier';
+$lang_Supprimer               = 'Supprimer';
+?>
