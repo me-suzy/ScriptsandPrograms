b9d492b97e0b0c6d0b3a421f4204d28d8ee37718
me-suzy <me.suzana@gmail.com>
2025-08-11 13:51:23 +0300

    Add Chat Scripts/phpshoutv3/index.php

---
+++ Chat Scripts/phpshoutv3/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>phpSHOUT Version 3 Created By Designanet.co.uk</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<IFRAME SRC="inc_form.php" WIDTH=200 HEIGHT=500 frameborder="0" scrolling="yes"></IFRAME>
+</body>
+</html>
