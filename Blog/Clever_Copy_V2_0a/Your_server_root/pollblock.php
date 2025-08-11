ce3bc26800e4b1c7ca3aed8a72264752688ca470
me-suzy <me.suzana@gmail.com>
2025-08-11 11:20:58 +0300

    Add Blog/Clever_Copy_V2_0a/Your_server_root/pollblock.php

---
+++ Blog/Clever_Copy_V2_0a/Your_server_root/pollblock.php
@@ -0,0 +1,43 @@
+<?php
+$getpoll="SELECT * FROM CC_poll";
+$getpoll2=mysql_query($getpoll) or die($no_poll_error);
+$getpoll3=mysql_fetch_array($getpoll2);
+$pollctr = '0';
+for($i = 1; $i <= 8; $i++)
+{
+   $thisanswer = $getpoll3[answer.$i];
+   if ($thisanswer !== "")
+   {
+         $pollctr++;
+   }
+}
+if($getprefs3[showseparator]==1)
+{
+   echo "<hr color=$getprefs3[separatorlinecolor] size = '1'>";
+}
+if ($getprefs3[useblockwrapper]==1)
+{
+   if ($useragent !== 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)')
+   {
+      echo "<table border='0' cellspacing='3' width='171' style='border-collapse: collapse; border: 1px solid $getprefs3[blockbordercolor]'>";
+   }else{
+      echo "<table border='0' cellspacing='3' width='100%' style='border-collapse: collapse; border: 1px solid $getprefs3[blockbordercolor]'>";
+   }
+   if ($getprefs3[block_use_heading_graphic] ==1)
+   {
+      echo "<tr bgcolor=$getprefs3[block_heading_background_color]><td height=$getprefs3[block_heading_height] background='bkgd.gif'>";
+   }else{
+      echo "<tr bgcolor=$getprefs3[block_heading_background_color]><td height=$getprefs3[block_heading_height]>";
+   }
+   echo "<font face=$getprefs3[block_heading_font_face] size=$getprefs3[block_heading_font_size] color=$getprefs3[block_heading_font_color]><$getprefs3[block_heading_font_decoration]>&nbsp;$getblocks3[poll_block_label]</font></b></center></td></tr>";
+   echo "<tr><td bgcolor=$getprefs3[block_background_color]>";
+   echo "<center>The poll block is not available on this site";
+   echo "</td></tr></table><br>";
+}else{
+   echo "<center><font face = $getprefs3[block_heading_font_face] size = $getprefs3[block_heading_font_size] color= $getprefs3[block_heading_font_color]><$getprefs3[block_heading_font_decoration]>$getblocks3[poll_block_label]</$getprefs3[block_heading_font_decoration]></center>"; 
+   echo "<hr color=$getprefs3[separatorlinecolor] size = '1'><br></font>";
+   echo "<center>The poll block is not available on this site";
+
+echo "<br>";
+}
+?>
