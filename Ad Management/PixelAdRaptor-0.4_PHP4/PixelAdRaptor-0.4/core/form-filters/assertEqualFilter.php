c9a7ae2f8464452b4356771fbb09d307f0ea94a5
me-suzy <me.suzana@gmail.com>
2025-08-11 10:19:20 +0300

    Add Ad Management/PixelAdRaptor-0.4_PHP4/PixelAdRaptor-0.4/core/form-filters/assertEqualFilter.php

---
+++ Ad Management/PixelAdRaptor-0.4_PHP4/PixelAdRaptor-0.4/core/form-filters/assertEqualFilter.php
@@ -0,0 +1,58 @@
+<?php
+/** 
+ *
+ * Copyright (C) 2005  Karolis Tamutis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+ */
+  
+if (!defined('MAIN')) {
+
+    die('No direct access.');
+
+}
+ 
+class assertEqualFilter {
+
+    /**
+     *
+     * A constructor
+     */
+    function assertEqualFilter() {
+    
+    }
+    
+    /**
+     *
+     * A validation method, it must have the same name and the same parameters,
+     * the code inside is modified according to the needs.
+     *
+     * @param string $field form field name
+     * @param mixed $param value to compare with
+     * @param string $method method to transfer data
+     */
+    function validate($field, $param, $method) {
+        
+        if ($method[$field] == $param) {
+   
+            return true;
+            
+        }
+                
+        return false;  
+    
+    }
+}
+?>
