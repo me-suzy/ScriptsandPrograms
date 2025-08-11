2bf5729caa912a9a3c354687b5b5944feec1b2b5
me-suzy <me.suzana@gmail.com>
2025-08-11 10:30:05 +0300

    Add Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/rsc/js/form_extensions.js

---
+++ Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/rsc/js/form_extensions.js
@@ -0,0 +1,87 @@
+/**
+ * @author fsaya: Fabrice SAYA-GASNIER / PROGIDISTRI
+ *
+ * @version $Id: form_extensions.js,v 1.3 2005/10/12 18:24:38 fplanque Exp $
+ */
+
+
+/**
+ * Takes an object and returns the parent form
+ *
+ * @param object the child object
+ * @return form the parent form of the child object
+ */
+function get_form( object )
+{
+ 	while( object.tagName != 'FORM' )
+	{	// loop which goes from the checkbox to the form
+		// alert( object.nodeName );
+		if( typeof( object ) != 'undefined' ) // make sure that we have not gone too far
+		{
+			object = object.parentNode;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	// alert( 'ok:'+object.tagName );
+	return object;
+}
+
+
+/**
+ * Toggles all checkboxes of the current form
+ *
+ * @param form the form
+ * @param integer force set/unset
+ */
+function check( object, action )
+{ 
+
+	form_obj = get_form( object );
+
+	if( ! form_obj )
+	{
+		alert( 'Could not find form' );
+		return false;
+	}
+
+	//checks or unchecks all checkboxes in the form
+	i = 0;
+	while( i < form_obj.length )
+	{
+		if( form_obj.elements[i].type == 'checkbox' )
+		{
+			form_obj.elements[i].checked = action;
+		}
+		i++;
+	}
+	
+	// Cancel default action:
+	return false;
+} 
+
+
+/**
+ * Clear the form the current object belongs to
+ *
+ * @param object the object whose form must be cleared
+ */
+function clear_form( object )
+{
+	object = check( object, false );
+	
+	// empties all the input fields of the form
+	i = 0;
+	while( i < object.length )
+	{
+		if( object.elements[i].type == 'text' )
+		{
+			object.elements[i].value = '';
+		}
+		i++;
+	}
+	
+	return object;
+}
