972392f20585e15ec81f8d3c986ade6b27a935fd
me-suzy <me.suzana@gmail.com>
2025-08-11 13:56:19 +0300

    Add Chat Scripts/x7chat1_3_6/X7Chat-v1.3.6B/Docs/Install.Xaraya/xarinit.php

---
+++ Chat Scripts/x7chat1_3_6/X7Chat-v1.3.6B/Docs/Install.Xaraya/xarinit.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * File: $Id:
+ */
+
+function X7chat_init()
+{
+
+    return true;
+}
+
+function X7chat_upgrade($oldversion)
+{
+    return true;
+}
+
+function X7chat_delete()
+{
+    return true;
+}
+
+?>
