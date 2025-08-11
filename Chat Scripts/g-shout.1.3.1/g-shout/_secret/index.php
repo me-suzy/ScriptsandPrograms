104559cac3be4df7b2cf6845426558e528e3b08d
me-suzy <me.suzana@gmail.com>
2025-08-11 13:49:08 +0300

    Add Chat Scripts/g-shout.1.3.1/g-shout/_secret/index.php

---
+++ Chat Scripts/g-shout.1.3.1/g-shout/_secret/index.php
@@ -0,0 +1,16 @@
+<?php
+/************************************************************************/
+/* G-Shout : Gravitasi Shoutbox                                         */
+/* ============================================                         */
+/*                                                                      */
+/* Copyright (c) 2005 by Yohanes Pradono                                */
+/* http://gravitasi.com                                                 */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/*                                                                      */
+/************************************************************************/
+
+die("What?");
+?>
