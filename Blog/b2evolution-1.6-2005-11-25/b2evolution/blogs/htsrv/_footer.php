d5a8b68ba778c188c288bac09fe12b707d08a756
me-suzy <me.suzana@gmail.com>
2025-08-11 10:28:18 +0300

    Add Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/htsrv/_footer.php

---
+++ Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/htsrv/_footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is the footer file for login/registering services
+ *
+ * b2evolution - {@link http://b2evolution.net/}
+ * Released under GNU GPL License - {@link http://b2evolution.net/about/license.html}
+ * @copyright (c)2003-2005 by Francois PLANQUE - {@link http://fplanque.net/}
+ *
+ * @package htsrv
+ */
+if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+?>
+
+</div>
+
+<p class="footer"><?php echo $app_footer_text; ?></p>
+
+<?php debug_info(); ?>
+
+</body>
+</html>
