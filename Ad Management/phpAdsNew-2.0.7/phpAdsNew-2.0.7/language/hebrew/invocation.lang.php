a086fdf69ec63572b383dda3684b636437513316
me-suzy <me.suzana@gmail.com>
2025-08-11 10:01:52 +0300

    Add Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/hebrew/invocation.lang.php

---
+++ Ad Management/phpAdsNew-2.0.7/phpAdsNew-2.0.7/language/hebrew/invocation.lang.php
@@ -0,0 +1,259 @@
+<?php // $Revision: 2.1.2.5 $
+
+
+
+/************************************************************************/
+
+/* phpAdsNew 2                                                          */
+
+/* ===========                                                          */
+
+/*                                                                      */
+
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+
+/* For more information visit: http://www.phpadsnew.com                 */
+
+/*                                                                      */
+
+/* This program is free software. You can redistribute it and/or modify */
+
+/* it under the terms of the GNU General Public License as published by */
+
+/* the Free Software Foundation; either version 2 of the License.       */
+
+/************************************************************************/
+
+
+
+
+
+
+
+// Invocation Types
+
+$GLOBALS['strInvocationRemote']			= "÷øéàä îøçå÷";
+
+$GLOBALS['strInvocationJS']			= "÷øéàä îøçå÷ ì÷åã Javascript";
+
+$GLOBALS['strInvocationIframes']		= "÷øéàä îøçå÷ ìîñâøåú";
+
+$GLOBALS['strInvocationXmlRpc']			= "÷øéàä îøçå÷ áùéîåù òí XML-RPC";
+
+$GLOBALS['strInvocationCombined']		= "÷øéàä îøçå÷ îùåìáú";
+
+$GLOBALS['strInvocationPopUp']			= "÷åôõ";
+
+$GLOBALS['strInvocationAdLayer']		= "òì-ùëáúé àå öó DHTML";
+
+$GLOBALS['strInvocationLocal']			= "î÷åîé";
+
+
+
+
+
+// Other
+
+$GLOBALS['strCopyToClipboard']			= "äòú÷ ìæéëøåï";
+
+
+
+
+
+// Measures
+
+$GLOBALS['strAbbrPixels']			= "ôé÷ñìéí";
+
+$GLOBALS['strAbbrSeconds']			= "ùðéåú";
+
+
+
+
+
+// Common Invocation Parameters
+
+$GLOBALS['strInvocationWhat']			= "áçéøú áàðø";
+
+$GLOBALS['strInvocationClientID']		= "îôøñí àå ÷îôééï";
+
+$GLOBALS['strInvocationTarget']			= "çìåï îèøä";
+
+$GLOBALS['strInvocationSource']			= "î÷åø";
+
+$GLOBALS['strInvocationWithText']		= "äöâ ëéúåá îúçú ìáðø";
+
+$GLOBALS['strInvocationDontShowAgain']		= "àì úöéâ áàðø æä ôòí ðåñôú áàåúå äòîåã";
+
+$GLOBALS['strInvocationDontShowAgainCampaign']		= "àì úöéã áàðø îàåúå ä÷îôééï ùåá áàåúå äòîåã";
+
+$GLOBALS['strInvocationTemplate'] 		= "ùîåø àú äáàðø áúåê îùúðä ëãé ùàôùø éäéä ìùìáå áúáðéú òîåã (Template)";
+
+
+
+
+
+// Iframe
+
+$GLOBALS['strIFrameRefreshAfter']		= "øòðï àçøé";
+
+$GLOBALS['strIframeResizeToBanner']		= "äúàí âåãì îñâøú ìîéãåú äáàðø";
+
+$GLOBALS['strIframeMakeTransparent']		= "ø÷ò îñâøú ù÷åó";
+
+$GLOBALS['strIframeIncludeNetscape4']		= "áëì ÷åã äúàîä òáåø ùëáú ilayer ùì Netscape 4 ";
+
+
+
+
+
+// PopUp
+
+$GLOBALS['strPopUpStyle']			= "ñåâ ÷åôõ";
+
+$GLOBALS['strPopUpStylePopUp']			= "÷åôõ îòì";
+
+$GLOBALS['strPopUpStylePopUnder']		= "÷åôõ îúçú";
+
+$GLOBALS['strPopUpCreateInstance']		= "äàéøåò ùáå é÷ôåõ äçìåï";
+
+$GLOBALS['strPopUpImmediately']			= "îééãéú";
+
+$GLOBALS['strPopUpOnClose']			= "ëàùø äòîåã ðñâø";
+
+$GLOBALS['strPopUpAfterSec']			= "ìàçø";
+
+$GLOBALS['strAutoCloseAfter']			= "éñâø àåèåîèéú àçøé";
+
+$GLOBALS['strPopUpTop']				= "îé÷åí äúçìúé (òìéåï)";
+
+$GLOBALS['strPopUpLeft']			= "îé÷åí äúçìúé (ùîàì)";
+
+$GLOBALS['strWindowOptions']		= "àåôöéåú çìåï";
+
+$GLOBALS['strShowToolbars']			= "ëìéí";
+
+$GLOBALS['strShowLocation']			= "îé÷åí";
+
+$GLOBALS['strShowMenubar']			= "úôøéè";
+
+$GLOBALS['strShowStatus']			= "ñèèåñ";
+
+$GLOBALS['strWindowResizable']		= "ùéðåé âåãì";
+
+$GLOBALS['strShowScrollbars']		= "âåììéí";
+
+
+
+
+
+// XML-RPC
+
+$GLOBALS['strXmlRpcLanguage']			= "ùôú àéøåç";
+
+
+
+
+
+// AdLayer
+
+$GLOBALS['strAdLayerStyle']			= "ñâðåï";
+
+
+
+$GLOBALS['strAlignment']			= "ééùåø";
+
+$GLOBALS['strHAlignment']			= "ééùåø àåô÷é";
+
+$GLOBALS['strLeft']				= "ùîàì";
+
+$GLOBALS['strCenter']				= "îøëæ";
+
+$GLOBALS['strRight']				= "éîéï";
+
+
+
+$GLOBALS['strVAlignment']			= "ééùåø àðëé";
+
+$GLOBALS['strTop']				= "òìéåï";
+
+$GLOBALS['strMiddle']				= "àîöò";
+
+$GLOBALS['strBottom']				= "úçúåï";
+
+
+
+$GLOBALS['strAutoCollapseAfter']		= "÷ôì àåèåîèéú àçøé";
+
+$GLOBALS['strCloseText']			= "ëéúåá ìäåøàú ñâéøä";
+
+$GLOBALS['strClose']				= "[ñâåø]";
+
+$GLOBALS['strBannerPadding']			= "ãéôåï áàðø";
+
+
+
+$GLOBALS['strHShift']				= "äñèä àåô÷éú";
+
+$GLOBALS['strVShift']				= "äñèä àðëéú";
+
+
+
+$GLOBALS['strShowCloseButton']			= "äöâ ìçöï ñâéøä";
+
+$GLOBALS['strBackgroundColor']			= "öáò ø÷ò";
+
+$GLOBALS['strBorderColor']			= "öáò îñâøú";
+
+
+
+$GLOBALS['strDirection']			= "ëéååï";
+
+$GLOBALS['strLeftToRight']			= "îùîàì ìéîéï";
+
+$GLOBALS['strRightToLeft']			= "îéîéï ìùîàì";
+
+$GLOBALS['strLooping']				= "çåæø òì òöîå";
+
+$GLOBALS['strAlwaysActive']			= "ôòéì úîéãéú";
+
+$GLOBALS['strSpeed']				= "îäéøåú";
+
+$GLOBALS['strPause']				= "àúðç";
+
+$GLOBALS['strLimited']				= "îåâáì";
+
+$GLOBALS['strLeftMargin']			= "ùåìééí ùîàìééí";
+
+$GLOBALS['strRightMargin']			= "ùåìééí éîðééí";
+
+$GLOBALS['strTransparentBackground']		= "ø÷ò ù÷åó";
+
+
+
+$GLOBALS['strSmoothMovement']		= "úðåòä çì÷ä";
+
+$GLOBALS['strHideNotMoving']		= "äñúø àú äáàðø ëàùø äñîï àéðå áúðåòä";
+
+$GLOBALS['strHideDelay']			= "äùäééä ìôðé ùäáàðø ðòìí";
+
+$GLOBALS['strHideTransparancy']		= "ù÷éôåú äáàðø äðòìí";
+
+
+
+
+
+$GLOBALS['strAdLayerStyleName']	= array(
+
+	'geocities'		=> "ñèééì Geocities",
+
+	'simple'		=> "ôùåè",
+
+	'cursor'		=> "úçú äòëáø",
+
+	'floater'		=> "îöåó"
+
+);
+
+
+
+?>
