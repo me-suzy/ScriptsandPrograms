42280313bbc1e51e838499ff64f68bb7168e90c9
me-suzy <me.suzana@gmail.com>
2025-08-11 13:31:28 +0300

    Add Calendars/calendarix_0_6_20050830/calendarix_0_6_20050830/cal_languages/c.week.php

---
+++ Calendars/calendarix_0_6_20050830/calendarix_0_6_20050830/cal_languages/c.week.php
@@ -0,0 +1,9 @@
+<?php
+$week[1]="ÐÇÆÚÈÕ Sunday";
+$week[2]="ÐÇÆÚÒ» Monday";
+$week[3]="ÐÇÆÚ¶þ Tuesday";
+$week[4]="ÐÇÆÚÈý Wednesday";
+$week[5]="ÐÇÆÚËÄ Thursday";
+$week[6]="ÐÇÆÚÎå Friday";
+$week[7]="ÐÇÆÚÁù Saturday";
+?>
