ce0b2dcabd443d9e29f8a258f605640583ddfc0c
me-suzy <me.suzana@gmail.com>
2025-08-11 13:39:10 +0300

    Add Calendars/ltwCalendar4.1.3/private/ltw_footer.php

---
+++ Calendars/ltwCalendar4.1.3/private/ltw_footer.php
@@ -0,0 +1,20 @@
+<?php
+//////////////////////////////////////////////////////////////////////////// 
+// ltw_footer.php 
+// $Id: ltw_footer.php,v 1.1 2003/07/18 02:51:18 t51admin Exp $ 
+// 
+// ltwCalendar Sample Footer File
+//////////////////////////////////////////////////////////////////////////// 
+
+echo "
+<!-- ltwCalendar Window is done -->
+<!-- Close the table cell & row it is in
+</td></tr>
+<tr><td align=\"center\" colspan=\"2\">
+Calendar provided by <a href=\"http://codewalkers.com/\" target=\"_blank\">Codewalkers</a>.
+</td></tr></table>
+</BODY>
+</HTML>
+";
+
+?>
