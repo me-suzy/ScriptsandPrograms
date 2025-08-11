34a934806e49f949f637fde6a54c5b21b0a09ede
me-suzy <me.suzana@gmail.com>
2025-08-11 13:51:16 +0300

    Add Chat Scripts/phpshoutv3/config.php

---
+++ Chat Scripts/phpshoutv3/config.php
@@ -0,0 +1,23 @@
+<?
+
+////////////////////////////////
+// Configuration for phpSHOUT //
+////////////////////////////////
+
+$numofposts = "20"; // Number of posts to display
+$archivenoofposts = "50"; // Number of posts to display per page in archive
+$showdate = "1"; // Display name when user hoovers mouse over post. 1 = On, 0 = Off
+$badlang = "1"; // Censor out bad language 1 = On, 0 = Off
+$maxchars = "200"; // Number of characters allowed in message field
+$imagepath = "http://localhost/phpSHOUT/shoutbox/images"; // Full path to image location. NO Trailing slash
+$savename = "0"; // Remember users name after posting a message.
+$floodtime = "20"; // Flood control time. In seconds the time before the same user can post another message. If you don't want any flood control change this to zero (0).
+
+//////////////////////////////////////
+// Configuration for phpSHOUT Admin //
+//////////////////////////////////////
+
+$username = "admin"; // admin username
+$password = "password"; // password for username
+
+?>
