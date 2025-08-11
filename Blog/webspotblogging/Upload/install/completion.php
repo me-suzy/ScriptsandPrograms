1b1749b3357e2ed4244a756b3aaf97b33da4de72
me-suzy <me.suzana@gmail.com>
2025-08-11 13:01:37 +0300

    Add Blog/webspotblogging/Upload/install/completion.php

---
+++ Blog/webspotblogging/Upload/install/completion.php
@@ -0,0 +1,11 @@
+<?
+$page = "Installation Completion";
+$path = "../";
+include("../inc/mainheader.inc.php");
+?>
+You have successfully set up a blogging system.<BR><BR>
+<b>Please remove this directory to finish completely.<BR></b>
+Thanks for choosing WebspotBlogging for your website. I hope you find this web application great!!!
+<?
+include("../inc/footer.inc.php");
+?>
