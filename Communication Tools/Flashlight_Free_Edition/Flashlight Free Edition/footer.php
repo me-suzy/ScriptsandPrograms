575088aaf8ca9dafdcb6ba77a748f91e5dd8e056
me-suzy <me.suzana@gmail.com>
2025-08-11 14:10:53 +0300

    Add Communication Tools/Flashlight_Free_Edition/Flashlight Free Edition/footer.php

---
+++ Communication Tools/Flashlight_Free_Edition/Flashlight Free Edition/footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+     <span>Copyright &copy; <a target="_new" href="http://www.xeweb.net" title="Visit XEWeb">XEWeb</a> 2005. All rights reserved.
+     <br /><a target="_new" href="http://flashlight.xeweb.net" title="Visit Flashlight Homepage">Flashlight Free Edition</a> v<?=$ver?> used by <a target="_new" href="<?=$company_url?>" title="Visit <?=$company_name?>"><?=$company_name?></a>.</span>
+</div>
+
+</div>
+</body>
+</html>
+<? mysql_close(); ?>
