0427d2da21af9279190ca52d5539f7fde5876826
me-suzy <me.suzana@gmail.com>
2025-08-11 13:41:25 +0300

    Add Calendars/ulc/ulc/etc/config.php

---
+++ Calendars/ulc/ulc/etc/config.php
@@ -0,0 +1,15 @@
+<?
+ini_set("register_globals","Off");
+ini_set("magic_quotes_runtime","Off");
+define("mysql_db_name","ulc");
+define("mysql_db_host","localhost");
+define("mysql_db_user","root");
+define("mysql_db_password","");
+
+define("app_name","ulc");
+define("session_life",3600);
+
+define("app_language","fr");
+// you can create files like : etc/local/lang-<language>.php
+
+?>
