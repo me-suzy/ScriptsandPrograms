fe2c0c945da4d44956b643856d4803c77f2624be
me-suzy <me.suzana@gmail.com>
2025-08-11 10:32:26 +0300

    Add Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/skins/_rss/_lastcomments.php

---
+++ Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/skins/_rss/_lastcomments.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This template generates an RSS 0.92 feed for the requested blog's latest comments
+ *
+ * See {@link http://backend.userland.com/rss092}
+ *
+ * This file is not meant to be called directly.
+ * It is meant to be called automagically by the main template (_main.php).
+ *
+ * This file is part of the b2evolution project - {@link http://b2evolution.net/}
+ *
+ * @copyright (c)2003-2005 by Francois PLANQUE - {@link http://fplanque.net/}
+ *
+ * @license http://b2evolution.net/about/license.html GNU General Public License (GPL)
+ * {@internal
+ * b2evolution is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * b2evolution is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with b2evolution; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * }}
+ *
+ * @package evoskins
+ * @subpackage rss
+ *
+ * {@internal Below is a list of authors who have contributed to design/coding of this file: }}
+ * @author fplanque: François PLANQUE - {@link http://fplanque.net/}
+ * }}
+ *
+ * @version $Id: _lastcomments.php,v 1.1 2005/11/21 20:37:40 fplanque Exp $
+ */
+if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+
+$CommentList = & new CommentList( $blog, "'comment'", $show_statuses, '',	'',	'DESC',	'',	20 );
+
+?>
+		<link><?php $Blog->disp( 'lastcommentsurl', 'xml' ) ?></link>
+		<description></description>
+		<language><?php $Blog->disp( 'locale', 'xml' ) ?></language>
+		<docs>http://backend.userland.com/rss092</docs>
+		<?php while( $Comment = $CommentList->get_next() )
+		{ // Loop through comments: ?>
+		<item>
+			<title><?php echo format_to_output( T_('In response to:'), 'xml' ) ?> <?php $Comment->Item->title( '', '', false, 'xml' ) ?></title>
+			<description><?php $Comment->content( 'entityencoded' ) ?></description>
+			<link><?php $Comment->permalink() ?></link>
+		</item>
+		<?php } // End of comment loop. ?>
