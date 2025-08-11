46c7afaf1bfd0c5b438df6085b35fdd995d69165
me-suzy <me.suzana@gmail.com>
2025-08-11 14:03:43 +0300

    Add Click Tracking/fclicksqllite/fclicksql/admin/inc/conf_body.inc.php

---
+++ Click Tracking/fclicksqllite/fclicksql/admin/inc/conf_body.inc.php
@@ -0,0 +1,19 @@
+<?
+// Anti Hack
+if(!stristr($_SERVER["PHP_SELF"], "admin.php")) header("Location: index.html");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Fast Click SQL <?=$CFG['SERIES']?> v<?=$CFG[VERSION]?> Administration</title>
+</head>
+<frameset cols="150,*">
+  <frame name="left" src="admin.php?page=conf_left" scrolling="no" noresize frameborder="0" target="main">
+  <frame name="content" src="admin.php?page=conf_help" frameborder="0">
+  <noframes>
+  <body bgcolor="#FFFFFF">
+  <p>Fast Click SQL requires you to use a frames enabled browser..</p>
+  </body>
+  </noframes>
+</frameset>
+</html>
