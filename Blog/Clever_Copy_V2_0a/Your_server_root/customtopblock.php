4e3e84508f6a811bc84e64223a0a840002fbd01a
me-suzy <me.suzana@gmail.com>
2025-08-11 11:17:49 +0300

    Add Blog/Clever_Copy_V2_0a/Your_server_root/customtopblock.php

---
+++ Blog/Clever_Copy_V2_0a/Your_server_root/customtopblock.php
@@ -0,0 +1,24 @@
+<?php
+$query =  ("SELECT * FROM CC_blocks WHERE block_file = 'customcenterblock1.php'") or die($no_blocks_found_error);
+$result = mysql_query($query);                                                                                                                                                                                     if ((isset($_SESSION['cuser'])) || (isset($_SESSION['cadmin']))){
+while($row = mysql_fetch_array($result)){
+       if (isset($_SESSION['cuser'])){
+          if ((($row["side"]=="2") && ($row["view"]=="1")) || (($row["side"]=="2") && ($row["view"]=="0"))){
+                  include "customcenterblock1.php";
+          }
+       }
+       elseif (isset($_SESSION['cadmin'])){
+               if ((($row["side"]=="2"))&& (($row["view"]=="2"))){
+                   include "customcenterblock1.php";
+             }
+         }
+    }
+}
+else{
+    while($row = mysql_fetch_array($result)){
+        if ((($row["side"]=="2") && ($row["view"]=="0"))){
+             include "customcenterblock1.php";
+        }
+   }
+}
+?>
