c7efca7694f568a6b17fae2c0fb1f8b903354deb
me-suzy <me.suzana@gmail.com>
2025-08-11 10:21:06 +0300

    Add Affiliate Programs/overstock_dfs/admin.php

---
+++ Affiliate Programs/overstock_dfs/admin.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+<TITLE>Site Administration</TITLE>
+</HEAD>
+
+<FRAMESET  COLS="25%, *">
+     <FRAME SRC="man.php" >
+     <FRAME SRC="usedcats.php" name='principal'>
+
+</FRAMESET>
+
+</HTML>
+
