6a1f8f64c1f5bb3dde2ef2dc2a1f3250253e2c12
me-suzy <me.suzana@gmail.com>
2025-08-11 10:24:26 +0300

    Add Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/admin/statuses.php

---
+++ Blog/b2evolution-1.6-2005-11-25/b2evolution/blogs/admin/statuses.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file implements the UI controler for status management.
+ *
+ * @copyright (c)2003-2005 by Francois PLANQUE - {@link http://fplanque.net/}.
+ *
+ * @license http://b2evolution.net/about/license.html GNU General Public License (GPL)
+ * {@internal
+ * b2evolution is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * b2evolution is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with b2evolution; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * }}
+ *
+ * @package admin
+ *
+ * @author fplanque: François PLANQUE
+ *
+ * @version $Id: statuses.php,v 1.5 2005/10/28 20:08:46 blueyed Exp $
+ */
+
+/**
+ * Includes:
+ */
+require_once dirname(__FILE__). '/_header.php';
+
+$AdminUI->set_path( 'options', 'statuses' );
+$edited_table = 'T_poststatuses';
+$edited_table_IDcol = 'pst_ID';
+$edited_table_namecol = 'pst_name';
+$edited_table_orderby = 'pst_ID ASC';
+$edited_name_maxlen = 40;
+
+require dirname(__FILE__). '/_listeditor.php';
+?>
