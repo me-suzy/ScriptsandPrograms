22ed6b39eee40b97a94563f3be080ea4b2a4bec4
me-suzy <me.suzana@gmail.com>
2025-08-11 13:51:50 +0300

    Add Chat Scripts/ready_chatbox_1-3/chatbox/smilies.php

---
+++ Chat Scripts/ready_chatbox_1-3/chatbox/smilies.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Insert Smilies and Text Decoration</title>
+<style type="text/css">
+<!--
+.topic {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+	color: #CCCCCC;
+	background-color: #003366;
+	font-weight: 500;
+	padding-top: 1px;
+	padding-bottom: 1px;
+	padding-left: 5px;
+}
+.cell_bg {
+	background-color: #0060BF;
+}
+.alt_a {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	color: #333333;
+	background-color: #006FDD;
+	border: 1px solid #333333;
+}
+.name {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	color: #333333;
+	font-weight: bold;
+	text-decoration: none;
+}
+.input {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 11px;
+	color: #333333;
+	background-color: #007AF4;
+	border: 1px solid #333333;
+}
+.href1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 13px;
+	color: #CCCCCC;
+	text-decoration: none;
+	font-weight: bold;
+}
+.log {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	font-style: italic;
+	color: #D7D7D7;
+}
+.unnamed1 {
+	font-size: 14px;
+	color: #333333;
+	text-decoration: underline;
+	font-family: Arial, Helvetica, sans-serif;
+	;
+	font-style: normal;
+}
+.insert {color: #CCCCCC; text-decoration: none; font-weight: bold; font-family: Arial, Helvetica, sans-serif;}
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+-->
+</style>
+
+<script language="javascript" type="text/javascript">
+<!--
+function insertcode(code) {
+	code = ' ' + code + ' ';
+	opener.document.chatbox.sb_message.value  += code;
+	window.close();
+}
+
+//-->
+</script>
+
+</head>
+
+<body>
+
+<table width="400"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="topic">Text</td>
+    <td class="topic">Smilies</td>
+  </tr>
+  <tr>
+    <td class="cell_bg">
+      <table width="95%"  border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td>
+		  	<a  href="javascript:insertcode('[b]'+ prompt('Please enter Text you wish to be Bold', '') +'[/b]')" style="font-size: 14px;color: #333333;text-decoration: none;font-family: Arial, Helvetica, sans-serif;font-weight: bold;">
+				Bold
+			</a>
+          </td>
+        </tr>
+		
+        <tr>
+          <td>
+		  	<a  href="javascript:insertcode('[i]'+ prompt('Please enter Text you wish to be Italic', '') +'[/i]')" style="font-size: 14px;color: #333333;text-decoration: none;font-family: Arial, Helvetica, sans-serif;font-weight: bold;font-style: italic;">
+				Italics
+			</a>
+          </td>
+		</tr>
+		 
+        <tr>
+          <td>
+		  	<a href="javascript:insertcode('[u]'+ prompt('Please enter Text you wish to be Underlined', '') +'[/u]')" style="font-size: 14px;color: #333333;text-decoration: underline;font-family: Arial, Helvetica, sans-serif;font-weight: bold;">
+				Underline
+			</a>
+          </td>
+        </tr>
+		
+        <tr>
+          <td>
+		  	<a href="javascript:insertcode('[color=' + prompt('Please enter color of the text \n Such as Red, Green, Blue, Black, White, etc.','') + ']' + prompt('Enter the text to be in this color','') + '[/color]')" style="font-size: 14px;color: #FF0000;text-decoration: none;font-family: Arial, Helvetica, sans-serif;font-weight: bold;">
+				Colors
+			</a>
+          </td>
+        </tr>
+		
+        <tr>
+          <td>
+		  	<a href="javascript:insertcode('[url=' + prompt('Enter the complete URL of the webpage', 'http://') + ']' + prompt('Enter the title of the webpage', '') + '[/url]' )" style="font-size: 14px;color: #333333;text-decoration: none;font-family: Arial, Helvetica, sans-serif;font-weight: bold;">
+				Insert Link
+			</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+		  	<a href="javascript:insertcode('[email=' + prompt('Please enter your email address', 'yourname@email.com') + ']' + prompt('Enter the link text', '') + '[/email]' )" style="font-size: 14px;color: #333333;text-decoration: none;font-family: Arial, Helvetica, sans-serif;font-weight: bold;">
+				Insert Email
+			</a>
+          </td>
+        </tr>	
+		<tr>			
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+    <td class="cell_bg">
+      <table width="95%"  border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td width="16"><a class="insert" href="javascript:insertcode(':?:')"><img src="smilies/icon_confused.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':?:')">:?:</a></td>
+		  
+          <td width="16"><a class="insert" href="javascript:insertcode(':D')"><img src="smilies/icon_mrgreen.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':D')">:D</a></td>
+        </tr>
+		
+        <tr>
+          <td width="16"><a class="insert" href="javascript:insertcode(':cool:')"><img src="smilies/icon_cool.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':cool:')">:cool:</a></td>
+		  
+          <td width="16"><a class="insert" href="javascript:insertcode(':|:')"><img src="smilies/icon_neutral.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':|:')">:|:</a></td>
+        </tr>
+        <tr>
+          <td width="16"><a class="insert" href="javascript:insertcode(':sad:')"><img src="smilies/icon_cry.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':sad:')">:sad:</a></td>
+		  
+          <td width="16"><a class="insert" href="javascript:insertcode(':P')"><img src="smilies/icon_razz.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':P')">:p</a></td>
+        </tr>
+        <tr>
+          <td width="16"><a class="insert" href="javascript:insertcode(':wow:')"><img src="smilies/icon_eek.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':wow:')">:wow:</a></td>
+		  
+          <td width="16"><a class="insert" href="javascript:insertcode(':red:')"><img src="smilies/icon_redface.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':red:')">:red:</a></td>
+        </tr>
+        <tr>
+          <td width="16"><a class="insert" href="javascript:insertcode(':evil:')"><img src="smilies/icon_evil.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':evil:')">:evil:</a></td>
+		  
+          <td width="16"><a class="insert" href="javascript:insertcode(':8')"><img src="smilies/icon_rolleyes.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':8')">:8</a></td>
+        </tr>
+        <tr>
+          <td width="16"><a class="insert" href="javascript:insertcode(':lol:')"><img src="smilies/icon_lol.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':lol:')">:lol:</a></td>
+		  
+          <td width="16"><a class="insert" href="javascript:insertcode(':)')"><img src="smilies/icon_smile.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':)')">:)</a></td>
+        </tr>
+        <tr>
+          <td width="16"><a class="insert" href="javascript:insertcode(':X')"><img src="smilies/icon_mad.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(':X')">:X</a></td>
+		  
+          <td width="16"><a class="insert" href="javascript:insertcode(';)')"><img src="smilies/icon_wink.gif" width="16" height="16" border="0" alt="" /></a></td>
+          <td width="1" class="style1">&nbsp;</td>
+          <td class="href1"><a class="insert" href="javascript:insertcode(';)')">;)</a></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
