4e3a0685de0680981517cdb7937f8cbc34521ffe
me-suzy <me.suzana@gmail.com>
2025-08-11 10:10:11 +0300

    Add Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/admin/logout.php

---
+++ Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/admin/logout.php
@@ -0,0 +1,32 @@
+<?php // $Revision: 2.0.2.3 $
+
+/************************************************************************/
+/* phpPgAds                                                             */
+/* ========                                                             */
+/*                                                                      */
+/* Copyright (c) 2001-2005 by the phpPgAds developers                   */
+/* For more information visit: http://phppgads.sourceforge.net          */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+
+// Include required files
+require ("../config.inc.php");
+require ("../libraries/lib-io.inc.php");
+require ("../libraries/lib-db.inc.php");
+require ("lib-permissions.inc.php");
+
+
+
+/*********************************************************/
+/* Main code                                             */
+/*********************************************************/
+
+phpAds_dbConnect();
+phpAds_Logout();
+
+?>
