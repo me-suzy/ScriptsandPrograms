bd328e3e126a6c4ab358e404f36e543a0dccf4dc
me-suzy <me.suzana@gmail.com>
2025-08-11 10:15:55 +0300

    Add Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/language/russian_cp1251/report.lang.php

---
+++ Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/language/russian_cp1251/report.lang.php
@@ -0,0 +1,24 @@
+<?php // $Revision: 2.0.2.2 $
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2000-2005 by the phpAdsNew developers                  */
+/* For more information visit: http://www.phpadsnew.com                 */
+/*                                                                      */
+/*                                                                      */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+$GLOBALS['strPluginAffiliate'] 		= "Ñãåíåðèðîâàòü îáîçðåíèå èñòîðèè âûáðàííîãî èçäàòåëÿ. Îò÷¸ò ýêñïîðòèðóåòñÿ â ôîðìàòå CSV äëÿ èñïîëüçîâàíèÿ â ýëåêòðîííîé òàáëèöå.";
+$GLOBALS['strPluginCampaign'] 		= "Ñãåíåðèðîâàòü îáîçðåíèå èñòîðèè âûáðàííîé êàìïàíèè. Îò÷¸ò ýêñïîðòèðóåòñÿ â ôîðìàòå CSV äëÿ èñïîëüçîâàíèÿ â ýëåêòðîííîé òàáëèöå.";
+$GLOBALS['strPluginClient'] 		= "Ñãåíåðèðîâàòü îáîçðåíèå èñòîðèè âûáðàííîãî ðåêëàìîäàòåëÿ. Îò÷¸ò ýêñïîðòèðóåòñÿ â ôîðìàòå CSV äëÿ èñïîëüçîâàíèÿ â ýëåêòðîííîé òàáëèöå.";
+$GLOBALS['strPluginGlobal'] 		= "Ñãåíåðèðîâàòü îáîçðåíèå ãëîáàëüíîé èñòîðèè. Îò÷¸ò ýêñïîðòèðóåòñÿ â ôîðìàòå CSV äëÿ èñïîëüçîâàíèÿ â ýëåêòðîííîé òàáëèöå.";
+$GLOBALS['strPluginZone'] 		= "Ñãåíåðèðîâàòü îáîçðåíèå èñòîðèè âûáðàííîé çîíû. Îò÷¸ò ýêñïîðòèðóåòñÿ â ôîðìàòå CSV äëÿ èñïîëüçîâàíèÿ â ýëåêòðîííîé òàáëèöå.";
+
+?>
