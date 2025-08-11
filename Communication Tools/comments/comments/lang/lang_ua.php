f8fd6f1e5fb8852d5da7cea2c1193205e432e879
me-suzy <me.suzana@gmail.com>
2025-08-11 14:06:30 +0300

    Add Communication Tools/comments/comments/lang/lang_ua.php

---
+++ Communication Tools/comments/comments/lang/lang_ua.php
@@ -0,0 +1,25 @@
+<?
+$COM_LANG['header'] = "Êîìåíòàð³";
+$COM_LANG['no_comments_yet'] = "Äîñ³ íåìàº æîäíîãî êîìåíòàðÿ";
+$COM_LANG['Name'] = "²ì'ÿ";
+$COM_LANG['r_disc_name'] = "Ïîëå '²ì'ÿ' íå ìîæå áóòè ïîðîæí³ì";
+$COM_LANG['E-mail'] = "Email";
+$COM_LANG['Notify'] = "Â³äñèëàòè ìåí³ ³íø³ êîìåíòàð³ íà ö³é ñòîð³íö³";
+$COM_LANG['Dont_show_email'] = "Íå ïîêàçóâàòè ì³é email";
+$COM_LANG['Text'] = "Òåêñò";
+$COM_LANG['r_disc_text'] = "Ïîëå 'Òåêñò' íå ìîæå áóòè ïîðîæí³ì";
+$COM_LANG['Submit'] = "Â³äïðàâèòè";
+$COM_LANG['not_allowed'] = "Âè íå ìîæåòå â³äïðàâëÿòè êîìåíòàð³";
+
+$COM_LANG['months'] = array("Ñ³÷", "Ëþò", "Áåð", "Êâ³", "Òðà", "×åð", "Ëèï", "Ñåð", "Âåð", "Æîâ", "Ëèñ", "Ãðó");
+
+$COM_LANG['email_new_comment'] = "Íîâ³ êîìåíòàð³";
+$COM_LANG['email_from'] = "Â³ä";
+$COM_LANG['email_to_unsubscribe'] = "ßêùî âè íå áàæàºòå îòðèìóâàòè ïîâ³äîìëåííÿ ïðî íîâ³ êîìåíòàð³ íà ö³é ñòîð³íö³, â³äêðèéòå öå ïîñèëàííÿ:";
+
+$COM_LANG['unsubscribed'] = "Âè â³äïèñàëèñÿ"; 
+$COM_LANG['not_unsubscribed'] = "Âè íå ï³äïèñóâàëèñÿ"; 
+
+$COM_LANG['admin_panel_name'] = "Àäì³í³ñòðàòèâíà ïàíåëü";
+
+?>
