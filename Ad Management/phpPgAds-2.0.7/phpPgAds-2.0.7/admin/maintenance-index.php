241e64d563e375537f91a8073786a94efc56aa50
me-suzy <me.suzana@gmail.com>
2025-08-11 10:10:19 +0300

    Add Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/admin/maintenance-index.php

---
+++ Ad Management/phpPgAds-2.0.7/phpPgAds-2.0.7/admin/maintenance-index.php
@@ -0,0 +1,21 @@
+<?php // $Revision: 2.0.2.3 $
+
+/************************************************************************/
+/* phpPgAds                                                             */
+/* ========                                                             */
+/*                                                                      */
+/* Copyright (c) 2001-2005 by the phpPgAds developers                   */
+/* For more information visit: http://phppgads.sourceforge.net          */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+
+// Redirect to admin settings page
+header("Location: maintenance-priority.php");
+exit;
+
+?>
