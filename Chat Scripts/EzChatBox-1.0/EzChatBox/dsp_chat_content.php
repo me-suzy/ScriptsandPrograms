2f0eff02d253a7ba585c2c4eed6b817e532c592f
me-suzy <me.suzana@gmail.com>
2025-08-11 13:47:40 +0300

    Add Chat Scripts/EzChatBox-1.0/EzChatBox/dsp_chat_content.php

---
+++ Chat Scripts/EzChatBox-1.0/EzChatBox/dsp_chat_content.php
@@ -0,0 +1,2 @@
+
+<font face="Arial" size="2" color="#ffffcc"><b><i>Terminal_fire</i></b>  <font size="1">(15:18)</font> : <font color="#ffffff">Thank you for choosing EZChatBox - Alpha Release. <img src="Images/smilies/smile.gif" alt="smile" /></font></font><br />
