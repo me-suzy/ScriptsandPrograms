3f093faa8d7a62a3e8e1dc9908be902e959d0a43
me-suzy <me.suzana@gmail.com>
2025-08-11 13:20:22 +0300

    Add Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/zh_TW/info.inc.php

---
+++ Bookmark Management/SiteBar-3.3.5/SiteBar-3.3.5/locale/zh_TW/info.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+$info = array
+(
+    'language' => '繁體中文', //Chinese traditional characters (TW+HK)
+    'authors' => array
+    (
+        'Chung-Ruei Huang' => 'mailto:crhuang@gmail.com',
+        'Sam Liu' => 'mailto:samliu@gmail.com',
+        'Henry Wong' => 'mailto:melink@users.sourceforge.net',
+    ),
+    'aliases' => array('zh_HK'),
+);
+
+?>
