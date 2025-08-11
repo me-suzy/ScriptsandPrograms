c686eb952e821a1f418ab3ae6c1729fb4d915e3e
me-suzy <me.suzana@gmail.com>
2025-08-11 14:00:32 +0300

    Add Classified Ads/jobline-1_1_1/jobline-1_1_1/language/norwegian.php

---
+++ Classified Ads/jobline-1_1_1/jobline-1_1_1/language/norwegian.php
@@ -0,0 +1,258 @@
+<?php
+// Norwegian language file for Jobline
+//
+// Distributed under the terms of the GNU General Public License
+// This software may be used without warrany provided and
+// copyright statements are left intact.
+
+// Frontend language strings
+DEFINE('_JL_ERROR', 'En feil har oppstått');
+DEFINE('_JL_NOSUBCATS', 'Ingen underkategorier.');
+DEFINE('_JL_CATEGORIES', 'Kategorier');
+DEFINE('_JL_NOTFOUND', 'Fant ikke siden.');
+DEFINE('_JL_HACKING_ATTEMPT', 'Hacking forsøk, avslutter.');
+DEFINE('_JL_JOBTYPE_FULLTIME', 'Fulltid');
+DEFINE('_JL_JOBTYPE_PARTTIME', 'Deltid');
+DEFINE('_JL_JOBTYPE_INTERNSHIP', 'praksis');
+DEFINE('_JL_LIST_JOBPOSTINGS', 'Jobbannonser');
+DEFINE('_JL_LIST_DESCRIPTION', 'nedenfor vises en liste over nåværende stillinger.');
+DEFINE('_JL_JOBID', 'ID');
+DEFINE('_JL_DATE_POSTED', 'Dato');
+DEFINE('_JL_JOB_TITLE', 'Jobb');
+DEFINE('_JL_COMPANY_NAME', 'Firma');
+DEFINE('_JL_JOBTYPE', 'Stilling');
+DEFINE('_JL_DATEPOSTED', 'Dato');
+DEFINE('_JL_COMPANYINFO', 'Bedriftsinfo');
+DEFINE('_JL_DESCRIPTION', 'Jobbeskrivelse');
+DEFINE('_JL_HOWTOAPPLY', 'Hvordan søke');
+DEFINE('_JL_EMAILRESUMETO', 'Send CV til');
+DEFINE('_JL_WEBSITE', 'Webside');
+DEFINE('_JL_APPLYFORJOB', 'Sende søknaden');
+DEFINE('_JL_FIRSTNAME', 'Fornavn');
+DEFINE('_JL_LASTNAME', 'etternavn');
+DEFINE('_JL_ADDRESS1', 'Addresse 1');
+DEFINE('_JL_ADDRESS2', 'Addresse 2');
+DEFINE('_JL_CITY', 'Sted');
+DEFINE('_JL_STATE', 'Fylke');
+DEFINE('_JL_ZIPCODE', 'Postnr');
+DEFINE('_JL_COVER_LETTER', 'Søknadsbrev');
+DEFINE('_JL_RESUME', 'CV');
+DEFINE('_JL_SEND_APPLICATION', 'Send søknad');
+DEFINE('_JL_RECEIVED_APPLICATION', 'Du har fått en søknad på følgende jobb:');
+DEFINE('_JL_APPLICATION', 'Søknad');
+DEFINE('_JL_APPLICATION_SUBJECT', 'Du har mottat en jobbsøknad');
+DEFINE('_JL_NOSUCHJOB', 'Ingen stilling funnet.');
+DEFINE('_JL_THANKYOU', 'Takk');
+DEFINE('_JL_THANKYOU_MESSAGE', 'Takk for din søknad.');
+DEFINE('_JL_GOBACKTOJOB', 'Gå tilbake til jobblisten');
+DEFINE('_JL_GOTOLIST', 'Gå til jobblisten');
+DEFINE('_JL_PREVIEW_JOB', 'Forhåndsvis');
+DEFINE('_JL_CANCEL_JOB', 'Avbryt');
+DEFINE('_JL_QUALIFICATIONS', 'Kvalifikasjoner');
+DEFINE('_JL_COMPENSATION', 'Lønn');
+DEFINE('_JL_CONTACTINFO', 'Kontaktinfo');
+DEFINE('_JL_CONTACTPHONE', 'Kontaktpersonens telefonnummer');
+DEFINE('_JL_EMAIL', 'Kontaktpersonens E-postadresse');
+DEFINE('_JL_SAVEOREDIT', 'Lagre eller fortsette?');
+DEFINE('_JL_SAVE_JOB', 'Lagre stillingsannonse');
+DEFINE('_JL_EDIT_JOB', 'Redigere stillingsannonse');
+DEFINE('_JL_PREVIEW_JOBPOSTING', 'Forhåndsvise jobbannonser');
+DEFINE('_JL_JOBPOSTINGSENT', 'Takk for din stillingsannonse. Efterdal vil sjekke denne innen forholdsvis kort tid..');
+DEFINE('_JL_GOTOSEARCH', 'Søk jobb');
+DEFINE('_JL_POSTJOB', 'Last opp jobb');
+DEFINE('_JL_ENTERJOBINFO', 'Sett inn jobbinformasjon');
+DEFINE('_JL_NEW_SEARCH', 'Søk etter jobb');
+DEFINE('_JL_NOSEARCHWORD', 'Du må fylle inn søkeord.');
+DEFINE('_JL_SEARCH', 'Søk');
+DEFINE('_JL_NO_RESULTS', 'Ingen resultater funnet');
+DEFINE('_JL_NOTHING_FOUND', 'Fant ingen lignende resultater, basert på ditt søk.');
+DEFINE('_JL_SEARCH_RESULTS', 'Søkeresultater');
+DEFINE('_JL_SEARCH_DESCRIPTION', 'Ditt søk ga følgende resultat:');
+DEFINE('_JL_BACKTOSEARCH', 'Tilbake til søk');
+DEFINE('_JL_TERMSOFSERVICE', 'Annonseringsvilkår');
+DEFINE('_JL_NOTACCESIBLE', 'Denne stillingen er ikke lenger tilgjengelig.');
+DEFINE('_JL_NOTOWNER', 'Du er ikke eier av denne annonsen' );
+DEFINE('_JL_NOTLOGGEDIN', 'Du må være innlogget for å redigere annonsen.');
+DEFINE('_JL_NEWJOBPOSTED', 'En ny stilling lastes opp');
+DEFINE('_JL_NEWJOB_MSG', 'En ny jobb med tittelen \'<<jobtitle>>\' Har blitt lagt inn. Gå til <<livesite>>/administrator for å godkjenne.');
+DEFINE('_JL_JOBSTATUS_SOURCING', 'Søker');
+DEFINE('_JL_JOBSTATUS_INTERVIEWING', 'Intervjuer');
+DEFINE('_JL_JOBSTATUS_CLOSED', 'Stengt for nye søkere');
+DEFINE('_JL_JOBSTATUS_FINALISTS', 'Finalister utvalgt');
+DEFINE('_JL_JOBSTATUS_PENDING', 'Venter på godkjenning');
+DEFINE('_JL_JOBSTATUS_HOLD', 'Lagt på vent');
+DEFINE('_JL_JOBSTATUS', 'Jobb Status');
+DEFINE('_JL_LIST_INTERNSHIPS', 'Praksis');
+DEFINE('_JL_LIST_INTERNSHIPS_DESC', 'Nedenfor vises en liste over alle tilgjengelige praksisplasser.');
+DEFINE('_JL_INTERNSHIPID', 'ID');
+DEFINE('_JL_INTERNSHIP_TITLE', 'Praksisplass');
+DEFINE('_JL_GOTOSEARCH_INTERN', 'Søk praksisplass');
+DEFINE('_JL_POSTINTERNSHIP', 'Legg inn praksisplassannonse');
+DEFINE('_JL_JOBAPPLICATION', 'Søknad om jobb');
+DEFINE('_JL_NOITEMS', 'Ingen stillinger funnet');
+DEFINE('_JL_ERRORSETTMPL', 'Fant ikke malfilen');
+DEFINE('_JL_JOBLOCATION', 'Hvor');
+DEFINE('_JL_JOBREFERENCE', 'Referanse');
+DEFINE('_JL_JOBDURATION', 'Varighet');
+DEFINE('_JL_DAYTELEPHONE', 'Telefon dagtid');
+DEFINE('_JL_EVENINGTELEPHONE', 'Telefon kveldstid');
+DEFINE('_JL_APPLICANTEMAIL', 'Email');
+DEFINE('_JL_JOBPOSTINGNOTALLOWED', 'Du får ikke lov til å legge inn annonser.');
+DEFINE('_JL_SUBMITYOURCV', 'Send inn din CV');
+DEFINE('_JL_ACCESS_DENIED', 'Du har ikke tillgang. Logg inn, eller registrer deg.');
+DEFINE('_JL_START_DATE', 'Start Dato');
+DEFINE('_JL_NEWITEM', 'New!');
+
+// Admin footer
+DEFINE('_OJ_VERSION', 'Versjon');
+DEFINE('_OJ_CREATED_BY', 'Denne komponenten er laget av');
+DEFINE('_OJ_VISIT', 'Besøk');
+DEFINE('_OJ_EXCITING_ADDONS', 'For flere spennende addons.');
+
+// Admin strings
+DEFINE('_JL_A_YES', 'Ja');
+DEFINE('_JL_A_NO', 'Nei');
+DEFINE('_JL_A_CONFIG_SAVED', 'konfigurasjonsfil lagret');
+DEFINE('_JL_A_CONFIGURATION', 'Jobline Configuration');
+DEFINE('_JL_A_INFORMATION', 'Informasjon');
+DEFINE('_JL_A_AREYOUSURE', 'Er du sikker?');
+DEFINE('_JL_A_SETTINGS', 'Settings');
+DEFINE('_JL_A_SELECTDELITEM', 'Velg en post for sletting');
+DEFINE('_JL_A_SELECTITEM', 'Velg en post for');
+DEFINE('_JL_A_NOTITLE', 'Du må skrive inn en tittel');
+DEFINE('_JL_A_ADDING', 'Legger til');
+DEFINE('_JL_A_EDITING', 'Editerer');
+DEFINE('_JL_A_JOBPOSTING', 'Jobbannonse');
+DEFINE('_JL_A_CONTENT', 'Rediger innhold');
+DEFINE('_JL_A_ENTER_TITLE', 'Tittel');
+DEFINE('_JL_A_ENTER_JOBTYPE', 'Jobb Type');
+DEFINE('_JL_A_ENTER_DESCRIPTION', 'Jobb beskrivelse');
+DEFINE('_JL_A_ENTER_QUALIFICATIONS', 'Kvalifikasjoner');
+DEFINE('_JL_A_ENTER_COMPENSATION', 'L¿nnsområde');
+DEFINE('_JL_A_SHOW_COMPENSATION' ,'Vis lønn?');
+DEFINE('_JL_A_HOW_TO_APPLY', 'Hvordan søke');
+DEFINE('_JL_A_LISTJOBS', 'Jobbannonser');
+DEFINE('_JL_A_DISPLAY', 'Vis #');
+DEFINE('_JL_A_JOBPOSTINGREJECTED', 'Stillingsannonsen din har blitt avvist');
+DEFINE('_JL_A_REJHI', 'Hei');
+DEFINE('_JL_A_REJECTION', 'Hei <<contactname>>,
+
+Vi har avvist din stillingsannonse med tittelen "<<jobtitle>>" På websiden <<sitename>>Kontakt admin på <<adminemail>>Ved sørsmål
+
+Takk.');
+DEFINE('_JL_A_JOBPOSTINGAPPROVED', 'Din annonse er lagt ut');
+DEFINE('_JL_A_APPROVE', 'Hei <<contactname>>,
+
+Vi har lagt inn din annonse ved navn "<<jobtitle>>" på websiden<<sitename>> Ved spørsmål angående dette kontakt admin på <<adminemail>>.
+
+Takk.');
+DEFINE('_JL_A_MAILFROMNAME', 'Navnet automatiske E-mails skal sendes fra');
+DEFINE('_JL_A_MAILFROMADDRESS', 'E-mail adressen automatiske E-mails skal sendes fra');
+DEFINE('_JL_TITLEASC', 'Tittel stigende');
+DEFINE('_JL_TITLEDESC', 'Tittel synkende');
+DEFINE('_JL_ORDERINGASC', 'Sortering stigende');
+DEFINE('_JL_ORDERINGDESC', 'Sortering synkende');
+DEFINE('_JL_JOBIDASC', 'Jobb ID stigende');
+DEFINE('_JL_JOBIDDESC', 'Jobb ID synkende');
+DEFINE('_JL_A_CREATEDASC', 'Dato stigende');
+DEFINE('_JL_A_CREATEDDESC', 'Dato synkende');
+DEFINE('_JL_A_ITEMLIMIT', 'Antall stillinger som skal vises pr side');
+DEFINE('_JL_A_SORTORDER', 'Sorteringsrekkefølge');
+DEFINE('_JL_A_LISTJOBQUEUE', 'Jobb som venter på godkjenning');
+DEFINE('_JL_A_JOBID', 'ID');
+DEFINE('_JL_A_TITLE', 'Tittel');
+DEFINE('_JL_A_JOB_TYPE', 'Jobb type');
+DEFINE('_JL_A_FUNCTIONS', 'Funksjoner');
+DEFINE('_JL_A_DATE_ADDED', 'Dato');
+DEFINE('_JL_A_COMPANY_NAME', 'Firma');
+DEFINE('_JL_A_PUBLISHED', 'Publisert');
+DEFINE('_JL_A_CHECKEDOUT', 'Utsjekket');
+DEFINE('_JL_A_REORDER', 'Sortere om');
+DEFINE('_JL_A_ENTER_COMPANYNAME', 'Skriv inn firmanavn');
+DEFINE('_JL_A_ENTER_ADDRESS1', 'Addresse 1');
+DEFINE('_JL_A_ENTER_ADDRESS2', 'Addresse 2');
+DEFINE('_JL_A_ENTER_CITY', 'Sted');
+DEFINE('_JL_A_ENTER_USSTATE', 'Velg fylke');
+DEFINE('_JL_A_ENTER_ZIPCODE', 'Postnr');
+DEFINE('_JL_A_ENTER_COMPANYURL', 'Firmaets hjemmeside');
+DEFINE('_JL_A_ENTER_CONTACTNAME', 'Kontaktperson');
+DEFINE('_JL_A_ENTER_CONTACTPHONE', 'Telefonnummer til kontaktperson');
+DEFINE('_JL_A_ENTER_CONTACTEMAIL', 'Emailadresse til kontaktperson');
+DEFINE('_JL_A_SHOWCONTACT', 'Vise kontaktinformasjon  i stillingsannonse?');
+DEFINE('_JL_A_ENTER_MEMBERID', 'Medlemsnummer');
+DEFINE('_JL_A_ENTER_CREDITCARD_TYPE', 'Kredittkort');
+DEFINE('_JL_A_ENTER_CREDITCARD_NUMBER', 'Kredittkortnummer');
+DEFINE('_JL_A_ENTER_CREDITCARD_EXPIRES', 'Utløpsdato');
+DEFINE('_JL_A_POSTJOB', 'Godkjenn');
+DEFINE('_JL_A_REJECTJOB', 'Avvis');
+DEFINE('_JL_A_SELECTITEMPOST', 'Velg en post for opplasting');
+DEFINE('_JL_A_MOVEUP', 'Flytt opp');
+DEFINE('_JL_A_MOVEDOWN', 'Flytt ned');
+DEFINE('_JL_A_TERMSOFSERVICE', 'Annonsevilkår');
+DEFINE('_JL_A_PUBLISHINGLIMIT', 'Antall dager annonsen skal vises (0 er ubegrenset)');
+DEFINE('_JL_A_CLOSE', 'Lukk');
+DEFINE('_JL_A_PRINT', 'Skriv ut');
+DEFINE('_JL_A_PREVIEW', 'Forhåndsvis stillingsannonse');
+DEFINE('_JL_A_NOIDYET', 'har ingen ID enda');
+DEFINE('_JL_A_PUBLISHITEM', 'Publiser stillingsannonse');
+DEFINE('_JL_A_SHOWCCFIELDS', 'Vis kredittkortfelt');
+DEFINE('_JL_A_SHOWUSSTATE', 'Vise valg for amerikansk delstat ved adresseformularet.');
+DEFINE('_JL_A_ENTER_JOBSTATUS', 'Skriv inn Jobb Status');
+DEFINE('_JL_A_DEFAULTJOBSTATUS', 'Verdi for jobb status');
+DEFINE('_JL_A_EMAILAPPLICATION', 'Benytt e-postprogram isteden for søknadsformular.');
+DEFINE('_JL_A_ENTER_JOBLOCATION', 'Sted');
+DEFINE('_JL_A_ENTER_JOBREFERENCE', 'Referanse');
+DEFINE('_JL_A_ENTER_JOBDURATION', 'Varighet');
+DEFINE('_JL_A_POSTJOBS', 'Begrens hvem som kan legge inn annonser på dette nivå');
+DEFINE('_JL_A_ITEMSNEWFOR', 'Antall dager for visning som ny stilling.');
+DEFINE('_JL_A_SELECTSTATE', 'Velg amerikansk stat');
+DEFINE('_JL_A_ORSELECTFROMLIST', 'Eller velg amerikansk stat:');
+DEFINE('_JL_A_KEYWORDCATS', 'N¿kkelkategorier');
+DEFINE('_JL_A_EXTRAFIELDS', 'Ekstra felt');
+DEFINE('_JL_A_COMPANY_CONTACT_INFO', 'Kontaktinformasjon');
+DEFINE('_JL_A_PAYMENTINFORMATION', 'Kredittkortinfo');
+DEFINE('_JL_A_JOBINFORMATION', 'Jobb Info');
+DEFINE('_JL_A_DATEFORMAT', 'Datoformat, la stå tomt for standard(uses <a target="_blank" href="http://se.php.net/manual/en/function.strftime.php">strftime()</a> syntax)');
+DEFINE('_JL_A_SELECTTEMPLATE', 'Velg maloppsett');
+DEFINE('_JL_A_AUTOAPPROVEJOBS', 'Godkjenn jobbannonser automatisk.');
+DEFINE('_JL_A_ACCESS', 'Tillgangsnivå');
+DEFINE('_JL_A_ACCESS_ALL', 'Offentlig');
+DEFINE('_JL_A_ACCESS_REGISTERED', 'Registrert');
+DEFINE('_JL_A_ACCESS_USER', 'Spesiell');
+DEFINE('_JL_A_ACCESS_NONE', 'Ingen');
+DEFINE('_JL_A_EXTRAINFO', 'Ekstra felt');
+DEFINE('_JL_A_PUBLISHING', 'Publisering');
+DEFINE('_JL_A_ITEM_DETAILS', 'Detaljer');
+DEFINE('_JL_A_COULDNTREADTEMPLATE', 'Kunne ikke vise malfil.');
+DEFINE('_JL_A_TEMPLATE_SAVED', 'Malfil er lagret.');
+DEFINE('_JL_A_LISTTEMPLATES', 'Vis malfiler fra maloppsett');
+DEFINE('_JL_A_CANCEL_TMPL', 'Malfilen ikke lagret.');
+DEFINE('_JL_A_EDITING_TEMPLATE', 'redigere malfil');
+DEFINE('_JL_A_TEMPLATE_FILE', 'Malfil');
+DEFINE('_JL_A_PATH', 'Lokasjon:');
+DEFINE('_JL_A_WRITEABLE', 'Skrivbar');
+DEFINE('_JL_A_UNWRITEABLE', 'Ikke skrivbar');
+DEFINE('_JL_A_NOEXTRAFIELDS', 'Ingen ekstra felt er konfigurert.');
+DEFINE('_JL_A_NO_EDITOR', 'No editor');
+DEFINE('_JL_A_DEFAULT_EDITOR', 'Mambo default editor');
+DEFINE('_JL_A_SELECTEDITOR', 'Use the Mambo default html editor on frontend?.');
+DEFINE('_JL_A_EDITORWIDTH', 'Width of HTML editor window in pixels');
+DEFINE('_JL_A_EDITORHEIGHT', 'Height of HTML editor window in pixels');
+DEFINE('_JL_A_INITIALIZE_HTML_EDITOR', 'Initialize WYSIWYG HTML editor for non-registered users?');
+
+// Module strings
+
+// Error messages
+DEFINE('_JL_ERR_YOURCONFIG', 'Konfigurasjonsfilen din er');
+DEFINE('_JL_ERR_CHMODFILE', 'Du må endre filrettighetene til 766 for å kunne lagre.');
+DEFINE('_JL_ERR_WARNING', 'Advarsel...');
+DEFINE('_JL_ERR_NOT_WRITEABLE', 'Kan ikke skrive til filen');
+DEFINE('_JL_ERR_OPEN_FILE', 'Alvorlig feil: Kan ikke åpne.');
+DEFINE('_JL_ERR_NOTITLE', 'tittel må angis');
+DEFINE('_JL_ERR_CHECKED_OUT1', 'Posten');
+DEFINE('_JL_ERR_CHECKED_OUT2', 'Endres av en annen administrator.');
+DEFINE('_JL_ERR_TMPL_NOT_WRITEABLE', 'Alvorlig feil: Kan ikke skrive til malfilen·');
+
+#DEFINE('', '');
+
+?>
